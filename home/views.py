02ef3b3838192a61f109d5348963b13373d9bf6a
Hermotimos <lukas.kozicki@gmail.com>
2019-05-30 15:18:42 +0200

    rename

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def home_view(request):
     context = {
-        'title': 'Home'
+        'page_title': 'Home'
     }
     return render(request, 'home/home.html', context)
