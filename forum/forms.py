d223f9f76188b433149f22c224ba9b6ac20a1ebf
Hermotimos <lukas.kozicki@gmail.com>
2019-06-04 16:49:49 +0200

    override Model fields // failed attempt at override ValidationError

--- forum/forms.py
+++ forum/forms.py
@@ -4,6 +4,20 @@ from .models import Post, Topic
 
 
 class CreatePostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'topic',
+            'text',
+            'rpg_time'
+        ]
+
+    topic = forms.ModelChoiceField(
+        label='Wybierz tytuł narady',
+        queryset=Topic.objects.all(),
+        # initial='-'
+    )
+
     text = forms.CharField(
         label='',
         widget=forms.Textarea(
@@ -14,12 +28,9 @@ class CreatePostForm(forms.ModelForm):
             }
         )
     )
-    topic = forms.ModelChoiceField(
-        label='Wybierz tytuł narady',
-        queryset=Topic.objects.all()
-    )
+
     rpg_time = forms.CharField(
-        label='Data',
+        label='Czas RPG',
         widget=forms.Textarea(
             attrs={'placeholder': 'np. 80. Jesieni 20. roku Archonatu Nemetha Samatiana',
                    'rows': 1,
@@ -28,10 +39,10 @@ class CreatePostForm(forms.ModelForm):
         )
     )
 
-    class Meta:
-        model = Post
-        fields = [
-            'topic',
-            'text',
-            'rpg_time'
-        ]
+    def clean_rpg_time(self):
+        data = self.cleaned_data
+        if not data.get['rpg_time']:
+            raise forms.ValidationError('Wybierz temat narady, w której chcesz zabrać głos:')
+        return data
+        # This does not work at all. The overriden error message does not show.
+        # Couldn't find answer why.
