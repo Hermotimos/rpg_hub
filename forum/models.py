b93e417ecea349db08815b056b82e0b78f2f9c4c
Hermotimos <lukas.kozicki@gmail.com>
2019-06-07 12:28:12 +0200

    minor

--- forum/models.py
+++ forum/models.py
@@ -16,7 +16,6 @@ class Board(models.Model):
     title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True, blank=True)
     description = models.CharField(max_length=100)
-    # publish_status = models.IntegerField(choices=STATUS, default=0)
 
     def __str__(self):
         return self.title
