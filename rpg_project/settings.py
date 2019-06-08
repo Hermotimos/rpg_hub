1c7eedc0d4f3d47c31e703d34020e88417af0711
Hermotimos <lukas.kozicki@gmail.com>
2019-06-08 15:28:04 +0200

    pip install django-crispy-forms + added in settings.py (apps and CRISPY_TEMPLATE_PACK = 'bootstrap4') + in loaded to template

--- rpg_project/settings.py
+++ rpg_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # imported
     'multiselectfield',
+    'crispy_forms',
 
     # own
     'home',
@@ -130,3 +131,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
