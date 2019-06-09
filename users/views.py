de43890cac084cb0a3f058430b60cf78f5602fcc
Hermotimos <lukas.kozicki@gmail.com>
2019-06-09 14:55:42 +0200

    update profile features created

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 # imports for LoginView and LogoutView
 from django.contrib.auth.views import (SuccessURLAllowedHostsMixin, FormView, TemplateView, AuthenticationForm,
@@ -30,8 +30,23 @@ def register_view(request):
 
 @login_required
 def profile_view(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Zaktualizowano profil postaci!')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
-        'page_title': 'Profil',
+        'page_title': 'Postać',
+        'user_form': user_form,
+        'profile_form': profile_form
     }
     return render(request, 'users/profile.html', context)
 
