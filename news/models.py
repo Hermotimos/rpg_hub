1833e7d8b6306d34491b383d515d9dfeebfec440
Hermotimos <lukas.kozicki@gmail.com>
2019-06-10 14:38:10 +0200

    News model created

--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from multiselectfield import MultiSelectField       # pip install django-multiselectfield
 
-# Create your models here.
+USERS = [(user.username, user.username) for user in User.objects.all()]
+
+
+class News(models.Model):
+    text = models.TextField(max_length=4000)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, related_name='news', on_delete=models.CASCADE)
+    allowed_users = MultiSelectField(max_length=100, choices=USERS)
+
+    def __str__(self):
+        return self.text[:30]
