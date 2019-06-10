b51f486e091cff6768e538a787164654bdc3b79a
Hermotimos <lukas.kozicki@gmail.com>
2019-06-10 14:36:46 +0200

    added character_status field to Profile model

--- users/models.py
+++ users/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+STATUS = [
+    ('player', 'Postać gracza'),
+    ('npc', 'BN'),
+    ('gm', 'MG')
+]
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    character_status = models.CharField(max_length=20, choices=STATUS, default='none')
 
     def __str__(self):
         return f'{self.user.username}'
