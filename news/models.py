84d86a92ab1ba44a67830208548c34e7401c344a
Hermotimos <lukas.kozicki@gmail.com>
2019-06-10 14:49:22 +0200

    model News modified and registered in admin.py

--- news/models.py
+++ news/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from users.models import Profile
 from multiselectfield import MultiSelectField
 
-USERS = [(user.username, user.username) for user in User.objects.all()]
+PLAYERS = [(player.user.username, player.user.username) for player in Profile.objects.filter(character_status='player')]
 
 
 class News(models.Model):
     text = models.TextField(max_length=4000)
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, related_name='news', on_delete=models.CASCADE)
-    allowed_users = MultiSelectField(max_length=100, choices=USERS)
+    allowed_users = MultiSelectField(max_length=100, choices=PLAYERS)
 
     def __str__(self):
-        return self.text[:30]
+        return self.text[:50]
