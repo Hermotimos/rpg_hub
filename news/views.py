b880dd053719596217f29ef4f717f7adbd237578
Hermotimos <lukas.kozicki@gmail.com>
2019-06-10 15:12:55 +0200

    template, view and url for create_news

--- news/views.py
+++ news/views.py
@@ -7,3 +7,10 @@ def news_view(request):
         'page_title': 'Słup ogłoszeń'
     }
     return render(request, 'news/news-list.html', context)
+
+
+def create_news_view(request):
+    context = {
+        'page_title': 'Nowe ogłoszenie'
+    }
+    return render(request, 'news/news-create.html', context)
