3fe084c04583b630ebdf39062ba6f31d78cebb96
Hermotimos <lukas.kozicki@gmail.com>
2019-07-01 16:05:28 +0200

    renames

--- forum/views.py
+++ forum/views.py
@@ -17,13 +17,6 @@ def forum_view(request):
         last_post = topic.posts.filter(date_posted=topics_with_last_post_date_dict[topic])
         topics_with_last_active_user_dict[topic] = last_post[0].author.profile.character_name if last_post else ''
 
-    # boards_with_allowed_users_dict = {}
-    # for board in boards_list:
-    #     allowed_users_as_str = ''
-    #     for topic in board.topics.all():
-    #         allowed_users_as_str += str(topic.allowed_users)
-    #     boards_with_allowed_users_dict[board] = allowed_users_as_str
-
     boards_with_allowed_profiles_dict = {}
     for board in boards_list:
         allowed_profiles = ''
@@ -38,7 +31,6 @@ def forum_view(request):
         'boards_list': boards_list,
         'topics_with_last_post_date_dict': topics_with_last_post_date_dict,
         'topics_with_last_active_user_dict': topics_with_last_active_user_dict,
-        # 'boards_with_allowed_users_dict': boards_with_allowed_users_dict,
         'boards_with_allowed_profiles_dict': boards_with_allowed_profiles_dict
     }
     return render(request, 'forum/forum.html', context)
@@ -50,22 +42,22 @@ def posts_in_topic_view(request, board_slug, topic_slug):
     logged_user = request.user
 
     if request.method == 'POST':
-        new_post_form = CreatePostForm(request.POST or None)
+        post_form = CreatePostForm(request.POST or None)
 
-        if new_post_form.is_valid():
-            new_post = new_post_form.save(commit=False)
-            new_post.topic = current_topic
-            new_post.author = logged_user
-            new_post.save()
+        if post_form.is_valid():
+            post = post_form.save(commit=False)
+            post.topic = current_topic
+            post.author = logged_user
+            post.save()
             return redirect('topic', board_slug=board_slug, topic_slug=current_topic.slug)
 
     else:
-        new_post_form = CreatePostForm()            # equals to: form = CreatePostForm(request.GET) - GET is the default
+        post_form = CreatePostForm()            # equals to: form = CreatePostForm(request.GET) - GET is the default
 
     context = {
         'page_title': current_topic.topic_name,
         'topic': current_topic,
-        'new_post_form': new_post_form
+        'post_form': post_form
     }
     return render(request, 'forum/topic.html', context)
 
@@ -75,7 +67,6 @@ def add_allowed_profiles_view(request, board_slug, topic_slug):
 
     if request.method == 'POST':
         topic_update_form = UpdateTopicForm(request.POST, instance=current_topic)
-
         if topic_update_form.is_valid():
             topic_update_form.save()
             return redirect('topic', board_slug=board_slug, topic_slug=current_topic.slug)
@@ -95,25 +86,25 @@ def create_topic_view(request, board_slug):
     logged_user = request.user
 
     if request.method == 'POST':
-        form = CreateTopicForm(request.POST or None)
-        if form.is_valid():
-            topic = form.save(commit=False)
+        topic_form = CreateTopicForm(request.POST or None)
+        if topic_form.is_valid():
+            topic = topic_form.save(commit=False)
             topic.board = Board.objects.get(slug=board_slug)
             topic.starter = logged_user
             topic.save()
 
             Post.objects.create(
-                text=form.cleaned_data.get('first_post'),
+                text=topic_form.cleaned_data.get('first_post'),
                 topic=topic,
                 author=logged_user
             )
             return redirect('topic', board_slug=board_slug, topic_slug=topic.slug)
     else:
-        form = CreateTopicForm()                   # equals to: form = CreateTopicForm(request.GET) - GET is the default
+        topic_form = CreateTopicForm()            # equals to: form = CreateTopicForm(request.GET) - GET is the default
 
     context = {
         'page_title': 'Nowa narada',
-        'topic_form': form,
+        'topic_form': topic_form,
     }
     return render(request, 'forum/create_topic.html', context)
 
@@ -121,15 +112,15 @@ def create_topic_view(request, board_slug):
 @login_required
 def create_board_view(request):
     if request.method == 'POST':
-        form = CreateBoardForm(request.POST or None)
-        if form.is_valid():
-            board = form.save()
+        board_form = CreateBoardForm(request.POST or None)
+        if board_form.is_valid():
+            board = board_form.save()
             return redirect('create_topic', board_slug=board.slug)
     else:
-        form = CreateBoardForm()                   # equals to: form = CreateBoardForm(request.GET) - GET is the default
+        board_form = CreateBoardForm()             # equals to: form = CreateBoardForm(request.GET) - GET is the default
 
     context = {
         'page_title': 'Nowy temat',
-        'board': form,
+        'board': board_form,
     }
     return render(request, 'forum/create_board.html', context)
