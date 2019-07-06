f3af7dade23498db669b5961a3d2d39816bd5145
Hermotimos <lukas.kozicki@gmail.com>
2019-07-06 15:49:03 +0200

    renamed url pattern according to template name

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,7 @@ from .views import news_view, create_news_view, news_detail_view
 
 
 urlpatterns = [
-    path('', news_view, name='news-main'),
+    path('', news_view, name='news-list'),
     path('news-create/', create_news_view, name='news-create'),
     path('<slug:slug>/', news_detail_view, name='news-detail')
 ]
