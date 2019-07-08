546694424401f3734d743d46d407e05c51eaa80f
Hermotimos <lukas.kozicki@gmail.com>
2019-07-08 13:26:18 +0200

    Response model created in news app

--- news/models.py
+++ news/models.py
@@ -45,3 +45,24 @@ class News(models.Model):
 
     class Meta:
         ordering = ['-date_updated']
+
+
+class Response(models.Model):
+    text = models.TextField(max_length=4000)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, related_name='responses', on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True, upload_to='news_pics')
+    news = models.ForeignKey(News, related_name='responses', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+    def save(self, *args, **kwargs):
+        super().save(*args, *kwargs)
+
+        if self.image:
+            img = Image.open(self.image.path)
+            if img.height > 700 or img.width > 700:
+                output_size = (700, 700)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
