bf8009ce3623952b4963fb6d71c96d5e01005785
Hermotimos <lukas.kozicki@gmail.com>
2019-07-08 13:28:32 +0200

    CreateResponseForm created

--- news/forms.py
+++ news/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from news.models import News
+from news.models import News, Response
 from pagedown.widgets import PagedownWidget
 
 
@@ -41,3 +41,29 @@ class CreateNewsForm(forms.ModelForm):
         required=False,
     )
 # TODO why image doesn't affect actual form ?? no label, required=False could be absent...
+
+
+class CreateResponseForm(forms.ModelForm):
+    class Meta:
+        model = Response
+        fields = [
+            'text',
+            'image'
+        ]
+
+    text = forms.CharField(
+        label='',
+        max_length=4000,
+        widget=PagedownWidget(
+            attrs={
+                'placeholder': 'Twoja odpowiedź (max. 4000 znaków)*',
+                'rows': 10,
+                'cols': 60
+            }
+        )
+    )
+
+    image = forms.ImageField(
+        label='Załącz obraz:',
+        required=False,
+    )
