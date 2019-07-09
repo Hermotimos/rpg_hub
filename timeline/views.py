62ac9f01656c1f3e342de5987e82c5ad714813e0
Hermotimos <lukas.kozicki@gmail.com>
2019-07-09 17:55:47 +0200

    timeline.html template developed (with timeline_view)

--- timeline/views.py
+++ timeline/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from users.models import Profile
+from timeline.models import Event
 
 
 @login_required
 def timeline_view(request):
+    event_list = Event.objects.all()
 
     context = {
-        'page_title': 'Kalendarium'
+        'page_title': 'Kalendarium',
+        'event_list': event_list
     }
     return render(request, 'timeline/timeline.html', context)
