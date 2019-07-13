0f004c3be002819e60ebe8acf9f6f972c5a47c79
Hermotimos <lukas.kozicki@gmail.com>
2019-07-13 21:37:40 +0200

    ordering added to Thread

--- timeline/models.py
+++ timeline/models.py
@@ -16,6 +16,9 @@ class Thread(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['name']
+
 
 class GeneralLocation(models.Model):
     name = models.CharField(max_length=200)
