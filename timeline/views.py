871d4f64a0ea42ed2739690bc59c26e99096381f
Hermotimos <lukas.kozicki@gmail.com>
2019-07-15 15:14:01 +0200

    attempt at timeline_filtered

--- timeline/views.py
+++ timeline/views.py
@@ -14,16 +14,30 @@ def timeline_view(request):
     return render(request, 'timeline/timeline.html', context)
 
 
+# @login_required
+# def thread_view(request, thread_name):
+#
+#
+#     context = {}
+#     return render(request, 'timeline/thread.html', context)
+
+
 @login_required
-def timeline__filtered_view(request, year=None, thread=None, participants_choice=None, location1=None, location2=None):
+def timeline__filtered_view(request, year=None, thread_id=None, general_location=None, specific_location=None):
     event_list = Event.objects.all()
-    criteria = year, thread, participants_choice, location1, location2
-    for criterion in criteria:
-        if criterion is not None:
-            event_list = Event.objects.get(criterion=criterion)
+
+    if year is not None:
+        event_list = event_list.objects.filter(year=year)
+        if thread_id is not None:
+            event_list = event_list.objects.filter(thread=thread_id)
+            if general_location is not None:
+                event_list = event_list.objects.filter(general_location=general_location)
+                if specific_location is not None:
+                    event_list = event_list.objects.filter(specific_location=specific_location)
+
 
     context = {
-        'page_title': 'Kalendarium',
+        'page_title': 'Kalendarium [filtered]',
         'event_list': event_list
     }
-    return render(request, 'timeline/timeline.html', context)
+    return render(request, 'timeline/timeline-filtered.html', context)
