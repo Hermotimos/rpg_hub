4aaeee093105bfd86fe46ba29f4d7ba80d122af8
Hermotimos <lukas.kozicki@gmail.com>
2019-07-18 11:51:31 +0200

    CreateEventForm customized

--- timeline/forms.py
+++ timeline/forms.py
@@ -7,6 +7,14 @@ class CreateEventForm(forms.ModelForm):
         model = Event
         exclude = ()
 
+    def __init__(self, *args, **kwargs):
+        super(CreateEventForm, self).__init__(*args, **kwargs)
+        self.fields['threads'].widget.attrs = {'size': 10}
+        self.fields['description'].widget.attrs = {'cols': 50, 'rows': 5}
+        self.fields['participants'].widget.attrs = {'size': 8}
+        self.fields['informed'].widget.attrs = {'size': 8}
+        self.fields['specific_locations'].widget.attrs = {'size': 10}
+
 
 class EventAddInformedForm(forms.ModelForm):
     class Meta:
