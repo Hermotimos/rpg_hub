120659d273432c215a73b85bec69d6a77a59c28e
Hermotimos <lukas.kozicki@gmail.com>
2019-07-23 09:55:09 +0200

    renames for reusable code

--- timeline/views.py
+++ timeline/views.py
@@ -11,13 +11,6 @@ from timeline.forms import CreateEventForm, EventAddInformedForm, EditEventForm,
 @login_required
 def timeline_view(request):
 
-    seasons_with_styles_dict = {
-        '1': 'season-spring',
-        '2': 'season-summer',
-        '3': 'season-autumn',
-        '4': 'season-winter'
-    }
-
     if request.user.profile == Profile.objects.get(character_status='gm'):
         events_queryset = Event.objects.all()
     else:
@@ -36,22 +29,22 @@ def timeline_view(request):
 @login_required
 def create_event_view(request):
     if request.method == 'POST':
-        event_form = CreateEventForm(request.POST or None)
-        if event_form.is_valid():
-            event = event_form.save()
-            event.threads.set(event_form.cleaned_data['threads'])
-            event.participants.set(event_form.cleaned_data['participants'])
-            event.informed.set(event_form.cleaned_data['informed'])
-            event.specific_locations.set(event_form.cleaned_data['specific_locations'])
+        form = CreateEventForm(request.POST or None)
+        if form.is_valid():
+            event = form.save()
+            event.threads.set(form.cleaned_data['threads'])
+            event.participants.set(form.cleaned_data['participants'])
+            event.informed.set(form.cleaned_data['informed'])
+            event.specific_locations.set(form.cleaned_data['specific_locations'])
             event.save()
             messages.success(request, f'Dodano wydarzenie!')
             return redirect('timeline')
     else:
-        event_form = CreateEventForm()
+        form = CreateEventForm()
 
     context = {
         'page_title': 'Nowe wydarzenie',
-        'event_form': event_form
+        'form': form
     }
     return render(request, 'timeline/create_event.html', context)
 
@@ -61,17 +54,17 @@ def edit_event_view(request, event_id):
     current_event = get_object_or_404(Event, id=event_id)
 
     if request.method == 'POST':
-        edit_event_form = EditEventForm(request.POST, instance=current_event)
-        if edit_event_form.is_valid():
-            edit_event_form.save()
+        form = EditEventForm(request.POST, instance=current_event)
+        if form.is_valid():
+            form.save()
             messages.success(request, f'Zmodyfikowano wydarzenie!')
             return redirect('timeline')
     else:
-        edit_event_form = EditEventForm(instance=current_event)
+        form = EditEventForm(instance=current_event)
 
     context = {
         'page_title': 'Edycja wydarzenia',
-        'edit_event_form': edit_event_form
+        'form': form
     }
     return render(request, 'timeline/edit_event.html', context)
 
@@ -81,9 +74,9 @@ def event_add_informed_view(request, event_id):
     current_event = get_object_or_404(Event, id=event_id)
 
     if request.method == 'POST':
-        add_informed_form = EventAddInformedForm(request.POST, instance=current_event)
-        if add_informed_form.is_valid():
-            add_informed_form.save()
+        form = EventAddInformedForm(request.POST, instance=current_event)
+        if form.is_valid():
+            form.save()
 
             if current_event.season == 'spring':
                 season = 'Wiosny'
@@ -97,7 +90,7 @@ def event_add_informed_view(request, event_id):
             subject = f"[RPG] {request.user.profile} opowiedział o swoich przygodach"
             message = f"{request.user.profile} znów rozprawia o swoich przygodach.\n" \
                       f"Oto kto już o nich słyszał: " \
-                      f"{[p.character_name for p in add_informed_form.cleaned_data['informed']]}\n\n" \
+                      f"{[p.character_name for p in form.cleaned_data['informed']]}\n\n" \
                       f"{current_event.day_start}{'-' + current_event.day_end if current_event.day_end else ''}" \
                       f" dnia {season} {current_event.year + 19}. " \
                       f"roku Archonatu Nemetha Samatiana rozegrało się co następuje:\n {current_event.description}\n" \
@@ -107,18 +100,18 @@ def event_add_informed_view(request, event_id):
             sender = settings.EMAIL_HOST_USER
             receivers_list = []
             for user in User.objects.all():
-                if user.profile in add_informed_form.cleaned_data['informed']:
+                if user.profile in form.cleaned_data['informed']:
                     receivers_list.append(user.email)
             send_mail(subject, message, sender, receivers_list)
 
             messages.success(request, f'Poinformowano wybrane postaci!')
             return redirect('timeline')
     else:
-        add_informed_form = EventAddInformedForm(instance=current_event)
+        form = EventAddInformedForm(instance=current_event)
 
     context = {
         'page_title': 'Poinformuj o wydarzeniu',
-        'add_informed_form': add_informed_form,
+        'form': form,
         'current_event': current_event
     }
     return render(request, 'timeline/event_add_informed.html', context)
@@ -135,20 +128,20 @@ def event_note_view(request, event_id):
         pass
 
     if request.method == 'POST':
-        note_form = EventNoteForm(request.POST, instance=current_note)
-        if note_form.is_valid():
-            note = note_form.save(commit=False)
+        form = EventNoteForm(request.POST, instance=current_note)
+        if form.is_valid():
+            note = form.save(commit=False)
             note.author = request.user
             note.event = current_event
             note.save()
             messages.success(request, f'Dodano notatkę!')
             return redirect('timeline')
     else:
-        note_form = EventNoteForm(instance=current_note)
+        form = EventNoteForm(instance=current_note)
 
     context = {
         'page_title': 'Notatka',
         'current_event': current_event,
-        'note_form': note_form
+        'form': form
     }
     return render(request, 'timeline/event_note.html', context)
