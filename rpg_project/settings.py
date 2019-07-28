c1bfd3bff774dd152dd16f95a34b31fac23f23db
Hermotimos <lukas.kozicki@gmail.com>
2019-07-28 19:18:15 +0200

    SECRET_KEY moved to credentials

--- rpg_project/settings.py
+++ rpg_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .credentials import EMAIL_HOST_USER, PASSWORD
+from .credentials import USER, PASSWORD, KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8bb8lkqy+^)!8i-ft*+1hb9&h&imr*n7yhwm#!3cwp6=^(expj'
+SECRET_KEY = KEY
 
 
 # WARNING: don't run with debug turned on in production!
@@ -153,5 +153,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_USER = USER
 EMAIL_HOST_PASSWORD = PASSWORD
