da7b081b79cdd45aa60c3fa37ccd21ecf21e0b05
Hermotimos <lukas.kozicki@gmail.com>
2019-07-30 22:38:51 +0200

    timeline.urls - restored explicit imports of views (otherwise pythonanywhere crashes)

--- timeline/urls.py
+++ timeline/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from timeline.views import *
+from timeline.views import timeline_main_view, timeline_events_view, timeline_by_thread_view, \
+    timeline_by_participant_view, timeline_by_general_location_view, timeline_by_specific_location_view, \
+    timeline_by_year_view, create_event_view, edit_event_view, event_add_informed_view, \
+    event_note_view, described_event_note_view, chronicles_chapters_view, chronicles_all_view, \
+    chronicles_one_chapter_view, create_described_event_view, described_event_add_informed_view,\
+    edit_described_event_view
 
 
 urlpatterns = [
@@ -10,7 +15,6 @@ urlpatterns = [
     path('timeline-by-general-location/<int:general_location_id>/', timeline_by_general_location_view, name='timeline-by-general-location'),
     path('timeline-by-specific-location/<int:specific_location_id>/', timeline_by_specific_location_view, name='timeline-by-specific-location'),
     path('timeline-by-year/<int:year>/', timeline_by_year_view, name='timeline-by-year'),
-
     path('create-event/', create_event_view, name='create-event'),
     path('<int:event_id>/event-add-informed/', event_add_informed_view, name='add-informed'),
     path('<int:event_id>/event-note/', event_note_view, name='event-note'),
