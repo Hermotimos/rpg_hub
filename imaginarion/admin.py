492758f2df441c4f43218cffbeae9281a379ad9a
Hermotimos <lukas.kozicki@gmail.com>
2019-08-10 13:49:35 +0200

    customizing admin: Picture

--- imaginarion/admin.py
+++ imaginarion/admin.py
@@ -3,7 +3,8 @@ from imaginarion.models import Picture
 
 
 class PictureAdmin(admin.ModelAdmin):
-    list_display = ('admin_title', 'description')
+    list_display = ('admin_title', 'description', 'image')
+    list_editable = ('description', 'image')
 
 
 admin.site.register(Picture, PictureAdmin)
