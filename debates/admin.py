88aa6922d9dc812511e0b9ff863f5bc915f53da6
Hermotimos <lukas.kozicki@gmail.com>
2019-08-10 13:49:23 +0200

    minor

--- debates/admin.py
+++ debates/admin.py
@@ -6,18 +6,14 @@ class TopicAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'date_created', 'date_updated')
 
 
-admin.site.register(Topic, TopicAdmin)
-
-
 class DebateAdmin(admin.ModelAdmin):
     list_display = ('title', 'topic', 'is_ended', 'is_individual', 'date_created', 'date_updated', )
-
-
-admin.site.register(Debate, DebateAdmin)
 
 
 class RemarkAdmin(admin.ModelAdmin):
     list_display = ('text_begin', 'debate', 'author', 'date_posted', 'image')
 
 
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Debate, DebateAdmin)
 admin.site.register(Remark, RemarkAdmin)
