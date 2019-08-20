8d71a2b0d17bb9dcf1602991b12b217e45b7e86d
Hermotimos <lukas.kozicki@gmail.com>
2019-08-20 17:21:47 +0200

    test functions renames

--- rules/tests/tests.py
+++ rules/tests/tests.py
@@ -4,11 +4,11 @@ from rules.views import rules_main_view
 
 
 class RulesMainTest(TestCase):
-    def test_rules_main_view_status_code(self):
+    def test_get(self):
         url = reverse('rules:main')
         response = self.client.get(url, follow=True)            # follow=True follows beyond @login_required
         self.assertEquals(response.status_code, 200)
 
-    def test_main_url_resolves_rules_main_view(self):
+    def test_url_resolves_view(self):
         view = resolve('/rules/')
         self.assertEquals(view.func, rules_main_view)
