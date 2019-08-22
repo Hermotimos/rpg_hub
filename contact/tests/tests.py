8d2f68e8332fd47cbf9dab49587a519a1c3eb449
Hermotimos <lukas.kozicki@gmail.com>
2019-08-22 07:50:29 +0200

    tests in contact updated

--- contact/tests/tests.py
+++ contact/tests/tests.py
@@ -1,18 +1,18 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from contact  import views
+from contact import views
 from contact.models import Demand
 from users.models import User
 
 
-class ReportTest(TestCase):
+class CreateDemandTest(TestCase):
     def test_get(self):
-        url = reverse('contact:report')
+        url = reverse('contact:create')
         response = self.client.get(url, follow=True)            # follow=True follows beyond @login_required
         self.assertEquals(response.status_code, 200)
 
     def test_url_resolves_view(self):
-        view = resolve('/contact/')
+        view = resolve('/contact/demands/create')
         self.assertEquals(view.func, views.create_demand_view)
 
 
@@ -22,10 +22,10 @@ class ReportsListTest(TestCase):
         Demand.objects.create(author=mock_user, text='Mock report')
 
     def test_get(self):
-        url = reverse('contact:reports-list')
+        url = reverse('contact:main')
         response = self.client.get(url, follow=True)
         self.assertEquals(response.status_code, 200)
 
     def test_url_resolves_view(self):
-        view = resolve('/contact/reports-list/')
+        view = resolve('/contact/demands/')
         self.assertEquals(view.func, views.main_view)
