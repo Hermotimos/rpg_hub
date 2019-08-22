78779834afe710aa847e03f5ee073c8850c8ac46
Hermotimos <lukas.kozicki@gmail.com>
2019-08-22 08:53:04 +0200

    contact app tests developed

--- contact/tests/tests.py
+++ contact/tests/tests.py
@@ -5,18 +5,7 @@ from contact.models import Demand
 from users.models import User
 
 
-class CreateDemandTest(TestCase):
-    def test_get(self):
-        url = reverse('contact:create')
-        response = self.client.get(url, follow=True)            # follow=True follows beyond @login_required
-        self.assertEquals(response.status_code, 200)
-
-    def test_url_resolves_view(self):
-        view = resolve('/contact/demands/create')
-        self.assertEquals(view.func, views.create_demand_view)
-
-
-class ReportsListTest(TestCase):
+class MainTest(TestCase):
     def setUp(self):
         mock_user = User.objects.create_user(username='test_user', email='test@user.com', password='sswapord123')
         Demand.objects.create(author=mock_user, text='Mock report')
@@ -29,3 +18,14 @@ class ReportsListTest(TestCase):
     def test_url_resolves_view(self):
         view = resolve('/contact/demands/')
         self.assertEquals(view.func, views.main_view)
+
+
+class CreateDemandTest(TestCase):
+    def test_get(self):
+        url = reverse('contact:create')
+        response = self.client.get(url, follow=True)            # follow=True follows beyond @login_required
+        self.assertEquals(response.status_code, 200)
+
+    def test_url_resolves_view(self):
+        view = resolve('/contact/demands/create')
+        self.assertEquals(view.func, views.create_demand_view)
