82c48c44bc95451f433e493ab6e79e27af83fd9e
Hermotimos <lukas.kozicki@gmail.com>
2019-08-22 09:48:05 +0200

    more tests for debates app

--- debates/tests/tests.py
+++ debates/tests/tests.py
@@ -79,3 +79,33 @@ class DebatesInviteTest(TestCase):
         self.assertEquals(view.func, views.debates_invite_view)
 
 
+class UnfollowDebateTest(TestCase):
+    def setUp(self):
+        mock_topic = Topic.objects.create(title='Mock Topic', description='Mock description.')
+        mock_user = User.objects.create_user(username='mock_user', email='mock@user.com', password='fakepsswrd111')
+        Debate.objects.create(topic=mock_topic, starter=mock_user)
+
+    def test_get(self):
+        url = reverse('debates:unfollow', kwargs={'topic_id': 1, 'debate_id': 1})
+        response = self.client.get(url, follow=True)
+        self.assertEquals(response.status_code, 200)
+
+    def test_url_resolves_view(self):
+        view = resolve('/debates/topic:1/debate:1/unfollow/')
+        self.assertEquals(view.func, views.unfollow_debate_view)
+
+
+class FollowDebateTest(TestCase):
+    def setUp(self):
+        mock_topic = Topic.objects.create(title='Mock Topic', description='Mock description.')
+        mock_user = User.objects.create_user(username='mock_user', email='mock@user.com', password='fakepsswrd111')
+        Debate.objects.create(topic=mock_topic, starter=mock_user)
+
+    def test_get(self):
+        url = reverse('debates:follow', kwargs={'topic_id': 1, 'debate_id': 1})
+        response = self.client.get(url, follow=True)
+        self.assertEquals(response.status_code, 200)
+
+    def test_url_resolves_view(self):
+        view = resolve('/debates/topic:1/debate:1/follow/')
+        self.assertEquals(view.func, views.follow_debate_view)
--- debates/urls.py
+++ debates/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('topic:<int:topic_id>/create-debate/', views.create_debate_view, name='create-debate'),
     path('topic:<int:topic_id>/debate:<int:debate_id>/', views.debate_view, name='debate'),
     path('topic:<int:topic_id>/debate:<int:debate_id>/invite/', views.debates_invite_view, name='invite'),
-    path('topic:<int:topic_id>/debate:<int:debate_id>/unfollow', views.unfollow_debate_view, name='unfollow'),
-    path('topic:<int:topic_id>/debate:<int:debate_id>/follow', views.follow_debate_view, name='follow'),
+    path('topic:<int:topic_id>/debate:<int:debate_id>/unfollow/', views.unfollow_debate_view, name='unfollow'),
+    path('topic:<int:topic_id>/debate:<int:debate_id>/follow/', views.follow_debate_view, name='follow'),
 ]
