53d346a439a0f6d044ee6949358a500823c3c1d0
Hermotimos <lukas.kozicki@gmail.com>
2019-08-23 10:10:05 +0200

    debates app tests simplified + added test_contains_link_to_main()

--- debates/tests/tests.py
+++ debates/tests/tests.py
@@ -7,64 +7,48 @@ from users.models import User
 
 class DebatesMainTest(TestCase):
     def setUp(self):
-        mock_topic = Topic.objects.create(title='Mock Topic', description='Mock description.')
+        url = reverse('debates:main')
+        self.response = self.client.get(url, follow=True)
+
+        mock_topic = Topic.objects.create(id=1, title='Mock Topic', description='Mock description.')
         mock_user = User.objects.create_user(username='mock_user', email='mock@user.com', password='fakepsswrd111')
-        debate = Debate.objects.create(topic=mock_topic, starter=mock_user, title='Mock Debate')
-        allowed = [mock_user.profile, ]
-        debate.allowed_profiles.set(allowed)
-        # self.client.force_login(mock_user)
+        Debate.objects.create(id=1, topic=mock_topic, starter=mock_user, title='Mock Debate')
         self.client.login(username=mock_user.username, password=mock_user.password)
 
     def test_get(self):
-        url = reverse('debates:main')
-        response = self.client.get(url, follow=True)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_url_resolves_view(self):
         view = resolve('/debates/')
         self.assertEquals(view.func, views.debates_main_view)
 
-    def test_new_topic_entry(self):
-        url = reverse('debates:main')
-        response = self.client.get(url, follow=True)
-        self.assertContains(response, 'Mock Topic')
-
-    def test_new_debate_entry(self):
-        url = reverse('debates:main')
-        response = self.client.get(url, follow=True)
-        self.assertContains(response, 'Mock Debate')
-
 
 class CreateTopicTest(TestCase):
-    def test_get(self):
+    def setUp(self):
         url = reverse('debates:create-topic')
-        response = self.client.get(url, follow=True)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url, follow=True)
+
+    def test_get(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_url_resolves_view(self):
         view = resolve('/debates/create_topic/')
         self.assertEquals(view.func, views.create_topic_view)
 
-    def test_contains_link_back(self):
-        linked_url = reverse('debates:main')
-        response = self.client.get(linked_url, follow=True)
-        self.assertContains(response, f'href="{linked_url}"')
-
     def test_contains_link_to_main(self):
-        url = reverse('debates:create-debate', kwargs={'topic_id': 1})
         linked_url = reverse('debates:main')
-        response = self.client.get(url, follow=True)
-        self.assertContains(response, f'href="{linked_url}"')
+        self.assertContains(self.response, f'href="{linked_url}"')
 
 
 class CreateDebateTest(TestCase):
     def setUp(self):
+        url = reverse('debates:create-debate', kwargs={'topic_id': 1})
+        self.response = self.client.get(url, follow=True)
+
         Topic.objects.create(title='Mock Topic', description='Mock description.')
 
     def test_get(self):
-        url = reverse('debates:create-debate', kwargs={'topic_id': 1})
-        response = self.client.get(url, follow=True)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     # 404 not testable with @login_required because redirection to login page returns 200 success code
     # def test_404(self):
@@ -77,71 +61,89 @@ class CreateDebateTest(TestCase):
         self.assertEquals(view.func, views.create_debate_view)
 
     def test_contains_link_to_main(self):
-        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
         linked_url = reverse('debates:main')
-        response = self.client.get(url, follow=True)
-        self.assertContains(response, f'href="{linked_url}"')
+        self.assertContains(self.response, f'href="{linked_url}"')
 
 
 class DebateTest(TestCase):
     def setUp(self):
+        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
+        self.response = self.client.get(url, follow=True)
+
         mock_topic = Topic.objects.create(title='Mock Topic', description='Mock description.')
         mock_user = User.objects.create_user(username='mock_user', email='mock@user.com', password='fakepsswrd111')
         Debate.objects.create(topic=mock_topic, starter=mock_user)
 
     def test_get(self):
-        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
-        response = self.client.get(url, follow=True)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_url_resolves_view(self):
         view = resolve('/debates/topic:1/debate:1/')
         self.assertEquals(view.func, views.debate_view)
 
+    def test_contains_link_to_main(self):
+        linked_url = reverse('debates:main')
+        self.assertContains(self.response, f'href="{linked_url}"')
+
 
 class DebatesInviteTest(TestCase):
     def setUp(self):
+        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
+        self.response = self.client.get(url, follow=True)
+
         mock_topic = Topic.objects.create(title='Mock Topic', description='Mock description.')
         mock_user = User.objects.create_user(username='mock_user', email='mock@user.com', password='fakepsswrd111')
         Debate.objects.create(topic=mock_topic, starter=mock_user)
 
     def test_get(self):
-        url = reverse('debates:invite', kwargs={'topic_id': 1, 'debate_id': 1})
-        response = self.client.get(url, follow=True)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_url_resolves_view(self):
         view = resolve('/debates/topic:1/debate:1/invite/')
         self.assertEquals(view.func, views.debates_invite_view)
 
+    def test_contains_link_to_main(self):
+        linked_url = reverse('debates:main')
+        self.assertContains(self.response, f'href="{linked_url}"')
+
 
 class UnfollowDebateTest(TestCase):
     def setUp(self):
+        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
+        self.response = self.client.get(url, follow=True)
+
         mock_topic = Topic.objects.create(title='Mock Topic', description='Mock description.')
         mock_user = User.objects.create_user(username='mock_user', email='mock@user.com', password='mockpsswrd111')
         Debate.objects.create(topic=mock_topic, starter=mock_user)
 
     def test_get(self):
-        url = reverse('debates:unfollow', kwargs={'topic_id': 1, 'debate_id': 1})
-        response = self.client.get(url, follow=True)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_url_resolves_view(self):
         view = resolve('/debates/topic:1/debate:1/unfollow/')
         self.assertEquals(view.func, views.unfollow_debate_view)
 
+    def test_contains_link_to_main(self):
+        linked_url = reverse('debates:main')
+        self.assertContains(self.response, f'href="{linked_url}"')
+
 
 class FollowDebateTest(TestCase):
     def setUp(self):
+        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
+        self.response = self.client.get(url, follow=True)
+
         mock_topic = Topic.objects.create(title='Mock Topic', description='Mock description.')
         mock_user = User.objects.create_user(username='mock_user', email='mock@user.com', password='fakepsswrd111')
         Debate.objects.create(topic=mock_topic, starter=mock_user)
 
     def test_get(self):
-        url = reverse('debates:follow', kwargs={'topic_id': 1, 'debate_id': 1})
-        response = self.client.get(url, follow=True)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_url_resolves_view(self):
         view = resolve('/debates/topic:1/debate:1/follow/')
         self.assertEquals(view.func, views.follow_debate_view)
+
+    def test_contains_link_to_main(self):
+        linked_url = reverse('debates:main')
+        self.assertContains(self.response, f'href="{linked_url}"')
