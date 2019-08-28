594eac507041af75ab43ae2766845f134077959c
Hermotimos <lukas.kozicki@gmail.com>
2019-08-28 09:01:46 +0200

    tests for homes app - developed

--- home/tests/tests.py
+++ home/tests/tests.py
@@ -6,43 +6,41 @@ from users.models import User
 
 class LinksToAppsTest(TestCase):
     def setUp(self):
-        url = reverse('home:home')
-        self.response = self.client.get(url, follow=True)
+        self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.url = reverse('home:home')
 
-        user1 = User.objects.create_user(username='user1', password='pass1111')
-        user1.profile.character_status = 'gm'
-        self.client.login(username=user1.username, password=user1.password)
+    def test_login_required(self):
+        redirect_url = reverse('users:login') + '?next=' + self.url
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_get(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.client.force_login(self.user1)
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
 
     def test_url_resolves_view(self):
         view = resolve('/')
         self.assertEquals(view.func, views.home_view)
 
     def test_contains_link_to_apps(self):
-        url_news = reverse('news:main')
-        url_rules = reverse('rules:main')
-        url_demands = reverse('contact:demands-main')
-        url_chronicle = reverse('history:chronicle-main')
-        url_timeline = reverse('history:timeline-main')
-        url_debates = reverse('debates:main')
-        url_users_profile = reverse('users:profile')
-        url_users_logout = reverse('users:logout')
-        url_demands_create = reverse('contact:demands-create')
-        url_plans = reverse('contact:plans-main')
-        url_admin = reverse('admin:index')
+        self.client.force_login(self.user1)
+        response = self.client.get(self.url)
 
-        # PASS
-        self.assertContains(self.response, f'href="{url_news}"')
-        self.assertContains(self.response, f'href="{url_rules}"')
-        self.assertContains(self.response, f'href="{url_demands}"')
-        self.assertContains(self.response, f'href="{url_chronicle}"')
-        self.assertContains(self.response, f'href="{url_timeline}"')
-        self.assertContains(self.response, f'href="{url_debates}"')
-        # FAIL
-        # self.assertContains(self.response, f'href="{url_users_profile}"')
-        # self.assertContains(self.response, f'href="{url_users_logout}"')
-        # self.assertContains(self.response, f'href="{url_demands_create}"')
-        # self.assertContains(self.response, f'href="{url_plans}"')
-        # self.assertContains(self.response, f'href="{url_admin}"')
+        # for non-game-master users:
+        self.assertContains(response, f'href="{reverse("news:main")}"')
+        self.assertContains(response, f'href="{reverse("rules:main")}"')
+        self.assertContains(response, f'href="{reverse("contact:demands-main")}"')
+        self.assertContains(response, f'href="{reverse("history:chronicle-main")}"')
+        self.assertContains(response, f'href="{reverse("history:timeline-main")}"')
+        self.assertContains(response, f'href="{reverse("debates:main")}"')
+        self.assertContains(response, f'href="{reverse("users:logout")}"')
+        self.assertContains(response, f'href="{reverse("contact:demands-create")}"')
+        self.assertContains(response, f'href="{reverse("contact:plans-main")}"')
+        self.assertContains(response, f'href="{reverse("users:profile")}"')
+
+        # for game-master users:
+        self.user1.profile.character_status = 'gm'
+        self.client.force_login(self.user1)
+        response = self.client.get(self.url)
+        self.assertContains(response, f'href="{reverse("admin:index")}"')
