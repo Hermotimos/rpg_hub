94041b6c8a4e5b26bf2000bf84483d05fbd64db9
Hermotimos <lukas.kozicki@gmail.com>
2019-08-28 12:57:33 +0200

    debates tests - simplified

--- debates/tests/tests.py
+++ debates/tests/tests.py
@@ -7,20 +7,17 @@ from users.models import User
 
 class DebatesMainTest(TestCase):
     def setUp(self):
-        # user1  - create, log in
-        user1 = User.objects.create_user(username='user1', password='pass1111')
-        self.client.force_login(user1)
+        self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.url = reverse('debates:main')
 
     def test_login_required(self):
-        self.client.logout()
-        url = reverse('debates:main')
-        redirect_url = reverse('users:login') + '?next=' + url
-        response = self.client.get(url)
+        redirect_url = reverse('users:login') + '?next=' + self.url
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_get(self):
-        url = reverse('debates:main')
-        response = self.client.get(url)
+        self.client.force_login(self.user1)
+        response = self.client.get(self.url)
         self.assertEquals(response.status_code, 200)
 
     def test_url_resolves_view(self):
@@ -30,19 +27,17 @@ class DebatesMainTest(TestCase):
 
 class CreateTopicTest(TestCase):
     def setUp(self):
-        user1 = User.objects.create_user(username='user1', password='pass1111')
-        self.client.force_login(user1)
+        self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.url = reverse('debates:create-topic')
 
     def test_login_required(self):
-        self.client.logout()
-        url = reverse('debates:create-topic')
-        redirect_url = reverse('users:login') + '?next=' + url
-        response = self.client.get(url)
+        redirect_url = reverse('users:login') + '?next=' + self.url
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_get(self):
-        url = reverse('debates:create-topic')
-        response = self.client.get(url)
+        self.client.force_login(self.user1)
+        response = self.client.get(self.url)
         self.assertEquals(response.status_code, 200)
 
     def test_url_resolves_view(self):
@@ -52,24 +47,22 @@ class CreateTopicTest(TestCase):
 
 class CreateDebateTest(TestCase):
     def setUp(self):
-        # user1  - create, log in and add to Debate.allowed_profiles
-        user1 = User.objects.create_user(username='user1', password='pass1111')
-        self.client.force_login(user1)
-        topic1 = Topic.objects.create(id=1)
-        debate1 = Debate.objects.create(id=1, topic=topic1, starter=user1)
-        debate1.allowed_profiles.set([user1.profile, ])
+        self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.topic1 = Topic.objects.create(id=1, title='Topic1', description='Description1')
+        self.debate1 = Debate.objects.create(id=1, topic=self.topic1, starter=self.user1)
+        self.debate1.allowed_profiles.set([self.user1.profile, ])
+        self.url = reverse('debates:create-debate', kwargs={'topic_id': self.topic1.id})
         # testing if user1 is added to allowed
         # self.assertTrue(self.user1.profile in self.debate1.allowed_profiles.all())
         # self.assertTrue(self.user1.profile in self.topic1.allowed_list())
 
     def test_login_required(self):
-        self.client.logout()
-        url = reverse('debates:create-debate', kwargs={'topic_id': 1})
-        redirect_url = reverse('users:login') + '?next=' + url
-        response = self.client.get(url)
+        redirect_url = reverse('users:login') + '?next=' + self.url
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_404(self):
+        self.client.force_login(self.user1)
         url = reverse('debates:create-debate', kwargs={'topic_id': 999})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 404)
@@ -77,18 +70,17 @@ class CreateDebateTest(TestCase):
     def test_redirect_if_unallowed(self):
         user2 = User.objects.create_user(username='user2', password='pass1111')
         self.client.force_login(user2)
-        url = reverse('debates:create-debate', kwargs={'topic_id': 1})
         redirect_url = reverse('home:dupa')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_get(self):
-        url = reverse('debates:create-debate', kwargs={'topic_id': 1})
-        response = self.client.get(url)
+        self.client.force_login(self.user1)
+        response = self.client.get(self.url)
         self.assertEquals(response.status_code, 200)
 
     def test_url_resolves_view(self):
-        view = resolve('/debates/topic:1/create-debate/')
+        view = resolve(f'/debates/topic:{self.topic1.id}/create-debate/')
         self.assertEquals(view.func, views.create_debate_view)
 
     # ensures other test don't base on objects created in this one: variables in setUp have to begin with 'self. '
@@ -103,157 +95,149 @@ class CreateDebateTest(TestCase):
 
 class DebateTest(TestCase):
     def setUp(self):
-        user1 = User.objects.create_user(username='user1', password='pass1111')
-        self.client.force_login(user1)
-        topic1 = Topic.objects.create(id=1, title='Topic1', description='Description1')
-        debate1 = Debate.objects.create(id=1, topic=topic1, starter=user1)
-        debate1.allowed_profiles.set([user1.profile, ])
+        self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.topic1 = Topic.objects.create(id=1, title='Topic1', description='Description1')
+        self.debate1 = Debate.objects.create(id=1, topic=self.topic1, starter=self.user1)
+        self.debate1.allowed_profiles.set([self.user1.profile, ])
+        self.url = reverse('debates:debate', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
 
     def test_login_required(self):
-        self.client.logout()
-        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
-        redirect_url = reverse('users:login') + '?next=' + url
-        response = self.client.get(url)
+        redirect_url = reverse('users:login') + '?next=' + self.url
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_redirect_if_unallowed(self):
         user2 = User.objects.create_user(username='user2', password='pass1111')
         self.client.force_login(user2)
-        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
         redirect_url = reverse('home:dupa')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_404(self):
-        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 999})
+        self.client.force_login(self.user1)
+        url = reverse('debates:debate', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id + 999})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 404)
 
     def test_get(self):
-        url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
-        self.response = self.client.get(url)
+        self.client.force_login(self.user1)
+        self.response = self.client.get(self.url)
         self.assertEquals(self.response.status_code, 200)
 
     def test_url_resolves_view(self):
-        view = resolve('/debates/topic:1/debate:1/')
+        view = resolve(f'/debates/topic:{self.topic1.id}/debate:{self.debate1.id}/')
         self.assertEquals(view.func, views.debate_view)
 
 
 class DebatesInviteTest(TestCase):
     def setUp(self):
-        user1 = User.objects.create_user(username='user1', password='pass1111')
-        self.client.force_login(user1)
-        topic1 = Topic.objects.create(id=1, title='Topic1', description='Description1')
-        debate1 = Debate.objects.create(id=1, topic=topic1, starter=user1)
-        debate1.allowed_profiles.set([user1.profile, ])
+        self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.topic1 = Topic.objects.create(id=1, title='Topic1', description='Description1')
+        self.debate1 = Debate.objects.create(id=1, topic=self.topic1, starter=self.user1)
+        self.debate1.allowed_profiles.set([self.user1.profile, ])
+        self.url = reverse('debates:invite', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
 
     def test_login_required(self):
-        self.client.logout()
-        url = reverse('debates:invite', kwargs={'topic_id': 1, 'debate_id': 1})
-        redirect_url = reverse('users:login') + '?next=' + url
-        response = self.client.get(url)
+        redirect_url = reverse('users:login') + '?next=' + self.url
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_redirect_if_unallowed(self):
         user2 = User.objects.create_user(username='user2', password='pass1111')
         self.client.force_login(user2)
-        url = reverse('debates:invite', kwargs={'topic_id': 1, 'debate_id': 1})
         redirect_url = reverse('home:dupa')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_404(self):
-        url = reverse('debates:invite', kwargs={'topic_id': 1, 'debate_id': 999})
+        self.client.force_login(self.user1)
+        url = reverse('debates:invite', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id + 999})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 404)
 
     def test_get(self):
-        url = reverse('debates:invite', kwargs={'topic_id': 1, 'debate_id': 1})
-        self.response = self.client.get(url)
+        self.client.force_login(self.user1)
+        self.response = self.client.get(self.url)
         self.assertEquals(self.response.status_code, 200)
 
     def test_url_resolves_view(self):
-        view = resolve('/debates/topic:1/debate:1/invite/')
+        view = resolve(f'/debates/topic:{self.topic1.id}/debate:{self.debate1.id}/invite/')
         self.assertEquals(view.func, views.debates_invite_view)
 
 
 class UnfollowDebateTest(TestCase):
     def setUp(self):
-        user1 = User.objects.create_user(username='user1', password='pass1111')
-        self.client.force_login(user1)
-        topic1 = Topic.objects.create(id=1, title='Topic1', description='Description1')
-        debate1 = Debate.objects.create(id=1, topic=topic1, starter=user1)
-        debate1.allowed_profiles.set([user1.profile, ])
+        self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.topic1 = Topic.objects.create(id=1, title='Topic1', description='Description1')
+        self.debate1 = Debate.objects.create(id=1, topic=self.topic1, starter=self.user1)
+        self.debate1.allowed_profiles.set([self.user1.profile, ])
+        self.url = reverse('debates:unfollow', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
 
     def test_login_required(self):
-        self.client.logout()
-        url = reverse('debates:unfollow', kwargs={'topic_id': 1, 'debate_id': 1})
-        redirect_url = reverse('users:login') + '?next=' + url
-        response = self.client.get(url)
+        redirect_url = reverse('users:login') + '?next=' + self.url
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_redirect_if_unallowed(self):
         user2 = User.objects.create_user(username='user2', password='pass1111')
         self.client.force_login(user2)
-        url = reverse('debates:unfollow', kwargs={'topic_id': 1, 'debate_id': 1})
         redirect_url = reverse('home:dupa')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_404(self):
-        url = reverse('debates:unfollow', kwargs={'topic_id': 1, 'debate_id': 999})
+        self.client.force_login(self.user1)
+        url = reverse('debates:follow', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id + 999})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 404)
 
     def test_redirect_if_allowed(self):
-        url = reverse('debates:unfollow', kwargs={'topic_id': 1, 'debate_id': 1})
-        redirect_url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
-        response = self.client.get(url)
+        self.client.force_login(self.user1)
+        redirect_url = reverse('debates:debate', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     # No test_get - no template generated by unfollow_debate_view()
 
     def test_url_resolves_view(self):
-        view = resolve('/debates/topic:1/debate:1/unfollow/')
+        view = resolve(f'/debates/topic:{self.topic1.id}/debate:{self.debate1.id}/unfollow/')
         self.assertEquals(view.func, views.unfollow_debate_view)
 
 
 class FollowDebateTest(TestCase):
     def setUp(self):
-        user1 = User.objects.create_user(username='user1', password='pass1111')
-        self.client.force_login(user1)
-        topic1 = Topic.objects.create(id=1, title='Topic1', description='Description1')
-        debate1 = Debate.objects.create(id=1, topic=topic1, starter=user1)
-        debate1.allowed_profiles.set([user1.profile, ])
+        self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.topic1 = Topic.objects.create(id=1, title='Topic1', description='Description1')
+        self.debate1 = Debate.objects.create(id=1, topic=self.topic1, starter=self.user1)
+        self.debate1.allowed_profiles.set([self.user1.profile, ])
+        self.url = reverse('debates:follow', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
 
     def test_login_required(self):
-        self.client.logout()
-        url = reverse('debates:follow', kwargs={'topic_id': 1, 'debate_id': 1})
-        redirect_url = reverse('users:login') + '?next=' + url
-        response = self.client.get(url)
+        redirect_url = reverse('users:login') + '?next=' + self.url
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_redirect_if_unallowed(self):
         user2 = User.objects.create_user(username='user2', password='pass1111')
         self.client.force_login(user2)
-        url = reverse('debates:follow', kwargs={'topic_id': 1, 'debate_id': 1})
         redirect_url = reverse('home:dupa')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_404(self):
-        url = reverse('debates:follow', kwargs={'topic_id': 1, 'debate_id': 999})
+        self.client.force_login(self.user1)
+        url = reverse('debates:follow', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id + 999})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 404)
 
     def test_redirect_if_allowed(self):
-        url = reverse('debates:follow', kwargs={'topic_id': 1, 'debate_id': 1})
-        redirect_url = reverse('debates:debate', kwargs={'topic_id': 1, 'debate_id': 1})
-        response = self.client.get(url)
+        self.client.force_login(self.user1)
+        redirect_url = reverse('debates:debate', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
+        response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     # No test_get - no template generated by follow_debate_view()
 
     def test_url_resolves_view(self):
-        view = resolve('/debates/topic:1/debate:1/follow/')
+        view = resolve(f'/debates/topic:{self.topic1.id}/debate:{self.debate1.id}/follow/')
         self.assertEquals(view.func, views.follow_debate_view)
