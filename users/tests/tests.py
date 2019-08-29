609f861f3f0e105a9c2476dba19c356fb8443f08
Hermotimos <lukas.kozicki@gmail.com>
2019-08-29 17:38:54 +0200

    test links in users app done

--- users/tests/tests.py
+++ users/tests/tests.py
@@ -5,18 +5,28 @@ from users.models import User
 
 
 class TestLogin(TestCase):
-    def test_get(self):
+    def setUp(self):
         self.url = reverse('users:login')
-        response = self.client.get(self.url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(self.url)
+
+    def test_get(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_url_resolves_view(self):
         view = resolve('/users/login/')
         # for class-based views add .view_class cause url ClassBasedView.as_views() creates new function
         self.assertEquals(view.func.view_class, views.LoginView)
 
+    def test_contains_links(self):
+        linked_url = reverse('users:register')
+        self.assertContains(self.response, f'href="{linked_url}"')
+
 
 class TestRegister(TestCase):
+    def setUp(self):
+        self.url = reverse('users:register')
+        self.response = self.client.get(self.url)
+
     def test_get(self):
         url = reverse('users:register')
         response = self.client.get(url)
@@ -25,6 +35,10 @@ class TestRegister(TestCase):
     def test_url_resolves_view(self):
         view = resolve('/users/register/')
         self.assertEquals(view.func, views.register_view)
+
+    def test_contains_links(self):
+        linked_url = reverse('users:login')
+        self.assertContains(self.response, f'href="{linked_url}"')
 
 
 class TestLogout(TestCase):
@@ -62,6 +76,12 @@ class TestProfile(TestCase):
     def test_url_resolves_view(self):
         view = resolve('/users/profile/')
         self.assertEquals(view.func, views.profile_view)
+
+    def test_contains_links(self):
+        self.client.force_login(self.user1)
+        linked_url = reverse('users:change-password')
+        response = self.client.get(self.url)
+        self.assertContains(response, f'href="{linked_url}"')
 
 
 class TestChangePassword(TestCase):
