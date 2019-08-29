117ec715e0e9bc19f2af3012a2953e1782c38879
Hermotimos <lukas.kozicki@gmail.com>
2019-08-29 09:47:05 +0200

    tests for history app (timeline) developed

--- history/tests/tests.py
+++ history/tests/tests.py
@@ -412,11 +412,9 @@ class TimelineParticipantTest(TestCase):
         self.user3 = User.objects.create_user(username='user3', password='pass1111')
         self.user4 = User.objects.create_user(username='user4', password='pass1111')
         game1 = GameSession.objects.create(game_no=1, title='Game1')
-        # thread1 = Thread.objects.create(name='Thread1')
         gen_loc1 = GeneralLocation.objects.create(name='gen_loc1')
         self.event1 = TimelineEvent.objects.create(game_no=game1, year=1, season=1, day_start=1,
                                                    description='Description1', general_location=gen_loc1)
-        # self.event1.threads.set([thread1, ])
         self.event1.participants.set([self.user1.profile, self.user4.profile])
         self.event1.informed.set([self.user2.profile, ])
         self.url = reverse('history:timeline-participant', kwargs={'participant_id': self.user4.id})
@@ -468,10 +466,8 @@ class TimelineGeneralLocationtTest(TestCase):
         self.user3 = User.objects.create_user(username='user3', password='pass1111')
         game1 = GameSession.objects.create(game_no=1, title='Game1')
         self.gen_loc1 = GeneralLocation.objects.create(name='gen_loc1')
-        # thread1 = Thread.objects.create(name='Thread1')
         event1 = TimelineEvent.objects.create(game_no=game1, year=1, season=1, day_start=1,
                                               description='Description1', general_location=self.gen_loc1)
-        # event1.threads.set([thread1, ])
         event1.participants.set([self.user1.profile, ])
         event1.informed.set([self.user2.profile, ])
         self.url = reverse('history:timeline-gen-loc', kwargs={'gen_loc_id': self.gen_loc1.id})
@@ -518,10 +514,8 @@ class TimelineSpecificLocationtTest(TestCase):
         game1 = GameSession.objects.create(game_no=1, title='Game1')
         gen_loc1 = GeneralLocation.objects.create(name='gen_loc1')
         self.spec_loc1 = SpecificLocation.objects.create(name='spec_loc1', general_location=gen_loc1)
-        # thread1 = Thread.objects.create(name='Thread1')
         event1 = TimelineEvent.objects.create(game_no=game1, year=1, season=1, day_start=1,
                                               description='Description1', general_location=gen_loc1)
-        # event1.threads.set([thread1, ])
         event1.participants.set([self.user1.profile, ])
         event1.informed.set([self.user2.profile, ])
         event1.specific_locations.set([self.spec_loc1, ])
@@ -568,20 +562,55 @@ class TimelineSpecificLocationtTest(TestCase):
 
 class TimelineDateTest(TestCase):
     def setUp(self):
+        self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.user2 = User.objects.create_user(username='user2', password='pass1111')
+        self.user3 = User.objects.create_user(username='user3', password='pass1111')
         game1 = GameSession.objects.create(game_no=1, title='Game1')
-        TimelineEvent.objects.create(game_no=game1, year=1, season=1, day_start=1, description='Description1')
+        gen_loc1 = GeneralLocation.objects.create(name='gen_loc1')
+        self.spec_loc1 = SpecificLocation.objects.create(name='spec_loc1', general_location=gen_loc1)
+        # thread1 = Thread.objects.create(name='Thread1')
+        event1 = TimelineEvent.objects.create(game_no=game1, year=1, season=1, day_start=1,
+                                              description='Description1', general_location=gen_loc1)
+        # event1.threads.set([thread1, ])
+        event1.participants.set([self.user1.profile, ])
+        event1.informed.set([self.user2.profile, ])
+        event1.specific_locations.set([self.spec_loc1, ])
 
-    def test_get(self):
-        url_only_year = reverse('history:timeline-date', kwargs={'year': 1, 'season': 0})
-        url_year_and_season = reverse('history:timeline-date', kwargs={'year': 1, 'season': 1})
-        response_only_year = self.client.get(url_only_year, follow=True)
-        response_year_and_season = self.client.get(url_year_and_season, follow=True)
+        self.url_only_year = reverse('history:timeline-date', kwargs={'year': 1, 'season': 0})
+        self.url_year_and_season = reverse('history:timeline-date', kwargs={'year': 1, 'season': 1})
+
+    def test_login_required(self):
+        redirect_url = reverse('users:login') + '?next=' + self.url_only_year
+        response = self.client.get(self.url_only_year)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_redirect_if_unallowed(self):
+        self.client.force_login(self.user3)
+        redirect_url = reverse('home:dupa')
+        response = self.client.get(self.url_only_year)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    # no test_404() becaues no objects timeline_date_view() has no args and calls no object with get_object_or_404()
+
+    def test_get_1(self):
+        self.client.force_login(self.user1)
+        response_only_year = self.client.get(self.url_only_year)
+        response_year_and_season = self.client.get(self.url_year_and_season)
+        self.assertEquals(response_only_year.status_code, 200)
+        self.assertEquals(response_year_and_season.status_code, 200)
+
+    def test_get_2(self):
+        self.client.force_login(self.user2)
+        response_only_year = self.client.get(self.url_only_year)
+        response_year_and_season = self.client.get(self.url_year_and_season)
         self.assertEquals(response_only_year.status_code, 200)
         self.assertEquals(response_year_and_season.status_code, 200)
 
     def test_url_resolves_view(self):
-        view = resolve('/history/timeline/date:1:1/')
-        self.assertEquals(view.func, views.timeline_date_view)
+        view_only_year = resolve('/history/timeline/date:1:0/')
+        view_year_and_season = resolve('/history/timeline/date:1:1/')
+        self.assertEquals(view_only_year.func, views.timeline_date_view)
+        self.assertEquals(view_year_and_season.func, views.timeline_date_view)
 
 
 class TimelineGameTest(TestCase):
