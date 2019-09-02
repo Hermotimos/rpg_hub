fa5ab977b05c9d8ed8f90a8b460226bae2f80ee7
Hermotimos <lukas.kozicki@gmail.com>
2019-09-02 12:02:50 +0200

    debates app - tests developed

--- debates/tests/tests.py
+++ debates/tests/tests.py
@@ -8,6 +8,7 @@ from users.models import User
 class DebatesMainTest(TestCase):
     def setUp(self):
         self.user1 = User.objects.create_user(username='user1', password='pass1111')
+        self.user2 = User.objects.create_user(username='user2', password='pass1111')
         self.topic1 = Topic.objects.create(id=1, title='Topic1')
         self.debate1 = Debate.objects.create(id=1, name='Debate1', topic=self.topic1, starter=self.user1)
         self.debate1.allowed_profiles.set([self.user1.profile, ])
@@ -27,18 +28,24 @@ class DebatesMainTest(TestCase):
         view = resolve('/debates/')
         self.assertEquals(view.func, views.debates_main_view)
 
-    def test_contains_links(self):
+    def test_links(self):
+        linked_url1 = reverse('debates:create-topic')
+        linked_url2 = reverse('debates:create-debate', kwargs={'topic_id': self.topic1.id})
+        linked_url3 = reverse('debates:debate', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
+
+        # case request.user.profile  in debate1.allowed_profiles
         self.client.force_login(self.user1)
         response = self.client.get(self.url)
+        self.assertContains(response, f'href="{linked_url1}"')
+        self.assertContains(response, f'href="{linked_url2}"')
+        self.assertContains(response, f'href="{linked_url3}"')
 
-        linked_url = reverse('debates:create-topic')
-        self.assertContains(response, f'href="{linked_url}"')
-
-        linked_url = reverse('debates:create-debate', kwargs={'topic_id': self.topic1.id})
-        self.assertContains(response, f'href="{linked_url}"')
-
-        linked_url = reverse('debates:debate', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
-        self.assertContains(response, f'href="{linked_url}"')
+        # case request.user.profile not in debate1.allowed_profiles
+        self.client.force_login(self.user2)
+        response = self.client.get(self.url)
+        self.assertContains(response, f'href="{linked_url1}"')
+        self.assertNotContains(response, f'href="{linked_url2}"')
+        self.assertNotContains(response, f'href="{linked_url3}"')
 
 
 class CreateTopicTest(TestCase):
@@ -68,9 +75,6 @@ class CreateDebateTest(TestCase):
         self.debate1 = Debate.objects.create(id=1, topic=self.topic1, starter=self.user1)
         self.debate1.allowed_profiles.set([self.user1.profile, ])
         self.url = reverse('debates:create-debate', kwargs={'topic_id': self.topic1.id})
-        # testing if user1 is added to allowed
-        # self.assertTrue(self.user1.profile in self.debate1.allowed_profiles.all())
-        # self.assertTrue(self.user1.profile in self.topic1.allowed_list())
 
     def test_login_required(self):
         redirect_url = reverse('users:login') + '?next=' + self.url
@@ -98,15 +102,6 @@ class CreateDebateTest(TestCase):
     def test_url_resolves_view(self):
         view = resolve(f'/debates/topic:{self.topic1.id}/create-debate/')
         self.assertEquals(view.func, views.create_debate_view)
-
-    # ensures other test don't base on objects created in this one: variables in setUp have to begin with 'self. '
-    # def clean_up(self):
-    #     self.user1.delete()
-    #     self.t1opic.delete()
-    #     self.debate1.delete()
-    #     self.assertFalse(User.objects.all())
-    #     self.assertFalse(Topic.objects.all())
-    #     self.assertFalse(Debate.objects.all())
 
 
 class DebateTest(TestCase):
@@ -146,22 +141,24 @@ class DebateTest(TestCase):
         view = resolve(f'/debates/topic:{self.topic1.id}/debate:{self.debate1.id}/')
         self.assertEquals(view.func, views.debate_view)
 
-    def test_contains_links(self):
+    def test_links(self):
+        linked_url1 = reverse('debates:invite', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
+        linked_url2 = reverse('debates:unfollow', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
+        linked_url3 = reverse('debates:follow', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
+
+        # case request.user.profile in debate1.followers
         self.client.force_login(self.user1)
         response = self.client.get(self.url)
+        self.assertContains(response, f'href="{linked_url1}"')
+        self.assertContains(response, f'href="{linked_url2}"')
+        self.assertNotContains(response, f'href="{linked_url3}"')
 
-        linked_url = reverse('debates:invite', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
-        self.assertContains(response, f'href="{linked_url}"')
-
-        # case request.user.profile in debate1.followers.all()
-        linked_url = reverse('debates:unfollow', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
-        self.assertContains(response, f'href="{linked_url}"')
-
-        # case request.user.profile not in debate1.followers.all()
+        # case request.user.profile not in debate1.followers
         self.client.force_login(self.user2)
         response = self.client.get(self.url)
-        linked_url = reverse('debates:follow', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
-        self.assertContains(response, f'href="{linked_url}"')
+        self.assertContains(response, f'href="{linked_url1}"')
+        self.assertNotContains(response, f'href="{linked_url2}"')
+        self.assertContains(response, f'href="{linked_url3}"')
 
 
 class DebatesInviteTest(TestCase):
