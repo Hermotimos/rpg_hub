0b22cb3f6ed952a373213b367ba1de4b125bb751
Hermotimos <lukas.kozicki@gmail.com>
2019-09-05 09:56:57 +0200

    rules models Skill and Synergy - model and admin developed

--- rules/admin.py
+++ rules/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.forms import Textarea
+from django.db import models
+from rules.models import Skill, Synergy
 
-# Register your models here.
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'tested_trait', 'description', 'lvl0_desc', 'lvl1_desc', 'lvl2_desc', 'lvl3_desc',
+                    'image']
+    list_editable = ['name', 'description', 'tested_trait', 'lvl0_desc', 'lvl1_desc', 'lvl2_desc', 'lvl3_desc', 'image']
+
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 8})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 30})},
+    }
+
+
+class SynergyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'lvl1_desc', 'lvl2_desc', 'lvl3_desc',
+                    'image']
+    list_editable = ['description', 'lvl1_desc', 'lvl2_desc', 'lvl3_desc', 'image']
+
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 8})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 30})},
+    }
+
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Synergy, SynergyAdmin)
--- rules/models.py
+++ rules/models.py
@@ -3,15 +3,18 @@ from users.models import Profile
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='Nazwa umiejętności')
-    description = models.CharField(max_length=100, verbose_name='Opis umiejętności', blank=True, null=True)
-    tested_trait = models.CharField(max_length=50, verbose_name='Cecha testowana')
-    lvl0_desc = models.TextField(max_length=4000, blank=True, null=True)
-    lvl1_desc = models.TextField(max_length=4000, blank=True, null=True)
-    lvl2_desc = models.TextField(max_length=4000, blank=True, null=True)
-    lvl3_desc = models.TextField(max_length=4000, blank=True, null=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Umiejętność')
+    tested_trait = models.CharField(max_length=50, verbose_name='Cecha/Cechy')
+    description = models.TextField(max_length=1000, blank=True, null=True, verbose_name='Opis umiejętności')
+    lvl0_desc = models.TextField(max_length=4000, blank=True, null=True, verbose_name='0')
+    lvl1_desc = models.TextField(max_length=4000, blank=True, null=True, verbose_name='1')
+    lvl2_desc = models.TextField(max_length=4000, blank=True, null=True, verbose_name='2')
+    lvl3_desc = models.TextField(max_length=4000, blank=True, null=True, verbose_name='3')
     image = models.ImageField(blank=True, null=True, upload_to='site_features_pics')
-    allowed_profiles = models.ManyToManyField(to=Profile, related_name='allowed_skills')
+    allowed_profiles = models.ManyToManyField(to=Profile, related_name='allowed_skills', blank=True)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
@@ -19,8 +22,19 @@ class Skill(models.Model):
 
 class Synergy(models.Model):
     skills = models.ManyToManyField(Skill, related_name='skills')
-    description = models.CharField(max_length=100, verbose_name='Opis umiejętności', blank=True, null=True)
-    lvl1_desc = models.TextField(max_length=4000, blank=True, null=True)
-    lvl2_desc = models.TextField(max_length=4000, blank=True, null=True)
-    lvl3_desc = models.TextField(max_length=4000, blank=True, null=True)
-    allowed_profiles = models.ManyToManyField(to=Profile, related_name='allowed_synergies')
+    description = description = models.TextField(max_length=1000, blank=True, null=True, verbose_name='Opis synergii')
+    lvl1_desc = models.TextField(max_length=4000, blank=True, null=True, verbose_name='1')
+    lvl2_desc = models.TextField(max_length=4000, blank=True, null=True, verbose_name='2')
+    lvl3_desc = models.TextField(max_length=4000, blank=True, null=True, verbose_name='3')
+    image = models.ImageField(blank=True, null=True, upload_to='site_features_pics')
+    allowed_profiles = models.ManyToManyField(to=Profile, related_name='allowed_synergies', blank=True)
+
+    def __str_(self):
+        return ' + '.join(s.name for s in self.skills.all())
+
+    def name(self):
+        return self.__str_()
+
+    class Meta:
+        verbose_name = 'Synergy'
+        verbose_name_plural = 'Synergies'
