a3482b0212e408b185e460b31e04b22bc408e642
Hermotimos <lukas.kozicki@gmail.com>
2019-09-07 22:58:40 +0200

    minor

--- rules/html2pdf_views.py
+++ rules/html2pdf_views.py
@@ -1,3 +1,6 @@
+#! /usr/bin/python
+# -*- encoding: utf-8 -*-
+
 from django.http import HttpResponse
 from django.template.loader import get_template
 from rules.utils import render_to_pdf, link_callback
