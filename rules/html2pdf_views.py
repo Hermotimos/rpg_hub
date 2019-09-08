2bf1552f2ba77d051b449c9c8ad875eca40ce43e
Hermotimos <lukas.kozicki@gmail.com>
2019-09-08 08:59:43 +0200

    failed attemts at generating pdfs

--- rules/html2pdf_views.py
+++ rules/html2pdf_views.py
@@ -3,13 +3,10 @@
 
 from django.http import HttpResponse
 from django.template.loader import get_template
-from rules.utils import render_to_pdf, link_callback
-from django.template import Context
+from rules.utils import render_to_pdf
 from xhtml2pdf import pisa
 from io import BytesIO, StringIO
 from django.conf import settings
-
-import os
 
 
 def generate_pdf_view(request, *args, **kwargs):
@@ -28,23 +25,24 @@ def generate_pdf_view(request, *args, **kwargs):
     HttpResponse('Wystąpił problem...')
 
 
-def render_pdf_view(request):
+def generate_pdf_view_2(request):
+    template = get_template('rules/combat.html')
     context = {
         'page_title': 'Przebieg walki'
     }
+    html = template.render(context)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(StringIO(html), result)
+    # other options, work the same, supposedly serve fonts but they don't:
+    # pdf = pisa.pisaDocument(StringIO(html), result, encoding='UTF-8')
+    # pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result, encoding='UTF-8')
+    # pdf = pisa.pisaDocument(StringIO(html), result, path=os.path.join(settings.STATIC_ROOT, '/fonts/'))
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return HttpResponse('We had some errors<pre>%s</pre>' % html.escape(html))
 
-    # Create a Django response object, and specify content_type as pdf
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
 
-    # find the template and render it.
-    template = get_template('rules/combat.html')
-    html = template.render(Context(context))
 
-    # create a pdf
-    pisaStatus = pisa.CreatePDF(html, dest=response, link_callback=link_callback)
 
-    # if error then show some funy view
-    if pisaStatus.err:
-        return HttpResponse('We had some errors <pre>' + html + '</pre>')
-    return response
+
+
--- rules/urls.py
+++ rules/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('skills/', views.rules_skills_view, name='skills'),
     path('combat/', views.rules_combat_view, name='combat'),
 
-    # path('combat/pdf/', html2pdf_views.html_to_pdf_directly, name='combat-pdf'),
-    path('combat/pdf2/', html2pdf_views.render_pdf_view, name='combat-pdf2'),
+    path('combat/pdf/', html2pdf_views.generate_pdf_view, name='combat-pdf'),
+    path('combat/pdf2/', html2pdf_views.generate_pdf_view_2, name='combat-pdf2'),
 
 ]
