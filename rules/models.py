48c643985b989e67565c306cc7175de74494cad7
Hermotimos <lukas.kozicki@gmail.com>
2019-09-10 06:53:58 +0200

    models in rules app modified

--- rules/models.py
+++ rules/models.py
@@ -6,11 +6,11 @@ from users.models import Profile
 class Skill(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Umiejętność')
     tested_trait = models.CharField(max_length=50, verbose_name='Cecha/Cechy')
-    description = models.TextField(max_length=1000, blank=True, null=True, verbose_name='Opis umiejętności')
-    lvl_0 = models.TextField(max_length=4000, blank=True, null=True, verbose_name='0')
-    lvl_1 = models.TextField(max_length=4000, blank=True, null=True, verbose_name='1')
-    lvl_2 = models.TextField(max_length=4000, blank=True, null=True, verbose_name='2')
-    lvl_3 = models.TextField(max_length=4000, blank=True, null=True, verbose_name='3')
+    description = models.TextField(max_length=1000, blank=True, null=True, verbose_name='Opis')
+    lvl_0 = models.TextField(max_length=4000, blank=True, null=True)
+    lvl_1 = models.TextField(max_length=4000, blank=True, null=True)
+    lvl_2 = models.TextField(max_length=4000, blank=True, null=True)
+    lvl_3 = models.TextField(max_length=4000, blank=True, null=True)
     image = models.ImageField(blank=True, null=True, upload_to='site_features_pics')
     allowed_profiles = models.ManyToManyField(Profile,
                                               blank=True,
@@ -18,8 +18,7 @@ class Skill(models.Model):
                                               Q(character_status='active_player') |
                                               Q(character_status='inactive_player') |
                                               Q(character_status='dead_player'),
-                                              related_name='allowed_skills',
-                                              )
+                                              related_name='allowed_skills')
 
     class Meta:
         ordering = ['name']
@@ -33,17 +32,16 @@ class Skill(models.Model):
 
 class Synergy(models.Model):
     skills = models.ManyToManyField(Skill, related_name='skills')
-    lvl_1 = models.TextField(max_length=4000, blank=True, null=True, verbose_name='1')
-    lvl_2 = models.TextField(max_length=4000, blank=True, null=True, verbose_name='2')
-    lvl_3 = models.TextField(max_length=4000, blank=True, null=True, verbose_name='3')
+    lvl_1 = models.TextField(max_length=4000, blank=True, null=True)
+    lvl_2 = models.TextField(max_length=4000, blank=True, null=True)
+    lvl_3 = models.TextField(max_length=4000, blank=True, null=True)
     allowed_profiles = models.ManyToManyField(Profile,
                                               blank=True,
                                               limit_choices_to=
                                               Q(character_status='active_player') |
                                               Q(character_status='inactive_player') |
                                               Q(character_status='dead_player'),
-                                              related_name='allowed_synergies',
-                                              )
+                                              related_name='allowed_synergies',)
 
     def __str_(self):
         return ' + '.join(s.name for s in self.skills.all())
@@ -60,8 +58,8 @@ class Synergy(models.Model):
 
 
 class CharacterClass(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='Klasa')
-    description = models.TextField(max_length=4000, default='')
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=4000, blank=True, null=True)
 
     class Meta:
         ordering = ['name']
@@ -74,30 +72,37 @@ class CharacterClass(models.Model):
 
 
 class CharacterProfession(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='Profesja')
+    name = models.CharField(max_length=100, unique=True)
     character_class = models.ForeignKey(CharacterClass, related_name='professions', on_delete=models.PROTECT)
-    description = models.TextField(max_length=4000, default='')
-    start_perks = models.TextField(max_length=4000, default='')
-    lvl_1 = models.CharField(max_length=500, default='')
-    lvl_2 = models.CharField(max_length=500, default='')
-    lvl_3 = models.CharField(max_length=500, default='')
-    lvl_4 = models.CharField(max_length=500, default='')
-    lvl_5 = models.CharField(max_length=500, default='')
-    lvl_6 = models.CharField(max_length=500, default='')
-    lvl_7 = models.CharField(max_length=500, default='')
-    lvl_8 = models.CharField(max_length=500, default='')
-    lvl_9 = models.CharField(max_length=500, default='')
-    lvl_10 = models.CharField(max_length=500, default='')
-    lvl_11 = models.CharField(max_length=500, default='')
-    lvl_12 = models.CharField(max_length=500, default='')
-    lvl_13 = models.CharField(max_length=500, default='')
-    lvl_14 = models.CharField(max_length=500, default='')
-    lvl_15 = models.CharField(max_length=500, default='')
-    lvl_16 = models.CharField(max_length=500, default='')
-    lvl_17 = models.CharField(max_length=500, default='')
-    lvl_18 = models.CharField(max_length=500, default='')
-    lvl_19 = models.CharField(max_length=500, default='')
-    lvl_20 = models.CharField(max_length=500, default='')
+    description = models.TextField(max_length=4000, blank=True, null=True)
+    start_perks = models.TextField(max_length=4000, blank=True, null=True)
+    lvl_1 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_2 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_3 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_4 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_5 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_6 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_7 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_8 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_9 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_10 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_11 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_12 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_13 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_14 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_15 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_16 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_17 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_18 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_19 = models.CharField(max_length=500, blank=True, null=True)
+    lvl_20 = models.CharField(max_length=500, blank=True, null=True)
+    allowed_profiles = models.ManyToManyField(Profile,
+                                              blank=True,
+                                              limit_choices_to=
+                                              Q(character_status='active_player') |
+                                              Q(character_status='inactive_player') |
+                                              Q(character_status='dead_player'),
+                                              related_name='allowed_professions')
 
     class Meta:
         ordering = ['name']
