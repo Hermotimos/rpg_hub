a8faae04a6acb643e8c4321dee72ab8189b07616
Hermotimos <lukas.kozicki@gmail.com>
2019-09-12 20:26:50 +0200

    minor

--- rules/admin.py
+++ rules/admin.py
@@ -9,7 +9,7 @@ class SkillAdmin(admin.ModelAdmin):
     list_editable = ['name', 'description', 'tested_trait', 'lvl_0', 'lvl_1', 'lvl_2', 'lvl_3', 'image']
 
     formfield_overrides = {
-        models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 8})},
+        models.CharField: {'widget': Textarea(attrs={'rows': 2, 'cols': 10})},
         models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 30})},
     }
 
@@ -19,7 +19,7 @@ class SynergyAdmin(admin.ModelAdmin):
     list_editable = ['name', 'lvl_1', 'lvl_2', 'lvl_3']
 
     formfield_overrides = {
-        models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 8})},
+        models.CharField: {'widget': Textarea(attrs={'rows': 2, 'cols': 10})},
         models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 30})},
     }
 
