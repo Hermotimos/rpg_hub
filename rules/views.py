a81c01ba71578195fab26a09ccb7a6c4e63ded10
Hermotimos <lukas.kozicki@gmail.com>
2019-09-16 18:31:29 +0200

    elite professions developed in rules

--- rules/views.py
+++ rules/views.py
@@ -83,17 +83,22 @@ def rules_professions_view(request):
     if request.user.profile.character_status == 'gm':
         classes = list(CharacterClass.objects.all())
         classes_with_professions_dict = {c: [p for p in c.professions.all()] for c in classes}
-        elite_professions = list(EliteClass.objects.all())
+        elite_classes = list(EliteClass.objects.all())
+        elite_classes_with_professions_dict = {ec: [ep for ep in ec.elite_professions.all()] for ec in elite_classes}
     else:
         classes = list(c for c in CharacterClass.objects.all() if request.user.profile in c.allowed_list())
         classes_with_professions_dict = \
             {c: [p for p in c.professions.all() if request.user.profile in p.allowed_profiles.all()] for c in classes}
-        elite_professions = list(p for p in EliteClass.objects.all() if request.user.profile in p.allowed_list())
+        elite_classes = list(ec for ec in EliteClass.objects.all() if request.user.profile in ec.allowed_list())
+        elite_classes_with_professions_dict = \
+            {ec: [ep for ep in ec.elite_professions.all() if request.user.profile in ep.allowed_profiles.all()]
+             for ec in elite_classes}
 
     context = {
         'page_title': 'Klasa, Profesja i rozwój postaci',
         'classes_with_professions_dict': classes_with_professions_dict,
-        'elite_professions': elite_professions
+        'elite_classes': elite_classes,
+        'elite_classes_with_professions_dict': elite_classes_with_professions_dict
     }
     return render(request, 'rules/professions.html', context)
 
