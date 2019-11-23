7bb2f8492198e371437d9c2631f37e343fe1d3b9
Hermotimos <lukas.kozicki@gmail.com>
2019-11-23 10:36:11 +0100

    minor

--- debates/urls.py
+++ debates/urls.py
@@ -5,7 +5,7 @@ from debates import views
 app_name = 'debates'
 urlpatterns = [
     path('', views.debates_main_view, name='main'),
-    path('create_topic/', views.create_topic_view, name='create-topic'),
+    path('create-topic/', views.create_topic_view, name='create-topic'),
     path('topic:<int:topic_id>/create-debate/', views.create_debate_view, name='create-debate'),
     path('topic:<int:topic_id>/debate:<int:debate_id>/', views.debate_view, name='debate'),
     path('topic:<int:topic_id>/debate:<int:debate_id>/invite/', views.debates_invite_view, name='invite'),
