cc2a7ea06897972c10bdd75c0570307c3666b8a6
Hermotimos <lukas.kozicki@gmail.com>
2019-12-03 18:31:34 +0100

    minor

--- characters/views.py
+++ characters/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+
+from rpg_project.utils import query_debugger
 
 
+@query_debugger
 @login_required
 def tricks_sheet_view(request):
     context = {
