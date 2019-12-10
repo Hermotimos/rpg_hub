446d6dc5af6ce81f5a435418960d3f1b1173afc7
Hermotimos <lukas.kozicki@gmail.com>
2019-12-10 21:24:38 +0100

    search_fields in users.admin.py on Profile model

--- users/admin.py
+++ users/admin.py
@@ -3,9 +3,10 @@ from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'character_name', 'character_status', 'image')
-    list_editable = ('character_name', 'character_status', 'image')
-    list_filter = ('character_status', )
+    list_display = ['id', 'user', 'character_name', 'character_status', 'image']
+    list_editable = ['character_name', 'character_status', 'image']
+    list_filter = ['character_status']
+    search_fields = ['character_name']
 
 
 admin.site.register(Profile, ProfileAdmin)
