67df73e5f2fb18b9f26906f9a99c1af7aca11de3
Hermotimos <lukas.kozicki@gmail.com>
2019-12-11 13:40:41 +0100

    users.admin.py - show image of character

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'character_name', 'character_status', 'image']
+    list_display = ['id', 'get_img', 'user', 'character_name', 'character_status', 'image']
     list_editable = ['character_name', 'character_status', 'image']
     list_filter = ['character_status']
-    search_fields = ['character_name']
+    search_fields = ['user', 'character_name']
+
+    def get_img(self, obj):
+        if obj.image:
+            return format_html(f'<img width="40" height="40" src="{obj.image.url}">')
+        else:
+            return format_html(f'<img width="40" height="40" src="media/profile_pics/profile_default.jpg">')
 
 
 admin.site.register(Profile, ProfileAdmin)
