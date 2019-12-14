a4f63e3b68949cfbf09d8bc2ca803a015dc9ac4f
Hermotimos <lukas.kozicki@gmail.com>
2019-12-14 21:06:05 +0100

    minor

--- debates/models.py
+++ debates/models.py
@@ -2,7 +2,6 @@ import datetime
 from django.db import models
 from django.db.models.signals import m2m_changed, post_save
 from django.contrib.auth.models import User
-from django.db.models import Max, Min
 from PIL import Image
 from users.models import Profile
 
