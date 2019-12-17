d590a3174967639a4e1b29e7eb68fc8a1c16414c
Hermotimos <lukas.kozicki@gmail.com>
2019-12-17 10:50:06 +0100

    toponomikon_main_view improved

--- toponomikon/views.py
+++ toponomikon/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 
 from rpg_project.utils import query_debugger
@@ -9,15 +10,21 @@ def toponomikon_main_view(request):
     profile = request.user.profile
     if profile.character_status == 'gm':
         gen_locs = GeneralLocation.objects.all()
+        spec_locs = SpecificLocation.objects.all()
     else:
         known_directly = GeneralLocation.objects.filter(known_directly=profile)
         known_indirectly = GeneralLocation.objects.filter(known_indirectly=profile)
         gen_locs = (known_directly | known_indirectly).distinct()
+        spec_locs = (profile.spec_locs_known_directly.all() | profile.spec_locs_known_indirectly.all()).distinct()
 
-    gen_locs = gen_locs.prefetch_related('specific_locations').select_related('main_image')
+    gen_locs = gen_locs\
+        .prefetch_related(Prefetch('specific_locations', queryset=spec_locs))\
+        .select_related('main_image')
 
     context = {
         'page_title': 'Toponomikon',
         'gen_locs': gen_locs,
     }
     return render(request, 'toponomikon/toponomikon_main.html', context)
+
+
