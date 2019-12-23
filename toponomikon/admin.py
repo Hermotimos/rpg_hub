3a9ed83b5bd39285e9fda249c5c19c2415644a56
Hermotimos <lukas.kozicki@gmail.com>
2019-12-23 20:44:22 +0100

    toponomikon.admin.py customized

--- toponomikon/admin.py
+++ toponomikon/admin.py
@@ -1,8 +1,39 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.db import models
+from django.db.models import Q
+from django.forms import Textarea
+
 from history.models import GeneralLocation, SpecificLocation
+from users.models import Profile
+from imaginarion.models import Picture
+
+
+class ToponomikonKnownForm(forms.ModelForm):
+    known_directly = forms.ModelMultipleChoiceField(queryset=Profile.objects
+                                                    .exclude(Q(character_status='dead_player') |
+                                                             Q(character_status='dead_npc') |
+                                                            Q(character_status='gm') |
+                                                            Q(character_status='living_npc')),
+                                                    widget=FilteredSelectMultiple('Known directly', False),
+                                                    required=False)
+    known_indirectly = forms.ModelMultipleChoiceField(queryset=Profile.objects
+                                                      .exclude(Q(character_status='dead_player') |
+                                                               Q(character_status='dead_npc') |
+                                                               Q(character_status='gm') |
+                                                               Q(character_status='living_npc')),
+                                                      widget=FilteredSelectMultiple('Known indirectly', False),
+                                                      required=False)
+
+    pictures = forms.ModelMultipleChoiceField(queryset=Picture.objects.all(),
+                                              widget=FilteredSelectMultiple('Known indirectly', False),
+                                              required=False)
+
 
 
 class SpecificLocationAdmin(admin.ModelAdmin):
+    form = ToponomikonKnownForm
     list_display = ['name', 'general_location', 'main_image', 'description']
     list_editable = ['general_location', 'description', 'main_image']
     list_filter = ['general_location']
@@ -13,8 +44,14 @@ class SpecificLocationInline(admin.TabularInline):
     model = SpecificLocation
     extra = 0
 
+    form = ToponomikonKnownForm
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 15, 'cols': 30})},
+    }
+
 
 class GeneralLocationAdmin(admin.ModelAdmin):
+    form = ToponomikonKnownForm
     list_display = ['name', 'main_image', 'description']
     list_editable = ['description', 'main_image']
     search_fields = ['name', 'description']
