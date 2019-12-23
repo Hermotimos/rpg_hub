741cf61fde59823cfb7ec7490dfa8623fb24d0f9
Hermotimos <lukas.kozicki@gmail.com>
2019-12-23 18:01:18 +0100

    minor

--- characters/models.py
+++ characters/models.py
@@ -7,7 +7,7 @@ from users.models import Profile
 
 class Character(models.Model):
     profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    skill_levels_acquired = models.ManyToManyField(SkillLevel, related_name='acquired_by_characters')
+    skill_levels_acquired = models.ManyToManyField(SkillLevel, related_name='acquired_by_characters', blank=True)
     pictures = models.ManyToManyField(Picture, related_name='characters_pics', blank=True)
 
     def __str__(self):
