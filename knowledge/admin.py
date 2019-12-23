60e65ca4afa2f473c3dd948daee8aaef97debd86
Hermotimos <lukas.kozicki@gmail.com>
2019-12-23 21:41:59 +0100

    added knowledge app to settings + admin customization

--- knowledge/admin.py
+++ knowledge/admin.py
@@ -1,6 +1,37 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.db.models import Q
+from django.db import models
+from django.forms.widgets import Textarea, TextInput
 
+from imaginarion.models import Picture
 from knowledge.models import KnowledgePacket
+from users.models import Profile
 
 
-admin.site.register(KnowledgePacket)
+class ForAllowedProfilesAndPicturesForm(forms.ModelForm):
+    allowed_profiles = forms.ModelMultipleChoiceField(queryset=Profile.objects
+                                                      .exclude(Q(character_status='dead_player') |
+                                                               Q(character_status='dead_npc') |
+                                                               Q(character_status='gm') |
+                                                               Q(character_status='living_npc')),
+                                                      widget=FilteredSelectMultiple('Allowed profiles', False),
+                                                      required=False)
+    pictures = forms.ModelMultipleChoiceField(queryset=Picture.objects.all(),
+                                              widget=FilteredSelectMultiple('Pictures', False),
+                                              required=False)
+
+
+class KnowledgePacketAdmin(admin.ModelAdmin):
+    form = ForAllowedProfilesAndPicturesForm
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': 70})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 120})},
+    }
+    list_display = ['id', 'title', 'text']
+    list_editable = ['title', 'text']
+    search_fields = ['title', 'text']
+
+
+admin.site.register(KnowledgePacket, KnowledgePacketAdmin)
--- rpg_project/settings.py
+++ rpg_project/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'history',
     'home',
     'imaginarion',
+    'knowledge',
     'reload',
     'rules',
     'news',
