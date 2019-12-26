fd7d2de00b2dfaea747d621bf473efaadad5244a
Hermotimos <lukas.kozicki@gmail.com>
2019-12-26 20:24:24 +0100

    knowledge_inform_view + form

---
+++ knowledge/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from knowledge.models import KnowledgePacket
+from users.models import Profile
+from django.db.models import Q
+
+
+class KnowledgePacketInformForm(forms.ModelForm):
+    class Meta:
+        model = KnowledgePacket
+        fields = ['allowed_profiles']
+
+    def __init__(self, *args, **kwargs):
+        authenticated_user = kwargs.pop('authenticated_user')
+        already_allowed_profiles = kwargs.pop('already_allowed_profiles')
+        super(KnowledgePacketInformForm, self).__init__(*args, **kwargs)
+        allowable_profiles = Profile.objects.exclude(Q(user=authenticated_user) |
+                                                     Q(character_status='dead_player') |
+                                                     Q(character_status='dead_npc') |
+                                                     Q(character_status='living_npc') |
+                                                     Q(character_status='gm'))
+        self.fields['allowed_profiles'].label = ''
+        self.fields['allowed_profiles'].queryset = allowable_profiles.exclude(
+            id__in=[p.id for p in already_allowed_profiles]
+        )
+        self.fields['allowed_profiles'].widget.attrs['size'] = 10
--- knowledge/views.py
+++ knowledge/views.py
@@ -1,7 +1,12 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
 
+from knowledge.forms import KnowledgePacketInformForm
 from knowledge.models import KnowledgePacket
+from rpg_project import settings
 from rpg_project.utils import query_debugger
 from rules.models import SkillLevel
 from toponomikon.models import GeneralLocation, SpecificLocation
@@ -16,7 +21,7 @@ def knowledge_sheet_view(request):
             .filter(skill_levels__in=SkillLevel.objects.all())\
             .prefetch_related('pictures')
         toponomikon_kn_packets = (
-                KnowledgePacket.objects.filter(general_locations__in=GeneralLocation.objects.all()) | \
+                KnowledgePacket.objects.filter(general_locations__in=GeneralLocation.objects.all()) |
                 KnowledgePacket.objects.filter(specific_locations__in=SpecificLocation.objects.all())
         )\
             .distinct()\
@@ -41,3 +46,52 @@ def knowledge_sheet_view(request):
         'toponomikon_kn_packets': toponomikon_kn_packets,
     }
     return render(request, 'knowledge/knowledge_sheet.html', context)
+
+
+@query_debugger
+@login_required
+def knowledge_inform_view(request, kn_packet_id):
+    profile = request.user.profile
+    kn_packet = get_object_or_404(KnowledgePacket, id=kn_packet_id)
+
+    allowed_profiles_old = kn_packet.allowed_profiles.all()
+
+    if request.method == 'POST':
+        form = KnowledgePacketInformForm(authenticated_user=request.user,
+                                         already_allowed_profiles=allowed_profiles_old,
+                                         data=request.POST,
+                                         instance=kn_packet)
+        if form.is_valid():
+            allowed_profiles_new = form.cleaned_data['allowed_profiles']
+            kn_packet.allowed_profiles.add(*list(allowed_profiles_new))
+
+            subject = f"[RPG] Transfer wiedzy: '{kn_packet.title}'"
+            message = f"{profile} przekazał Ci wiedzę na temat: '{kn_packet.title}'.\n"\
+                f"Więdzę tę możesz odnaleźć w zakładce Wiedza/Okruchy wiedzy: " \
+                f"{request.get_host()}/knowledge/knowledge-sheet/\n"\
+                f"Zobaczysz tam, z jakimi elementami jest powiązana ta wiedza (Umiejętności, Toponomikon itp.).\n" \
+                f"Wiedzę możesz przeglądać zarówno w Okruchach wiedzy, jak i we wskazanych tam miejscach."
+            sender = settings.EMAIL_HOST_USER
+            receivers = []
+            for p in allowed_profiles_new:
+                receivers.append(p.user.email)
+            if profile.character_status != 'gm':
+                receivers.append('lukas.kozicki@gmail.com')
+            send_mail(subject, message, sender, receivers)
+
+            messages.info(request, f'Dokonałeś transferu wiedzy do wybranych postaci!')
+            _next = request.POST.get('next', '/')
+            return HttpResponseRedirect(_next)
+    else:
+        form = KnowledgePacketInformForm(authenticated_user=request.user,
+                                         already_allowed_profiles=allowed_profiles_old)
+
+    context = {
+        'page_title': 'Dokonaj transferu wiedzy',
+        'kn_packet': kn_packet,
+        'form': form,
+    }
+    if profile in kn_packet.allowed_profiles.all() or profile.character_status == 'gm':
+        return render(request, 'knowledge/knowledge_inform.html', context)
+    else:
+        return redirect('home:dupa')
