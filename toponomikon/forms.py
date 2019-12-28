c6923575fcd9f6611cd444b2f46cb832d33df5ba
Hermotimos <lukas.kozicki@gmail.com>
2019-12-28 17:38:10 +0100

    toponomikon_inform_view created + forms, urls, templates

--- toponomikon/forms.py
+++ toponomikon/forms.py
@@ -4,26 +4,45 @@ from users.models import Profile
 from django.db.models import Q
 
 
-class ToponomikonInformForm(forms.ModelForm):
+class GeneralLocationInformForm(forms.ModelForm):
     class Meta:
         model = GeneralLocation
         fields = ['known_indirectly']
 
     def __init__(self, *args, **kwargs):
         authenticated_user = kwargs.pop('authenticated_user')
-        already_know_directly = kwargs.pop('known_directly')
-        already_know_indirectly = kwargs.pop('known_indirectly')
-        super(ToponomikonInformForm, self).__init__(*args, **kwargs)
+        already_known_directly = kwargs.pop('known_directly_old')
+        already_known_indirectly = kwargs.pop('known_indirectly_old')
+        super(GeneralLocationInformForm, self).__init__(*args, **kwargs)
         informable_profiles = Profile.objects.exclude(Q(user=authenticated_user) |
                                                       Q(character_status='dead_player') |
                                                       Q(character_status='dead_npc') |
                                                       Q(character_status='living_npc') |
                                                       Q(character_status='gm') |
-                                                      Q(id__in=already_know_directly) |
-                                                      Q(id__in=already_know_indirectly))
+                                                      Q(id__in=already_known_directly) |
+                                                      Q(id__in=already_known_indirectly))
         self.fields['known_indirectly'].label = ''
-        self.fields['known_indirectly'].queryset = informable_profiles\
-        #     .exclude(
-        #     id__in=[p.id for p in already_allowed_profiles]
-        # )
+        self.fields['known_indirectly'].queryset = informable_profiles
+        self.fields['known_indirectly'].widget.attrs['size'] = 10
+
+
+class SpecificLocationInformForm(forms.ModelForm):
+    class Meta:
+        model = SpecificLocation
+        fields = ['known_indirectly']
+
+    def __init__(self, *args, **kwargs):
+        authenticated_user = kwargs.pop('authenticated_user')
+        already_known_directly = kwargs.pop('known_directly')
+        already_known_indirectly = kwargs.pop('known_indirectly')
+        super(SpecificLocationInformForm, self).__init__(*args, **kwargs)
+        informable_profiles = Profile.objects.exclude(Q(user=authenticated_user) |
+                                                      Q(character_status='dead_player') |
+                                                      Q(character_status='dead_npc') |
+                                                      Q(character_status='living_npc') |
+                                                      Q(character_status='gm') |
+                                                      Q(id__in=already_known_directly) |
+                                                      Q(id__in=already_known_indirectly))
+        self.fields['known_indirectly'].label = ''
+        self.fields['known_indirectly'].queryset = informable_profiles
         self.fields['known_indirectly'].widget.attrs['size'] = 10
