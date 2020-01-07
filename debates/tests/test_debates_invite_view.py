de1d773b65cf5b7bab0fb8174430d2562ef9dda0
Hermotimos <lukas.kozicki@gmail.com>
2020-01-07 12:33:36 +0100

    test_debates_invite_view.py - OK

--- debates/tests/test_debates_invite_view.py
+++ debates/tests/test_debates_invite_view.py
@@ -11,8 +11,8 @@ class DebatesInviteTest(TestCase):
         self.user1 = User.objects.create_user(username='user1', password='pass1111')
         self.user2 = User.objects.create_user(username='user2', password='pass1111')
 
-        self.topic1 = Topic.objects.create(id=1, title='Topic1')
-        self.debate1 = Debate.objects.create(id=1, topic=self.topic1, starter=self.user1)
+        self.topic1 = Topic.objects.create(title='Topic1')
+        self.debate1 = Debate.objects.create(topic=self.topic1, starter=self.user1)
         self.debate1.allowed_profiles.set([self.user1.profile, ])
         self.url = reverse('debates:invite', kwargs={'topic_id': self.topic1.id, 'debate_id': self.debate1.id})
 
@@ -22,6 +22,7 @@ class DebatesInviteTest(TestCase):
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_redirect_if_unallowed(self):
+        # user2 not in debate.allowed_profiles.all()
         self.client.force_login(self.user2)
         redirect_url = reverse('home:dupa')
         response = self.client.get(self.url)
@@ -53,34 +54,24 @@ class DebatesInviteTest(TestCase):
         form = response.context.get('form')
         self.assertIsInstance(form, InviteForm)
 
-    # TODO won't pass - WHY? error ' "" nie jest poprawną wartością.' though form.data seems legit
-    # def test_valid_post_data(self):
-    #     self.client.force_login(self.user1)
-    #     form = InviteForm(instance=self.debate1,
-    #                       authenticated_user=self.user1,
-    #                       already_allowed_profiles_ids=[self.user1.profile.id, ])
-    #     data = form.initial
-    #
-    #     print('\n', data)
-    #     self.assertTrue(Profile.objects.get(user__username='user1'))
-    #     data['allowed_profiles'] = [self.user1.profile, self.user2.profile]
-    #     print('\n', data)
-    #     response = self.client.post(self.url, data)
-    #     form = response.context.get('form')
-    #     print('\n', form.errors)
-    #
-    #     # self.client.post(self.url, data)
-    #     # self.assertFalse(form.errors)
-    #     self.assertTrue(self.user2.profile in Debate.objects.get(id=1).allowed_profiles.all())
+    def test_valid_post_data(self):
+        self.client.force_login(self.user1)
+        form = InviteForm(instance=self.debate1,
+                          authenticated_user=self.user1,
+                          old_allowed_profiles=[self.user1.profile.id, ])
+        data = form.initial
+        data['allowed_profiles'] = [self.user2.profile.id]
+        self.assertFalse(self.user2.profile in Debate.objects.get(id=1).allowed_profiles.all())
+        self.client.post(self.url, data)
+        self.assertTrue(self.user2.profile in Debate.objects.get(id=1).allowed_profiles.all())
 
     def test_invalid_post_data(self):
         self.client.force_login(self.user1)
         form = InviteForm(instance=self.debate1,
                           authenticated_user=self.user1,
-                          already_allowed_profiles_ids=[self.user1.profile.id, ])
+                          old_allowed_profiles=[self.user1.profile.id, ])
         data = form.initial
         data['allowed_profiles'] = 'Wrong kind of data'
-
         response = self.client.post(self.url, data)
         form = response.context.get('form')
         # should show the form again, not redirect
@@ -91,13 +82,11 @@ class DebatesInviteTest(TestCase):
         self.client.force_login(self.user1)
         form = InviteForm(instance=self.debate1,
                           authenticated_user=self.user1,
-                          already_allowed_profiles_ids=[self.user1.profile.id, ])
+                          old_allowed_profiles=[self.user1.profile.id, ])
         data = form.initial
         data['allowed_profiles'] = ''
-
         response = self.client.post(self.url, data)
         form = response.context.get('form')
         # should show the form again, not redirect
         self.assertEquals(response.status_code, 200)
         self.assertTrue(form.errors)
-        self.assertTrue(self.user2.profile not in Debate.objects.get(id=1).allowed_profiles.all())
