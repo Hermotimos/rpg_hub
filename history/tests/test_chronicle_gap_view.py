cc55f5105039e3666f8335586670f746bd5e5f4c
Hermotimos <lukas.kozicki@gmail.com>
2020-01-08 12:29:48 +0100

    test_chronicle_gap_view.py created TODO

---
+++ history/tests/test_chronicle_gap_view.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from history import views
+from history.models import Chapter,  ChronicleEvent, GameSession
+from users.models import User
+
+
+class ChronicleGapTest(TestCase):
+    pass
+    # TODO create test
