3d698b3dab935ad9e1202ee5c75f1a8ac66d73c8
Hermotimos <lukas.kozicki@gmail.com>
2020-01-09 08:59:07 +0100

    rules.admin.py - improved (code ordering and added FilteredSelectMultiple for 'knowledge_packets' field on SkillLevel model

--- rules/admin.py
+++ rules/admin.py
@@ -7,45 +7,60 @@ from django.forms import Textarea
 from django.utils.translation import ugettext_lazy
 
 from imaginarion.models import Picture
+from knowledge.models import KnowledgePacket
 from rules.models import Skill, SkillLevel, Synergy, CharacterClass, CharacterProfession, EliteClass, EliteProfession, \
     WeaponClass, WeaponType, PlateType, ShieldType
 from users.models import Profile
 
 
-class ForAllowedProfilesForm(forms.ModelForm):
+# 'allowed_profiles' FIELD AS FilteredSelectMultiple
+class Form1(forms.ModelForm):
     allowed_profiles = forms.ModelMultipleChoiceField(queryset=Profile.objects
                                                       .exclude(Q(character_status='dead_player') |
                                                                Q(character_status='dead_npc') |
                                                                Q(character_status='gm') |
                                                                Q(character_status='living_npc')),
-                                                      widget=FilteredSelectMultiple('Allowed profiles', False),
-                                                      required=False)
+                                                      required=False,
+                                                      widget=FilteredSelectMultiple('Allowed profiles', False))
 
 
-class ForAllowedProfilesAndPicturesForm(forms.ModelForm):
+# 'allowed_profiles' AND 'pictures' FIELDS AS FilteredSelectMultiple
+class Form2(forms.ModelForm):
     allowed_profiles = forms.ModelMultipleChoiceField(queryset=Profile.objects
                                                       .exclude(Q(character_status='dead_player') |
                                                                Q(character_status='dead_npc') |
                                                                Q(character_status='gm') |
                                                                Q(character_status='living_npc')),
-                                                      widget=FilteredSelectMultiple('Allowed profiles', False),
-                                                      required=False)
-    pictures = forms.ModelMultipleChoiceField(queryset=Picture.objects.all(),
-                                              widget=FilteredSelectMultiple('Pictures', False),
-                                              required=False)
+                                                      required=False,
+                                                      widget=FilteredSelectMultiple('Allowed profiles', False))
+    pictures = forms.ModelMultipleChoiceField(queryset=Picture.objects.all(), required=False,
+                                              widget=FilteredSelectMultiple('Pictures', False))
 
 
-class ForAllowedProfilesAndSkillsForm(forms.ModelForm):
+# 'allowed_profiles' AND 'skills' FIELDS AS FilteredSelectMultiple
+class Form3(forms.ModelForm):
     allowed_profiles = forms.ModelMultipleChoiceField(queryset=Profile.objects
                                                       .exclude(Q(character_status='dead_player') |
                                                                Q(character_status='dead_npc') |
                                                                Q(character_status='gm') |
                                                                Q(character_status='living_npc')),
-                                                      widget=FilteredSelectMultiple('Allowed profiles', False),
-                                                      required=False)
-    skills = forms.ModelMultipleChoiceField(queryset=Skill.objects.all(),
-                                            widget=FilteredSelectMultiple('Skills', False),
-                                            required=False)
+                                                      required=False,
+                                                      widget=FilteredSelectMultiple('Allowed profiles', False))
+    skills = forms.ModelMultipleChoiceField(queryset=Skill.objects.all(), required=False,
+                                            widget=FilteredSelectMultiple('Skills', False))
+
+
+# 'allowed_profiles' AND 'knowledge_packets' FIELDS AS FilteredSelectMultiple
+class Form4(forms.ModelForm):
+    allowed_profiles = forms.ModelMultipleChoiceField(queryset=Profile.objects
+                                                      .exclude(Q(character_status='dead_player') |
+                                                               Q(character_status='dead_npc') |
+                                                               Q(character_status='gm') |
+                                                               Q(character_status='living_npc')),
+                                                      required=False,
+                                                      widget=FilteredSelectMultiple('Allowed profiles', False))
+    knowledge_packets = forms.ModelMultipleChoiceField(queryset=KnowledgePacket.objects.all(), required=False,
+                                                       widget=FilteredSelectMultiple('Knowledge packets', False))
 
 
 class SkillLevelFilter(admin.SimpleListFilter):
@@ -65,6 +80,7 @@ class SkillLevelFilter(admin.SimpleListFilter):
 
 
 class SkillLevelAdmin(admin.ModelAdmin):
+    form = Form4
     formfield_overrides = {
         models.TextField: {'widget': Textarea(attrs={'rows': 8, 'cols': 80})},
     }
@@ -84,7 +100,7 @@ class SkillLevelInline(admin.TabularInline):
 
 
 class SkillAdmin(admin.ModelAdmin):
-    form = ForAllowedProfilesForm
+    form = Form1
     formfield_overrides = {
         models.CharField: {'widget': Textarea(attrs={'rows': 2, 'cols': 10})},
         models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 30})},
@@ -96,7 +112,7 @@ class SkillAdmin(admin.ModelAdmin):
 
 
 class SynergyAdmin(admin.ModelAdmin):
-    form = ForAllowedProfilesAndSkillsForm
+    form = Form3
     formfield_overrides = {
         models.CharField: {'widget': Textarea(attrs={'rows': 3, 'cols': 10})},
         models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 30})},
@@ -111,7 +127,7 @@ class CharacterProfessionInline(admin.TabularInline):
     extra = 2
 
     fields = ['name', 'description', 'start_perks', 'allowed_profiles']
-    form = ForAllowedProfilesForm
+    form = Form1
     formfield_overrides = {
         models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 10})},
         models.TextField: {'widget': Textarea(attrs={'rows': 15, 'cols': 40})},
@@ -119,7 +135,7 @@ class CharacterProfessionInline(admin.TabularInline):
 
 
 class CharacterProfessionAdmin(admin.ModelAdmin):
-    form = ForAllowedProfilesForm
+    form = Form1
     formfield_overrides = {
         models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 10})},
         models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 60})},
@@ -140,7 +156,7 @@ class EliteProfessionInline(admin.TabularInline):
     model = EliteProfession
     extra = 2
 
-    form = ForAllowedProfilesForm
+    form = Form1
     formfield_overrides = {
         models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 10})},
         models.TextField: {'widget': Textarea(attrs={'rows': 15, 'cols': 40})},
@@ -148,7 +164,7 @@ class EliteProfessionInline(admin.TabularInline):
 
 
 class EliteProfessionAdmin(admin.ModelAdmin):
-    form = ForAllowedProfilesForm
+    form = Form1
     formfield_overrides = {
         models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 10})},
         models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 60})},
@@ -159,7 +175,7 @@ class EliteProfessionAdmin(admin.ModelAdmin):
 
 
 class EliteClassAdmin(admin.ModelAdmin):
-    form = ForAllowedProfilesForm
+    form = Form1
     inlines = [EliteProfessionInline]
     list_display = ['name', 'description']
     list_editable = ['description']
@@ -170,7 +186,7 @@ class WeaponTypeInline(admin.TabularInline):
     model = WeaponType
     extra = 2
 
-    form = ForAllowedProfilesAndPicturesForm
+    form = Form2
     formfield_overrides = {
         models.TextField: {'widget': Textarea(attrs={'rows': 1, 'cols': 25})},
         models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 5})},
@@ -188,7 +204,7 @@ class WeaponClassAdmin(admin.ModelAdmin):
 
 
 class WeaponTypeAdmin(admin.ModelAdmin):
-    form = ForAllowedProfilesAndPicturesForm
+    form = Form2
     formfield_overrides = {
         models.TextField: {'widget': Textarea(attrs={'rows': 2, 'cols': 100})},
     }
@@ -198,7 +214,7 @@ class WeaponTypeAdmin(admin.ModelAdmin):
 
 
 class PlateTypeAdmin(admin.ModelAdmin):
-    form = ForAllowedProfilesAndPicturesForm
+    form = Form2
     formfield_overrides = {
         models.TextField: {'widget': Textarea(attrs={'rows': 2, 'cols': 100})},
     }
@@ -208,7 +224,7 @@ class PlateTypeAdmin(admin.ModelAdmin):
 
 
 class ShieldTypeAdmin(admin.ModelAdmin):
-    form = ForAllowedProfilesForm
+    form = Form2
     formfield_overrides = {
         models.TextField: {'widget': Textarea(attrs={'rows': 2, 'cols': 100})},
     }
