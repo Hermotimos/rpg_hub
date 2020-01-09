e97fe357b34e5887b67ca742d9dd5a004fac7f94
Hermotimos <lukas.kozicki@gmail.com>
2020-01-09 09:43:04 +0100

    news.admin.py improved

--- news/admin.py
+++ news/admin.py
@@ -23,6 +23,15 @@ class NewsAdminForm(forms.ModelForm):
                                                required=False)
 
 
+class SurveyAdminForm(forms.ModelForm):
+    addressees = forms.ModelMultipleChoiceField(queryset=Profile.objects
+                                                .exclude(Q(character_status='dead_player') |
+                                                         Q(character_status='dead_npc') |
+                                                         Q(character_status='gm')),
+                                                required=False,
+                                                widget=FilteredSelectMultiple('Addressees', False))
+
+
 class NewsAdmin(admin.ModelAdmin):
     form = NewsAdminForm
     list_display = ['id', 'author', 'title', 'date_posted', 'image']
@@ -33,6 +42,7 @@ class NewsAdmin(admin.ModelAdmin):
 
 class NewsAnswerAdmin(admin.ModelAdmin):
     list_display = ['id', 'news_title', 'author', 'get_caption', 'date_posted']
+    readonly_fields = ['seen_by']
     search_fields = ['text']
 
     def news_title(self, obj):
@@ -45,15 +55,7 @@ class NewsAnswerAdmin(admin.ModelAdmin):
 class SurveyOptionInline(admin.TabularInline):
     model = SurveyOption
     extra = 4
-
-
-class SurveyAdminForm(forms.ModelForm):
-    addressees = forms.ModelMultipleChoiceField(queryset=Profile.objects
-                                                .exclude(Q(character_status='dead_player') |
-                                                         Q(character_status='dead_npc') |
-                                                         Q(character_status='gm')),
-                                                widget=FilteredSelectMultiple('Allowed profiles', False),
-                                                required=False)
+    readonly_fields = ['yes_voters', 'no_voters']
 
 
 class SurveyAdmin(admin.ModelAdmin):
@@ -64,9 +66,14 @@ class SurveyAdmin(admin.ModelAdmin):
     search_fields = ['title', 'text']
 
 
+class SurveyAnswerAdmin(admin.ModelAdmin):
+    readonly_fields = ['seen_by']
+
+
 class SurveyOptionAdmin(admin.ModelAdmin):
     list_display = ['survey', 'author', 'option_text']
     list_editable = ['option_text']
+    readonly_fields = ['yes_voters', 'no_voters']
     search_fields = ['option_text']
 
 
@@ -74,4 +81,4 @@ admin.site.register(News, NewsAdmin)
 admin.site.register(NewsAnswer, NewsAnswerAdmin)
 admin.site.register(Survey, SurveyAdmin)
 admin.site.register(SurveyOption, SurveyOptionAdmin)
-admin.site.register(SurveyAnswer)
+admin.site.register(SurveyAnswer, SurveyAnswerAdmin)
