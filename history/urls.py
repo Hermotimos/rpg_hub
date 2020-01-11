0ec0ef6131a99090790906ffe0d50a35565e054c
Hermotimos <lukas.kozicki@gmail.com>
2020-01-11 14:15:36 +0100

    replaced all views for all and for filtered TimelineEvent-s with 1 view

--- history/urls.py
+++ history/urls.py
@@ -17,15 +17,24 @@ urlpatterns = [
     # timeline
     path('timeline/', views.timeline_main_view, name='timeline-main'),
     path('timeline/create/', views.timeline_create_view, name='timeline-create'),
-    path('timeline/all-events/', views.timeline_all_events_view, name='timeline-all-events'),
-    path('timeline/thread:<int:thread_id>/', views.timeline_thread_view, name='timeline-thread'),
-    path('timeline/participant:<int:participant_id>/', views.timeline_participant_view, name='timeline-participant'),
-    path('timeline/gen-loc:<int:gen_loc_id>/', views.timeline_general_location_view, name='timeline-gen-loc'),
-    path('timeline/spec-loc:<int:spec_loc_id>/', views.timeline_specific_location_view, name='timeline-spec-loc'),
-    path('timeline/date:<int:year>:<str:season>/', views.timeline_date_view, name='timeline-date'),
-    path('timeline/game:<int:game_id>/', views.timeline_game_view, name='timeline-game'),
+    path('timeline/'
+         'thread:<int:thread_id>/'
+         'participant:<int:participant_id>/'
+         'gen-loc:<int:gen_loc_id>/'
+         'spec-loc:<int:spec_loc_id>/'
+         'date:<int:year>:<str:season>/'
+         'game:<int:game_id>/',
+         views.timeline_filter_events_view, name='timeline-filtered-events'),
+    # replaced by timeline_filter_events_view (delete when no longer needed for reference):
+    # path('timeline/all-events/', views.timeline_all_events_view, name='timeline-all-events'),
+    # path('timeline/thread:<int:thread_id>/', views.timeline_thread_view, name='timeline-thread'),
+    # path('timeline/participant:<int:participant_id>/', views.timeline_participant_view, name='timeline-participant'),
+    # path('timeline/gen-loc:<int:gen_loc_id>/', views.timeline_general_location_view, name='timeline-gen-loc'),
+    # path('timeline/spec-loc:<int:spec_loc_id>/', views.timeline_specific_location_view, name='timeline-spec-loc'),
+    # path('timeline/date:<int:year>:<str:season>/', views.timeline_date_view, name='timeline-date'),
+    # path('timeline/game:<int:game_id>/', views.timeline_game_view, name='timeline-game'),
+
     path('timeline/inform:<int:event_id>/', views.timeline_inform_view, name='timeline-inform'),
     path('timeline/note:<int:event_id>/', views.timeline_note_view, name='timeline-note'),
     path('timeline/edit:<int:event_id>/', views.timeline_edit_view, name='timeline-edit'),
-
 ]
