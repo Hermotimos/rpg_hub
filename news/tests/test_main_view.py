d91d091b2fbcedcfccd5ed1eeb509b0b4d3a5459
Hermotimos <lukas.kozicki@gmail.com>
2020-01-20 14:14:33 +0100

    news app - test_main_view.py - OK

--- news/tests/test_main_view.py
+++ news/tests/test_main_view.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from news import views
-from news.models import News
+from news.models import News, Survey
 from users.models import User
 
 
@@ -9,12 +9,15 @@ class MainTest(TestCase):
     def setUp(self):
         self.user1 = User.objects.create_user(username='user1', password='pass1111')
         self.user2 = User.objects.create_user(username='user2', password='pass1111')
+        self.user3 = User.objects.create_user(username='user3', password='pass1111')
         self.user4 = User.objects.create_user(username='user4', password='pass1111')
         self.user4.profile.character_status = 'gm'
         self.user4.profile.save()
 
         self.news1 = News.objects.create(title='Title1', author=self.user1)
-        self.news1.allowed_profiles.set([self.user1.profile, ])
+        self.news1.allowed_profiles.set([self.user1.profile, self.user2.profile])
+        self.survey1 = Survey.objects.create(title='Survey1', author=self.user1)
+        self.survey1.addressees.set([self.user1.profile, self.user2.profile])
 
         self.url = reverse('news:main')
 
@@ -35,21 +38,32 @@ class MainTest(TestCase):
     def test_links(self):
         linked_url1 = reverse('news:create')
         linked_url2 = reverse('news:detail', kwargs={'news_id': self.news1.id})
+        linked_url3 = reverse('news:survey-detail', kwargs={'survey_id': self.survey1.id})
 
-        # case request.user.profile in news.allowed_profiles
+        # request.user.profile in news1.allowed_profiles.all() and is survey1.author
         self.client.force_login(self.user1)
         response = self.client.get(self.url)
         self.assertContains(response, f'href="{linked_url1}"')
         self.assertContains(response, f'href="{linked_url2}"')
+        self.assertContains(response, f'href="{linked_url3}"')
 
-        # case request.user.profile not in news.allowed_profiles
+        # request.user.profile in news1.allowed_profiles and survey1.addressees.all()
         self.client.force_login(self.user2)
         response = self.client.get(self.url)
         self.assertContains(response, f'href="{linked_url1}"')
-        self.assertNotContains(response, f'href="{linked_url2}"')
+        self.assertContains(response, f'href="{linked_url2}"')
+        self.assertContains(response, f'href="{linked_url3}"')
 
-        # case request.user.profile.character_status == 'gm'
+        # request.user.profile not in news1.allowed_profiles.all() and not in survey1.addressees.all()
+        self.client.force_login(self.user3)
+        response = self.client.get(self.url)
+        self.assertContains(response, f'href="{linked_url1}"')
+        self.assertNotContains(response, f'href="{linked_url2}"')
+        self.assertNotContains(response, f'href="{linked_url3}"')
+
+        # request.user.profile.character_status == 'gm'
         self.client.force_login(self.user4)
         response = self.client.get(self.url)
         self.assertContains(response, f'href="{linked_url1}"')
         self.assertContains(response, f'href="{linked_url2}"')
+        self.assertContains(response, f'href="{linked_url3}"')
