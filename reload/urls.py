30ee1e30bf15f1cabb089bb10263e5ffc959c5ec
Hermotimos <lukas.kozicki@gmail.com>
2020-01-24 12:51:50 +0100

    reload app - developed

--- reload/urls.py
+++ reload/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.reload_main_view, name='reload-main'),
     path('reload-history/', views.reload_history, name='reload-history'),
     path('reload-rules/', views.reload_rules, name='reload-rules'),
+    path('reload-toponomikon/', views.reload_toponomikon, name='reload-toponomikon'),
     ]
--- reload/views.py
+++ reload/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from history.models import Thread, SpecificLocation, GeneralLocation, TimelineEvent
-from rules.models import Skill, Synergy, CharacterClass, CharacterProfession, EliteClass, EliteProfession, WeaponClass, WeaponType
+from rules.models import Skill, SkillLevel, Synergy, SynergyLevel, CharacterClass, CharacterProfession, EliteClass, \
+    EliteProfession, WeaponClass, WeaponType
 from rpg_project.utils import query_debugger
 
 
@@ -26,14 +27,28 @@ def reload_history(request):
 
         for obj in Thread.objects.all():
             obj.save()
-        for obj in GeneralLocation.objects.all():
-            obj.save()
-        for obj in SpecificLocation.objects.all():
-            obj.save()
         for obj in TimelineEvent.objects.all():
             obj.save()
 
         messages.info(request, f'Przeładowano "sorting_name" dla aplikacji "history"!')
+
+        return redirect('reload:reload-main')
+    else:
+        return redirect('home:dupa')
+
+
+@query_debugger
+@login_required
+def reload_toponomikon(request):
+    if request.user.profile.character_status == 'gm':
+
+        for obj in GeneralLocation.objects.all():
+            obj.save()
+        for obj in SpecificLocation.objects.all():
+            obj.save()
+
+        messages.info(request, f'Przeładowano "sorting_name" dla aplikacji "toponomikon"!')
+
         return redirect('reload:reload-main')
     else:
         return redirect('home:dupa')
@@ -46,22 +61,31 @@ def reload_rules(request):
 
         for obj in Skill.objects.all():
             obj.save()
+        for obj in SkillLevel.objects.all():
+            obj.save()
+
         for obj in Synergy.objects.all():
             obj.save()
+        for obj in SynergyLevel.objects.all():
+            obj.save()
+
         for obj in CharacterClass.objects.all():
             obj.save()
         for obj in CharacterProfession.objects.all():
             obj.save()
+
         for obj in EliteClass.objects.all():
             obj.save()
         for obj in EliteProfession.objects.all():
             obj.save()
+
         for obj in WeaponClass.objects.all():
             obj.save()
         for obj in WeaponType.objects.all():
             obj.save()
 
         messages.info(request, f'Przeładowano "sorting_name" dla aplikacji "rules"!')
+
         return redirect('reload:reload-main')
     else:
         return redirect('home:dupa')
