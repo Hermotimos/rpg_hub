08a52309b96fe546edcf8c8bc4520cfd8d681c1e
Hermotimos <lukas.kozicki@gmail.com>
2020-01-30 13:04:42 +0100

    minor

--- characters/admin.py
+++ characters/admin.py
@@ -5,6 +5,7 @@ from django.utils.html import format_html
 
 from characters.models import Character
 from imaginarion.models import Picture
+from knowledge.models import KnowledgePacket
 from rules.models import SkillLevel, SynergyLevel
 
 
@@ -13,7 +14,8 @@ class CharacterAdminForm(forms.ModelForm):
                                                            widget=FilteredSelectMultiple('Skill levels', False))
     synergy_levels_acquired = forms.ModelMultipleChoiceField(queryset=SynergyLevel.objects.all(), required=False,
                                                              widget=FilteredSelectMultiple('Synergy levels', False))
-
+    knowledge_packets = forms.ModelMultipleChoiceField(queryset=KnowledgePacket.objects.all(), required=False,
+                                                       widget=FilteredSelectMultiple('Knowledge packets', False))
     pictures = forms.ModelMultipleChoiceField(queryset=Picture.objects.all(), required=False,
                                               widget=FilteredSelectMultiple('Pictures', False))
 
