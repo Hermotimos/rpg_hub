e6f8fa8a63d843e01e02ff3cda4bf6ce1780775d
Hermotimos <lukas.kozicki@gmail.com>
2020-02-04 16:21:42 +0100

    del old code

--- knowledge/models.py
+++ knowledge/models.py
@@ -2,27 +2,14 @@ from django.db import models
 
 from imaginarion.models import Picture
 from rpg_project.utils import create_sorting_name
-from rules.models import Skill, Synergy
-from users.models import Profile
-
-
-# class KnowledgePacketType(models.Model):
-#     name = models.CharField(max_length=30, unique=True)
-#
-#     def __str__(self):
-#         return self.name
-#
-#     class Meta:
-#         ordering = ['name']
+from rules.models import Skill
 
 
 class KnowledgePacket(models.Model):
     title = models.CharField(max_length=100, unique=True)
-    # packet_types = models.ManyToManyField(KnowledgePacketType, related_name='knowledge_packets')
     skills = models.ManyToManyField(Skill, related_name='knowledge_packets')
     text = models.TextField()
     pictures = models.ManyToManyField(Picture, related_name='knowledge_packets', blank=True)
-    # allowed_profiles = models.ManyToManyField(Profile, related_name='allowed_knowledge_packets', blank=True)
     sorting_name = models.CharField(max_length=250, blank=True, null=True)
 
     def __str__(self):
@@ -34,15 +21,3 @@ class KnowledgePacket(models.Model):
 
     class Meta:
         ordering = ['title']
-
-
-# not working:
-# def add_gms_to_allowed_profiles(sender, instance, **kwargs):
-#     gms = Profile.objects.filter(character_status='gm')
-#     for gm in gms:
-#         if gm not in instance.allowed_profiles.all():
-#             instance.allowed_profiles.add(gm)
-#             instance.m2msave()
-#
-#
-# post_save.connect(add_gms_to_allowed_profiles, sender=KnowledgePacket)
