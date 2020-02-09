f5c8c72a0d5c401c02a652208933d1c6cf3125d8
Hermotimos <lukas.kozicki@gmail.com>
2020-02-09 22:42:34 +0100

    minor

--- contact/views.py
+++ contact/views.py
@@ -86,7 +86,7 @@ def demands_delete_view(request, demand_id):
 @login_required
 def demands_detail_view(request, demand_id):
     demand = get_object_or_404(Demand, id=demand_id)
-    answers = DemandAnswer.objects.filter(demand=demand).select_related('author', 'author__profile')
+    answers = DemandAnswer.objects.filter(demand=demand).select_related('author__profile').order_by('date_posted')
 
     if request.method == 'POST':
         form = DemandAnswerForm(request.POST, request.FILES)
