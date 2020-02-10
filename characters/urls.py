a13b0c72484b7e933fb4032187c9da74721c58b4
Hermotimos <lukas.kozicki@gmail.com>
2020-02-10 17:05:32 +0100

    characters.views - transitioned FBV -> CBV

--- characters/urls.py
+++ characters/urls.py
@@ -6,8 +6,9 @@ from rpg_project.utils import query_debugger
 app_name = 'characters'
 urlpatterns = [
     # path('tricks/', views.tricks_view, name='tricks'),
-    path('tricks/', query_debugger(views.TricksView.as_view()), name='tricks'),
-    path('skills-sheet:<int:profile_id>/', views.skills_sheet_view, name='skills-sheet'),
+    path('tricks/', query_debugger(views.CharacterTricksView.as_view()), name='tricks'),
+    # path('character-skills:<int:profile_id>/', views.skills_sheet_view, name='character-skills'),
+    path('character-skills:<int:profile_id>/', query_debugger(views.CharacterSkillsView.as_view()), name='character-skills'),
     # path('skills-sheets-for-gm/', views.skills_sheets_for_gm_view, name='skills-sheets-for-gm'),
     path('skills-sheets-for-gm/', query_debugger(views.SkillsForGmView.as_view()), name='skills-sheets-for-gm'),
 
--- characters/views.py
+++ characters/views.py
@@ -3,40 +3,15 @@ from django.db.models import Prefetch, Q
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.generic import ListView
 
 from characters.models import Character
-from rpg_project.utils import query_debugger
 from rules.models import Skill, SkillLevel, Synergy, SynergyLevel
 from users.models import Profile
 
 
-# @query_debugger
-# @login_required
-# def tricks_view(request):
-#     profile = request.user.profile
-#
-#     if profile.character_status == 'gm':
-#         players_profiles = Profile.objects.exclude(Q(character_status='dead_player') |
-#                                                    Q(character_status='living_npc') |
-#                                                    Q(character_status='dead_npc') |
-#                                                    Q(character_status='gm'))
-#     else:
-#         players_profiles = [profile]
-#
-#     context = {
-#         'page_title': f'Podstępy - {profile.character_name}',
-#         'players_profiles': players_profiles
-#     }
-#     return render(request, 'characters/tricks.html', context)
-
-
-class TricksView(View):
+class CharacterTricksView(View):
 
     @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
-
     def get(self, request):
         profile = request.user.profile
 
@@ -52,59 +27,61 @@ class TricksView(View):
             'page_title': f'Podstępy - {profile.character_name}',
             'players_profiles': players_profiles
         }
-        return render(request, 'characters/tricks.html', context)
+        return render(request, 'characters/character_tricks.html', context)
 
 
-@query_debugger
-@login_required
-def skills_sheet_view(request, profile_id='0'):
-    try:
-        profile = Profile.objects.get(id=profile_id)
-    except Profile.DoesNotExist:
+class CharacterSkillsView(View):
+
+    @method_decorator(login_required)
+    def get(self, request, profile_id='0'):
+        try:
+            profile = Profile.objects.get(id=profile_id)
+        except Profile.DoesNotExist:
+            profile = request.user.profile
+
+        skills = Skill.objects \
+            .filter(skill_levels__acquired_by_characters=profile.character) \
+            .exclude(name__icontains='Doktryn') \
+            .prefetch_related(Prefetch(
+                'skill_levels',
+                queryset=SkillLevel.objects.filter(acquired_by_characters=profile.character)
+            )) \
+            .distinct()
+
+        synergies = Synergy.objects \
+            .filter(synergy_levels__acquired_by_characters=profile.character) \
+            .prefetch_related(Prefetch(
+                'synergy_levels',
+                queryset=SynergyLevel.objects.filter(acquired_by_characters=profile.character)
+            )) \
+            .distinct()
+
+        context = {
+            'page_title': f'Umiejętności - {profile.character_name}',
+            'skills': skills,
+            'synergies': synergies,
+        }
+        if request.user.profile.character_status != 'gm' and profile_id != 0:
+            return redirect('home:dupa')
+        else:
+            return render(request, 'characters/skills_sheet.html', context)
+
+
+class SkillsForGmView(View):
+
+    @method_decorator(login_required)
+    def get(self, request):
         profile = request.user.profile
+        characters = Character.objects.all().select_related('profile')
 
-    skills = Skill.objects\
-        .filter(skill_levels__acquired_by_characters=profile.character)\
-        .exclude(name__icontains='Doktryn')\
-        .prefetch_related(Prefetch(
-            'skill_levels',
-            queryset=SkillLevel.objects.filter(acquired_by_characters=profile.character)
-        ))\
-        .distinct()
-
-    synergies = Synergy.objects\
-        .filter(synergy_levels__acquired_by_characters=profile.character) \
-        .prefetch_related(Prefetch(
-            'synergy_levels',
-            queryset=SynergyLevel.objects.filter(acquired_by_characters=profile.character)
-        )) \
-        .distinct()
-
-    context = {
-        'page_title': f'Umiejętności - {profile.character_name}',
-        'skills': skills,
-        'synergies': synergies,
-    }
-    if request.user.profile.character_status != 'gm' and profile_id != 0:
-        return redirect('home:dupa')
-    else:
-        return render(request, 'characters/skills_sheet.html', context)
-
-
-# @query_debugger
-# @login_required
-# def skills_sheets_for_gm_view(request):
-#     profile = request.user.profile
-#     characters = Character.objects.all().select_related('profile')
-#
-#     context = {
-#         'page_title': 'Umiejętności graczy',
-#         'characters': characters
-#     }
-#     if profile.character_status == 'gm':
-#         return render(request, 'characters/skills_sheets_for_gm.html', context)
-#     else:
-#         return redirect('home:dupa')
+        context = {
+            'page_title': 'Umiejętności graczy',
+            'characters': characters
+        }
+        if profile.character_status == 'gm':
+            return render(request, 'characters/skills_sheets_for_gm.html', context)
+        else:
+            return redirect('home:dupa')
 
 
 # class SkillsForGmListView(ListView):
@@ -124,18 +101,73 @@ def skills_sheet_view(request, profile_id='0'):
 #         return context
 
 
-class SkillsForGmView(View):
+# @query_debugger
+# @login_required
+# def tricks_view(request):
+#     profile = request.user.profile
+#
+#     if profile.character_status == 'gm':
+#         players_profiles = Profile.objects.exclude(Q(character_status='dead_player') |
+#                                                    Q(character_status='living_npc') |
+#                                                    Q(character_status='dead_npc') |
+#                                                    Q(character_status='gm'))
+#     else:
+#         players_profiles = [profile]
+#
+#     context = {
+#         'page_title': f'Podstępy - {profile.character_name}',
+#         'players_profiles': players_profiles
+#     }
+#     return render(request, 'characters/character_tricks.html', context)
 
-    @method_decorator(login_required)
-    def get(self, request):
-        profile = request.user.profile
-        characters = Character.objects.all().select_related('profile')
 
-        context = {
-            'page_title': 'Umiejętności graczy',
-            'characters': characters
-        }
-        if profile.character_status == 'gm':
-            return render(request, 'characters/skills_sheets_for_gm.html', context)
-        else:
-            return redirect('home:dupa')
+# @query_debugger
+# @login_required
+# def skills_sheet_view(request, profile_id='0'):
+#     try:
+#         profile = Profile.objects.get(id=profile_id)
+#     except Profile.DoesNotExist:
+#         profile = request.user.profile
+#
+#     skills = Skill.objects\
+#         .filter(skill_levels__acquired_by_characters=profile.character)\
+#         .exclude(name__icontains='Doktryn')\
+#         .prefetch_related(Prefetch(
+#             'skill_levels',
+#             queryset=SkillLevel.objects.filter(acquired_by_characters=profile.character)
+#         ))\
+#         .distinct()
+#
+#     synergies = Synergy.objects\
+#         .filter(synergy_levels__acquired_by_characters=profile.character) \
+#         .prefetch_related(Prefetch(
+#             'synergy_levels',
+#             queryset=SynergyLevel.objects.filter(acquired_by_characters=profile.character)
+#         )) \
+#         .distinct()
+#
+#     context = {
+#         'page_title': f'Umiejętności - {profile.character_name}',
+#         'skills': skills,
+#         'synergies': synergies,
+#     }
+#     if request.user.profile.character_status != 'gm' and profile_id != 0:
+#         return redirect('home:dupa')
+#     else:
+#         return render(request, 'characters/skills_sheet.html', context)
+
+
+# @query_debugger
+# @login_required
+# def skills_sheets_for_gm_view(request):
+#     profile = request.user.profile
+#     characters = Character.objects.all().select_related('profile')
+#
+#     context = {
+#         'page_title': 'Umiejętności graczy',
+#         'characters': characters
+#     }
+#     if profile.character_status == 'gm':
+#         return render(request, 'characters/skills_sheets_for_gm.html', context)
+#     else:
+#         return redirect('home:dupa')
