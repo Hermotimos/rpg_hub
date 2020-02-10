018558ee1d1b005c139c4bddffb154d6be50405a
Hermotimos <lukas.kozicki@gmail.com>
2020-02-10 17:17:31 +0100

    reordering in characters.urls

--- characters/urls.py
+++ characters/urls.py
@@ -5,13 +5,14 @@ from rpg_project.utils import query_debugger
 
 app_name = 'characters'
 urlpatterns = [
-    # path('tricks/', views.character_tricks_view, name='tricks'),
+    # CBV
     path('tricks/', query_debugger(views.CharacterTricksView.as_view()), name='tricks'),
-    # path('character-skills:<int:profile_id>/', views.character_skills_view, name='character-skills'),
     path('character-skills:<int:profile_id>/', query_debugger(views.CharacterSkillsView.as_view()),
          name='character-skills'),
-    # path('character-all-skills-for-gm/', views.skills_sheets_for_gm_view, name='character-all-skills-for-gm'),
     path('character-all-skills-for-gm/', query_debugger(views.CharacterAllSkillsForGmView.as_view()),
          name='character-all-skills-for-gm'),
-
+    # FBV
+    # path('tricks/', views.character_tricks_view, name='tricks'),
+    # path('character-skills:<int:profile_id>/', views.character_skills_view, name='character-skills'),
+    # path('character-all-skills-for-gm/', views.skills_sheets_for_gm_view, name='character-all-skills-for-gm'),
 ]
