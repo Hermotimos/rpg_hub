1dd6e944d150c77e7a6c842f30c9bd73c97123f5
Hermotimos <lukas.kozicki@gmail.com>
2020-03-25 23:09:50 +0100

    restored some stugg from characters app - needed

--- characters/urls.py
+++ characters/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from characters import views
 from rpg_project.utils import query_debugger
 
-# app_name = 'characters'
-# urlpatterns = [
+app_name = 'characters'
+urlpatterns = [
     # CBV
     # path('tricks/', query_debugger(views.CharacterTricksView.as_view()), name='tricks'),
     # path('character-skills:<int:profile_id>/', query_debugger(views.CharacterSkillsView.as_view()),
@@ -15,4 +15,4 @@ from rpg_project.utils import query_debugger
     # path('tricks/', views.character_tricks_view, name='tricks'),
     # path('character-skills:<int:profile_id>/', views.character_skills_view, name='character-skills'),
     # path('character-all-skills-for-gm/', views.skills_sheets_for_gm_view, name='character-all-skills-for-gm'),
-# ]
+]
