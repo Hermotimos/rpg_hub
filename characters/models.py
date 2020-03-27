fc7baaf73f5096cbd61c4b36fb61323a659e873a
Hermotimos <lukas.kozicki@gmail.com>
2020-03-27 17:38:26 +0100

    minor

--- characters/models.py
+++ characters/models.py
@@ -1,9 +1,2 @@
 from django.db import models
 
-from knowledge.models import KnowledgePacket
-from imaginarion.models import Picture
-from rules.models import SkillLevel, SynergyLevel
-from users.models import Profile
-
-
-
