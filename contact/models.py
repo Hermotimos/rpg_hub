c0df85b4a7c04c300cc9b01258396fd74c448fdc
Hermotimos <lukas.kozicki@gmail.com>
2020-04-03 18:30:53 +0200

    ordering in contact.models.py

--- contact/models.py
+++ contact/models.py
@@ -1,13 +1,17 @@
 import datetime
+
+from PIL import Image
 from django.db import models
 from django.db.models.signals import post_save
+
 from users.models import User
-from PIL import Image
 
 
 class Demand(models.Model):
-    author = models.ForeignKey(User, related_name='authored_demands', on_delete=models.CASCADE)
-    addressee = models.ForeignKey(User, related_name='received_demands', on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, related_name='authored_demands',
+                               on_delete=models.CASCADE)
+    addressee = models.ForeignKey(to=User, related_name='received_demands',
+                                  on_delete=models.CASCADE)
     text = models.TextField(max_length=4000)
     date_created = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='contact_pics', blank=True, null=True)
@@ -17,7 +21,8 @@ class Demand(models.Model):
         ordering = ['-date_created']
 
     def __str__(self):
-        return f'{self.text[0:50]}...' if len(str(self.text)) > 50 else self.text
+        text = self.text
+        return f'{text[0:50] + "..." if len(str(text)) > 50 else text}'
 
     def save(self, *args, **kwargs):
         first_save = True if not self.pk else False
@@ -31,11 +36,13 @@ class Demand(models.Model):
 
 
 class DemandAnswer(models.Model):
-    demand = models.ForeignKey(Demand, related_name='demand_answers', on_delete=models.CASCADE)
-    author = models.ForeignKey(User, related_name='demand_answers', on_delete=models.CASCADE)
+    demand = models.ForeignKey(to=Demand, related_name='demand_answers',
+                               on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, related_name='demand_answers',
+                               on_delete=models.CASCADE)
     text = models.TextField(max_length=4000)
     date_posted = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(blank=True, null=True, upload_to='contact_pics')
+    image = models.ImageField(upload_to='contact_pics', blank=True, null=True)
 
     class Meta:
         ordering = ['-date_posted']
@@ -51,18 +58,21 @@ class DemandAnswer(models.Model):
                 img.save(self.image.path)
 
     def __str__(self):
-        return f'{self.text[0:100] if len(str(self.text)) > 100 else self.text}...'
+        text = self.text
+        return f'{text[0:100] + "..." if len(str(text)) > 100 else text}'
 
 
 class Plan(models.Model):
-    author = models.ForeignKey(User, related_name='plans', on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, related_name='plans',
+                               on_delete=models.CASCADE)
     inform_gm = models.BooleanField(default=False)
     text = models.TextField(max_length=4000)
     date_created = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='contact_pics', blank=True, null=True)
 
     def __str__(self):
-        return f'{self.text[0:100] if len(str(self.text)) > 100 else self.text}...'
+        text = self.text
+        return f'{text[0:100] + "..." if len(str(text)) > 100 else text}'
 
     def save(self, *args, **kwargs):
         first_save = True if not self.pk else False
@@ -78,14 +88,16 @@ class Plan(models.Model):
         ordering = ['-date_created']
 
 
-# ---------------------------------------------------------------------------------------------------------------------
-# -------------------------------------------------- SIGNALS ---------------------------------------------------------
-# ---------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# ----------------------------------- SIGNALS ---------------------------------
+# -----------------------------------------------------------------------------
 
 
 def delete_if_doubled(sender, instance, **kwargs):
     time_span = datetime.datetime.now() - datetime.timedelta(minutes=2)
-    doubled = DemandAnswer.objects.filter(text=instance.text, author=instance.author, date_posted__gte=time_span)
+    doubled = DemandAnswer.objects.filter(text=instance.text,
+                                          author=instance.author,
+                                          date_posted__gte=time_span)
     if doubled.count() > 1:
         instance.delete()
 
