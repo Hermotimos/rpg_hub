799edb8f658ef606b8142d6a5db29ce53835285c
Hermotimos <lukas.kozicki@gmail.com>
2020-04-04 16:51:58 +0200

    contact tests corrected

--- contact/tests/test_demands_create_view.py
+++ contact/tests/test_demands_create_view.py
@@ -8,16 +8,20 @@ from users.models import User
 
 class DemandsCreateTest(TestCase):
     def setUp(self):
-        self.user1 = User.objects.create_user(username='user1', password='pass1111')
-        self.user2 = User.objects.create_user(username='user2', password='pass1111')
+        self.user1 = User.objects.create_user(username='u1', password='ps1')
+        self.user2 = User.objects.create_user(username='u2', password='ps1')
+        self.user1.profile.status = 'active_player'
         self.user2.profile.status = 'active_player'
-        self.user2.save()
+        self.user1.profile.save()
+        self.user2.profile.save()
+        
         self.url = reverse('contact:demands-create')
 
     def test_login_required(self):
         redirect_url = reverse('users:login') + '?next=' + self.url
         response = self.client.get(self.url)
-        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+        self.assertRedirects(response, redirect_url, status_code=302,
+                             target_status_code=200)
 
     def test_get(self):
         self.client.force_login(self.user1)
@@ -39,16 +43,12 @@ class DemandsCreateTest(TestCase):
         form = response.context.get('form')
         self.assertIsInstance(form, DemandsCreateForm)
 
-    # ---IMPORTANT NOTE---
-    # Manual test - OK. Tests fails when there's a custom queryset for 'addressee' field in the form.
-    # When the form is left without custom queryset for 'addressee' field, the test passes.
     def test_valid_post_data(self):
         self.client.force_login(self.user1)
-        self.demand_ = {
-            'addressee': '2',  # by ForeignKeyField pk or id has to be provided
+        data = {
+            'addressee': self.user2.id,  # by ForeignKeyField pk or id required
             'text': 'Demand2',
         }
-        data = self.demand_
         self.assertFalse(Demand.objects.exists())
         self.client.post(self.url, data)
         self.assertTrue(Demand.objects.exists())
@@ -77,7 +77,7 @@ class DemandsCreateTest(TestCase):
         self.assertFalse(Demand.objects.exists())
 
 
-#######################################################################################################################
+###############################################################################
 # HINT FOR DEBUGGING FORMS' TESTS:
 # If test fails, see list of errors for details. Compare it with data sent.
 #     data = {
@@ -85,7 +85,9 @@ class DemandsCreateTest(TestCase):
 #     }
 #     print('DATA:\n', data)
 #     response = self.client.post(self.url, data)
-#     form1 = response.context.get('form1_name')    # by one form usually form_name is just 'form' - depends how in view
+#     # by one form usually form_name is just 'form' - depends how in view
+#     form1 = response.context.get('form1_name')
 #     form2 = response.context.get('form2_name')
-#     print('ERRORS:\n', form1.errors)        # To be run multiple times as only first error in form gets printed
-#     print('ERRORS:\n', form2.errors)        # To be run multiple times as only first error in form gets printed
+#     # To be run multiple times as only first error in form gets printed:
+#     print('ERRORS:\n', form1.errors)
+#     print('ERRORS:\n', form2.errors)
