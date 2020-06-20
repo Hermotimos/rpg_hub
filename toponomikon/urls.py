317291acbfa484f3b1816ea5330e9b51244c0746
Hermotimos <lukas.kozicki@gmail.com>
2020-06-20 19:47:24 +0200

    model Location replaced SpecificLocation & GeneralLocation - completed

--- toponomikon/urls.py
+++ toponomikon/urls.py
@@ -5,6 +5,5 @@ from toponomikon import views
 app_name = 'toponomikon'
 urlpatterns = [
     path('', views.toponomikon_main_view, name='main'),
-    path('gen-loc:<int:gen_loc_id>/', views.toponomikon_general_location_view, name='general-location'),
-    path('spec-loc:<int:spec_loc_id>/', views.toponomikon_specific_location_view, name='specific-location'),
+    path('<str:loc_name>/', views.toponomikon_location_view, name='location'),
 ]
--- toponomikon/views.py
+++ toponomikon/views.py
@@ -26,19 +26,19 @@ def toponomikon_main_view(request):
     locs_lvl_1 = locs_lvl_1\
         .prefetch_related(Prefetch('locations', queryset=locs_lvl_2))\
         .select_related('main_image', 'location_type__default_img')\
-        .distinct()\
-        .annotate(known_only_indirectly=Case(
-            When(
-                Q(known_indirectly=profile) & ~Q(known_directly=profile),
-                then=Value(1)
-            ),
-            default=Value(0),
-            output_field=IntegerField()
-        ))
+        .distinct()
+        # .annotate(known_only_indirectly=Case(
+        #     When(
+        #         Q(known_indirectly=profile) & ~Q(known_directly=profile),
+        #         then=Value(1)
+        #     ),
+        #     default=Value(0),
+        #     output_field=IntegerField()
+        # ))
 
     context = {
         'page_title': 'Toponomikon',
-        'gen_locs': locs_lvl_1,
+        'locs_lvl_1': locs_lvl_1.distinct(),
     }
 
     # for gen_loc in GeneralLocation.objects.all():
@@ -82,40 +82,188 @@ def toponomikon_main_view(request):
     return render(request, 'toponomikon/toponomikon_main.html', context)
 
 
+#
+# @login_required
+# def toponomikon_general_location_view(request, gen_loc_id):
+#     profile = request.user.profile
+#     gen_loc = get_object_or_404(Location, id=gen_loc_id)
+#
+#     gen_loc_known_directly = gen_loc.known_directly.all()
+#     gen_loc_known_indirectly = gen_loc.known_indirectly.all()
+#     allowed = (gen_loc_known_directly | gen_loc_known_indirectly)
+#
+#     # TABS
+#     if profile.status == 'gm':
+#         spec_locs = gen_loc.locations.all()
+#         kn_packets = gen_loc.knowledge_packets.all()
+#         only_indirectly = False
+#     else:
+#         known_directly = gen_loc.locations.filter(known_directly=profile)
+#         known_indirectly = gen_loc.locations.filter(known_indirectly=profile).exclude(id__in=known_directly)
+#         spec_locs = (known_directly | known_indirectly)
+#         kn_packets = gen_loc.knowledge_packets.filter(acquired_by=profile)
+#         only_indirectly = True \
+#             if profile in gen_loc_known_indirectly\
+#             and profile not in gen_loc_known_directly \
+#             else False
+#
+#     spec_locs = spec_locs\
+#         .select_related('main_image')\
+#         .distinct()\
+#         .annotate(known_only_indirectly=Case(
+#             When(
+#                 Q(known_indirectly=profile) & ~Q(known_directly=profile),
+#                 then=Value(1)),
+#             default=Value(0),
+#             output_field=IntegerField()
+#         ))
+#
+#     # INFORM LOCATION
+#     # dict(request.POST).items() == < QueryDict: {
+#     #     'csrfmiddlewaretoken': ['KcoYDwb7r86Ll2SdQUNrDCKs...'],
+#     #     '2': ['on'],
+#     #     'location': ['']
+#     # } >
+#     if request.method == 'POST' and 'location' in request.POST:
+#         data = dict(request.POST)
+#         informed_ids = [k for k, v_list in data.items() if 'on' in v_list]
+#         gen_loc.known_indirectly.add(*informed_ids)
+#
+#         send_emails(request, informed_ids, location=gen_loc)
+#         if informed_ids:
+#             messages.info(request, f'Poinformowano wybrane postacie!')
+#
+#     # INFORM KNOWLEDGE PACKETS
+#     # dict(request.POST).items() == < QueryDict: {
+#     #   'csrfmiddlewaretoken': ['42GqawP0aa5WOfpuTkKixYsROBaKSQng...'],
+#     #   '2': ['on'],
+#     #   'kn_packet': ['38']
+#     # } >
+#     elif request.method == 'POST' and 'kn_packet' in request.POST:
+#         data = dict(request.POST)
+#         informed_ids = [k for k, v_list in data.items() if 'on' in v_list]
+#         kn_packet_id = data['kn_packet'][0]
+#         kn_packet = KnowledgePacket.objects.get(id=kn_packet_id)
+#         kn_packet.acquired_by.add(*informed_ids)
+#
+#         send_emails(request, informed_ids, kn_packet=kn_packet)
+#         if informed_ids:
+#             messages.info(request, f'Poinformowano wybrane postacie!')
+#
+#     context = {
+#         # General
+#         'page_title': gen_loc.name,
+#         'gen_loc': gen_loc,
+#         'only_indirectly': only_indirectly,
+#         # Tabs
+#         'kn_packets': kn_packets,
+#         'spec_locs': spec_locs,
+#         'pictures': gen_loc.pictures.all(),
+#     }
+#     if profile in allowed or profile.status == 'gm':
+#         return render(request, 'toponomikon/general_location.html', context)
+#     else:
+#         return redirect('home:dupa')
+#
+#
+#
+# @login_required
+# def toponomikon_specific_location_view(request, spec_loc_id):
+#     profile = request.user.profile
+#     spec_loc = get_object_or_404(Location, id=spec_loc_id)
+#
+#     spec_loc_known_directly = spec_loc.known_directly.all()
+#     spec_loc_known_indirectly = spec_loc.known_indirectly.all()
+#     allowed = (spec_loc_known_directly | spec_loc_known_indirectly)
+#
+#     if profile.status == 'gm':
+#         kn_packets = spec_loc.knowledge_packets.all()
+#         only_indirectly = False
+#     else:
+#         kn_packets = spec_loc.knowledge_packets.filter(acquired_by=profile)
+#         only_indirectly = True \
+#             if profile in spec_loc_known_indirectly\
+#             and profile not in spec_loc_known_directly \
+#             else False
+#
+#     # INFORM LOCATION
+#     # dict(request.POST).items() == < QueryDict: {
+#     #     'csrfmiddlewaretoken': ['KcoYDwb7r86Ll2SdQUNrDCKs...'],
+#     #     '2': ['on'],
+#     #     'location': ['']
+#     # } >
+#     if request.method == 'POST' and 'location' in request.POST:
+#         data = dict(request.POST)
+#         informed_ids = [k for k, v_list in data.items() if 'on' in v_list]
+#         spec_loc.known_indirectly.add(*informed_ids)
+#
+#         send_emails(request, informed_ids, location=spec_loc)
+#         if informed_ids:
+#             messages.info(request, f'Poinformowano wybrane postacie!')
+#
+#     # INFORM KNOWLEDGE PACKETS
+#     # dict(request.POST).items() == < QueryDict: {
+#     #   'csrfmiddlewaretoken': ['42GqawP0aa5WOfpuTkKixYsROBaKSQng...'],
+#     #   '2': ['on'],
+#     #   'kn_packet': ['38']
+#     # } >
+#
+#     elif request.method == 'POST' and 'kn_packet' in request.POST:
+#         data = dict(request.POST)
+#         informed_ids = [k for k, v_list in data.items() if 'on' in v_list]
+#         kn_packet_id = data['kn_packet'][0]
+#         kn_packet = KnowledgePacket.objects.get(id=kn_packet_id)
+#         kn_packet.acquired_by.add(*informed_ids)
+#
+#         send_emails(request, informed_ids, kn_packet=kn_packet)
+#         if informed_ids:
+#             messages.info(request, f'Poinformowano wybrane postacie!')
+#
+#     context = {
+#         # General
+#         'page_title': spec_loc.name,
+#         'spec_loc': spec_loc,
+#         'only_indirectly': only_indirectly,
+#         # Tabs
+#         'kn_packets': kn_packets,
+#         'pictures': spec_loc.pictures.all(),
+#     }
+#     if profile in allowed or profile.status == 'gm':
+#         return render(request, 'toponomikon/specific_location.html', context)
+#     else:
+#         return redirect('home:dupa')
+
 
 @login_required
-def toponomikon_general_location_view(request, gen_loc_id):
+def toponomikon_location_view(request, loc_name):
     profile = request.user.profile
-    gen_loc = get_object_or_404(Location, id=gen_loc_id)
+    location = get_object_or_404(Location, name=loc_name)
     
-    gen_loc_known_directly = gen_loc.known_directly.all()
-    gen_loc_known_indirectly = gen_loc.known_indirectly.all()
-    allowed = (gen_loc_known_directly | gen_loc_known_indirectly)
-
+    known_directly_to = location.known_directly.all()
+    known_indirectly_to = location.known_indirectly.all()
+    allowed = (known_directly_to | known_indirectly_to)
+    
     # TABS
     if profile.status == 'gm':
-        spec_locs = gen_loc.locations.all()
-        kn_packets = gen_loc.knowledge_packets.all()
-        only_indirectly = False
+        locations = location.locations.all()
+        kn_packets = location.knowledge_packets.all()
     else:
-        known_directly = gen_loc.locations.filter(known_directly=profile)
-        known_indirectly = gen_loc.locations.filter(known_indirectly=profile).exclude(id__in=known_directly)
-        spec_locs = (known_directly | known_indirectly)
-        kn_packets = gen_loc.knowledge_packets.filter(acquired_by=profile)
-        only_indirectly = True \
-            if profile in gen_loc_known_indirectly\
-            and profile not in gen_loc_known_directly \
-            else False
-
-    spec_locs = spec_locs\
-        .select_related('main_image')\
-        .distinct()\
+        known_directly = location.locations.filter(known_directly=profile)
+        known_indirectly = location.locations.filter(
+            known_indirectly=profile).exclude(id__in=known_directly)
+        locations = (known_directly | known_indirectly)
+        kn_packets = location.knowledge_packets.filter(acquired_by=profile)
+    
+    locations = locations \
+        .select_related('main_image') \
+        .distinct() \
         .annotate(known_only_indirectly=Case(
             When(
                 Q(known_indirectly=profile) & ~Q(known_directly=profile),
-                then=Value(1)),
+                then=Value(1)
+            ),
             default=Value(0),
-            output_field=IntegerField()
+            output_field=IntegerField(),
         ))
     
     # INFORM LOCATION
@@ -127,12 +275,12 @@ def toponomikon_general_location_view(request, gen_loc_id):
     if request.method == 'POST' and 'location' in request.POST:
         data = dict(request.POST)
         informed_ids = [k for k, v_list in data.items() if 'on' in v_list]
-        gen_loc.known_indirectly.add(*informed_ids)
+        location.known_indirectly.add(*informed_ids)
         
-        send_emails(request, informed_ids, location=gen_loc)
+        send_emails(request, informed_ids, location=location)
         if informed_ids:
             messages.info(request, f'Poinformowano wybrane postacie!')
-
+    
     # INFORM KNOWLEDGE PACKETS
     # dict(request.POST).items() == < QueryDict: {
     #   'csrfmiddlewaretoken': ['42GqawP0aa5WOfpuTkKixYsROBaKSQng...'],
@@ -145,90 +293,24 @@ def toponomikon_general_location_view(request, gen_loc_id):
         kn_packet_id = data['kn_packet'][0]
         kn_packet = KnowledgePacket.objects.get(id=kn_packet_id)
         kn_packet.acquired_by.add(*informed_ids)
-
+        
         send_emails(request, informed_ids, kn_packet=kn_packet)
         if informed_ids:
             messages.info(request, f'Poinformowano wybrane postacie!')
 
+    page_title = location.name + ' (znasz z opowieści)' \
+        if profile not in location.known_directly.all() else location.name
+    
     context = {
         # General
-        'page_title': gen_loc.name,
-        'gen_loc': gen_loc,
-        'only_indirectly': only_indirectly,
+        'page_title': page_title,
+        'location': location,
         # Tabs
         'kn_packets': kn_packets,
-        'spec_locs': spec_locs,
-        'pictures': gen_loc.pictures.all(),
+        'locations': locations,
+        'pictures': location.pictures.all(),
     }
     if profile in allowed or profile.status == 'gm':
-        return render(request, 'toponomikon/general_location.html', context)
-    else:
-        return redirect('home:dupa')
-
-
-
-@login_required
-def toponomikon_specific_location_view(request, spec_loc_id):
-    profile = request.user.profile
-    spec_loc = get_object_or_404(Location, id=spec_loc_id)
-    
-    spec_loc_known_directly = spec_loc.known_directly.all()
-    spec_loc_known_indirectly = spec_loc.known_indirectly.all()
-    allowed = (spec_loc_known_directly | spec_loc_known_indirectly)
-
-    if profile.status == 'gm':
-        kn_packets = spec_loc.knowledge_packets.all()
-        only_indirectly = False
-    else:
-        kn_packets = spec_loc.knowledge_packets.filter(acquired_by=profile)
-        only_indirectly = True \
-            if profile in spec_loc_known_indirectly\
-            and profile not in spec_loc_known_directly \
-            else False
-
-    # INFORM LOCATION
-    # dict(request.POST).items() == < QueryDict: {
-    #     'csrfmiddlewaretoken': ['KcoYDwb7r86Ll2SdQUNrDCKs...'],
-    #     '2': ['on'],
-    #     'location': ['']
-    # } >
-    if request.method == 'POST' and 'location' in request.POST:
-        data = dict(request.POST)
-        informed_ids = [k for k, v_list in data.items() if 'on' in v_list]
-        spec_loc.known_indirectly.add(*informed_ids)
-    
-        send_emails(request, informed_ids, location=spec_loc)
-        if informed_ids:
-            messages.info(request, f'Poinformowano wybrane postacie!')
-
-    # INFORM KNOWLEDGE PACKETS
-    # dict(request.POST).items() == < QueryDict: {
-    #   'csrfmiddlewaretoken': ['42GqawP0aa5WOfpuTkKixYsROBaKSQng...'],
-    #   '2': ['on'],
-    #   'kn_packet': ['38']
-    # } >
-
-    elif request.method == 'POST' and 'kn_packet' in request.POST:
-        data = dict(request.POST)
-        informed_ids = [k for k, v_list in data.items() if 'on' in v_list]
-        kn_packet_id = data['kn_packet'][0]
-        kn_packet = KnowledgePacket.objects.get(id=kn_packet_id)
-        kn_packet.acquired_by.add(*informed_ids)
-    
-        send_emails(request, informed_ids, kn_packet=kn_packet)
-        if informed_ids:
-            messages.info(request, f'Poinformowano wybrane postacie!')
-
-    context = {
-        # General
-        'page_title': spec_loc.name,
-        'spec_loc': spec_loc,
-        'only_indirectly': only_indirectly,
-        # Tabs
-        'kn_packets': kn_packets,
-        'pictures': spec_loc.pictures.all(),
-    }
-    if profile in allowed or profile.status == 'gm':
-        return render(request, 'toponomikon/specific_location.html', context)
+        return render(request, 'toponomikon/this_location.html', context)
     else:
         return redirect('home:dupa')
