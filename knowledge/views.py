fdf65c94985729a6274b898ef3b77c286c1a7854
Hermotimos <lukas.kozicki@gmail.com>
2020-07-21 23:00:53 +0200

    upgraded skills_view in knowledge app

--- knowledge/views.py
+++ knowledge/views.py
@@ -12,15 +12,28 @@ from rules.models import SkillLevel, Skill, TheologySkill, BooksSkill
 
 
 def prepare_skills(profile, skill_model_name):
-    skills = Skill.objects.all()
+    skill_model = apps.get_app_config('rules').get_model(skill_model_name)
+    skills = skill_model.objects.all()
+
+    # Filter skills queryset according to possible proxy usage
+    skill_proxies = [
+        m for m in apps.get_app_config('rules').get_models()
+        if issubclass(m, Skill) and m.__name__ != Skill.__name__
+    ]
+    page_title = skill_model._meta.verbose_name
+    if skill_model == Skill:
+        page_title = 'Almanach'
+        for proxy in skill_proxies:
+            skills = skills.exclude(id__in=proxy.objects.all())
     
+    # Filter skills queryset according to profile's permissions
     if profile.status == 'gm':
         kn_packets = KnowledgePacket.objects.all()
         skill_levels = SkillLevel.objects.all()
     else:
         kn_packets = profile.knowledge_packets.all()
         skill_levels = SkillLevel.objects.filter(acquired_by=profile)
-    
+        
     skills = skills.filter(knowledge_packets__in=kn_packets)
     skills = skills.prefetch_related(
         Prefetch('knowledge_packets', queryset=kn_packets),
@@ -29,33 +42,13 @@ def prepare_skills(profile, skill_model_name):
     )
     skills = skills.distinct()
     
-    books_filter = Q(id__in=BooksSkill.objects.all())
-    theology_filter = Q(id__in=TheologySkill.objects.all())
-    
-    if skill_model_name == BooksSkill.__name__:
-        skills = skills.filter(books_filter)
-    elif skill_model_name == TheologySkill.__name__:
-        skills = skills.filter(theology_filter)
-    else:
-        skills = skills.exclude(theology_filter)
-        skills = skills.exclude(books_filter)
-
-    skill_models = [
-        m for m in apps.get_app_config('rules').get_models()
-        if m == Skill or issubclass(m, Skill)
-    ]
-    skill_model_verbose_names = {
-        m.__name__: m._meta.verbose_name for m in skill_models
-    }
-    page_title = skill_model_verbose_names.get(skill_model_name) or 'Almanach'
-    
     return page_title, skills
 
 
 @login_required
 def skills_view(request, skill_model):
     page_title, skills = prepare_skills(request.user.profile, skill_model)
-
+    
     # INFORM FORM
     if request.method == 'POST' and 'kn_packet' in request.POST:
         # dict(request.POST).items() == < QueryDict: {
@@ -68,16 +61,15 @@ def skills_view(request, skill_model):
         kn_packet_id = data['kn_packet'][0]
         kn_packet = KnowledgePacket.objects.get(id=kn_packet_id)
         kn_packet.acquired_by.add(*informed_ids)
-    
+        
         send_emails(request, informed_ids, kn_packet=kn_packet)
         messages.info(request, f'Poinformowano wybrane postaci!')
-
+    
     context = {
         'page_title': page_title,
         'skills': skills,
     }
     return render(request, 'knowledge/skills_with_kn_packets.html', context)
-
 
 #
 # @login_required
