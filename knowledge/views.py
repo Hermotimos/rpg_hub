ce7e89b69036d3fea6d811ad4c34456b9860ad80
Hermotimos <lukas.kozicki@gmail.com>
2020-08-14 22:07:15 +0200

    make 'Teologia' first again

--- knowledge/views.py
+++ knowledge/views.py
@@ -1,12 +1,29 @@
 from django.apps import apps
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Prefetch
+from django.db.models import Prefetch, Q, ExpressionWrapper, BooleanField
 from django.shortcuts import render
 
 from knowledge.models import KnowledgePacket
 from rpg_project.utils import handle_inform_form
 from rules.models import SkillLevel, Skill
+
+
+def custom_sort(skills_qs):
+    # Custom sorting to bring specific skills to the front of the queryset:
+    # Source: https://stackoverflow.com/questions/11622501/order-query-results-by-startswith-match
+    
+    search_terms = [
+        'Teolog',
+    ]
+    for search_term in search_terms:
+        # Use expression evaluation to annotate objects; order by annotation
+        expression = Q(name__startswith=search_term)
+        is_match = ExpressionWrapper(expression, output_field=BooleanField())
+        skills_qs = skills_qs.annotate(annotation=is_match)
+        # Order by the annotated field in reverse, so `True` is first (0 < 1).
+        skills_qs = skills_qs.order_by('-annotation', 'name')
+    
+    return skills_qs
 
 
 @login_required
@@ -40,6 +57,7 @@ def knowledge_packets_in_skills_view(request, model_name):
         'knowledge_packets__pictures',
     )
     skills = skills.distinct()
+    skills = custom_sort(skills)
 
     if request.method == 'POST':
         handle_inform_form(request)
