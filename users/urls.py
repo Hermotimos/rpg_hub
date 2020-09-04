f7f3b7a0afda282e93806d1162e529bacb64e6bf
Hermotimos <lukas.kozicki@gmail.com>
2020-09-04 15:36:21 +0200

    login and logout views - generics

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,8 @@ from users import views
 app_name = 'users'
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('profile/', views.profile_view, name='profile'),
     path('profile/change_password/', views.change_password_view, name='change-password'),
     
