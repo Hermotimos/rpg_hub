b1d8422b57d20e2df6252a3ea60bf56e8d3f155a
Hermotimos <lukas.kozicki@gmail.com>
2020-09-20 18:58:32 +0200

    reordering in Toponomikon views

--- toponomikon/views.py
+++ toponomikon/views.py
@@ -10,15 +10,16 @@ from toponomikon.models import Location, LocationType, PrimaryLocation, \
 @login_required
 def toponomikon_main_view(request):
     profile = request.user.profile
-    all_locs = Location.objects.values('name')
-    primary_locs = PrimaryLocation.objects.prefetch_related('locations')
-    
-    if profile.status != 'gm':
+    if profile.status == 'gm':
+        all_locs = Location.objects.values('name')
+        primary_locs = PrimaryLocation.objects.prefetch_related('locations')
+    else:
         known_dir = profile.locs_known_directly.all()
         known_indir = profile.locs_known_indirectly.all()
+        known_only_indir = known_indir.exclude(id__in=known_dir)
         all_known = (known_dir | known_indir)
         
-        all_locs = all_locs.filter(id__in=all_known)
+        all_locs = Location.objects.values('name').filter(id__in=all_known)
         primary_locs = PrimaryLocation.objects.filter(id__in=all_known)
         primary_locs = primary_locs.prefetch_related(
             Prefetch(
@@ -26,14 +27,14 @@ def toponomikon_main_view(request):
                 queryset=SecondaryLocation.objects.filter(id__in=all_known)
             )
         )
-        known_only_indirectly = known_indir.exclude(id__in=known_dir)
         primary_locs = primary_locs.annotate(
             only_indirectly=Case(
-                When(id__in=known_only_indirectly, then=Value(1)),
+                When(id__in=known_only_indir, then=Value(1)),
                 default=Value(0),
                 output_field=IntegerField(),
             ),
         )
+        
     primary_locs = primary_locs.select_related('main_image')
     
     context = {
@@ -47,19 +48,17 @@ def toponomikon_main_view(request):
 @login_required
 def toponomikon_location_view(request, loc_name):
     profile = request.user.profile
-    known_directly = profile.locs_known_directly.all()
-    known_indirectly = profile.locs_known_indirectly.all()
-    known_only_indirectly = known_indirectly.exclude(id__in=known_directly)
-    known_all = (known_directly | known_indirectly).distinct()
+    known_dir = profile.locs_known_directly.all()
+    known_indir = profile.locs_known_indirectly.all()
+    known_only_indir = known_indir.exclude(id__in=known_dir)
+    known_all = (known_dir | known_indir).distinct()
     
-    # Get this_location with prefetched data and annotations
-    this_location = Location.objects.select_related('main_image')
+    # THIS LOCATION
+    prep = Location.objects.select_related('main_image', 'audio')
     if profile.status == 'gm':
-        this_location = this_location.prefetch_related(
-            'knowledge_packets__pictures',
-        )
+        prep = prep.prefetch_related('knowledge_packets__pictures', 'pictures')
     else:
-        this_location = this_location.prefetch_related(
+        prep = prep.prefetch_related(
             Prefetch(
                 'knowledge_packets',
                 queryset=profile.knowledge_packets.all()
@@ -67,20 +66,20 @@ def toponomikon_location_view(request, loc_name):
             'knowledge_packets__pictures',
             'pictures',
         )
-    this_location = this_location.annotate(
-        only_indirectly=Case(
-            When(id__in=known_only_indirectly, then=Value(1)),
-            default=Value(0),
-            output_field=IntegerField(),
+        prep = prep.annotate(
+            only_indirectly=Case(
+                When(id__in=known_only_indir, then=Value(1)),
+                default=Value(0),
+                output_field=IntegerField(),
+            )
         )
-    )
-    this_location = this_location.get(name=loc_name)
-    
+        
+    this_location = prep.get(name=loc_name)
     page_title = this_location.name
-    if this_location in known_only_indirectly:
+    if this_location in known_only_indir:
         page_title += ' (znasz z opowieści)'
     
-    # TABS
+    # LOCATIONS TAB
     if profile.status == 'gm':
         locations = this_location.locations.prefetch_related('locations')
     else:
@@ -90,7 +89,7 @@ def toponomikon_location_view(request, loc_name):
         )
         locations = locations.annotate(
             only_indirectly=Case(
-                When(id__in=known_only_indirectly, then=Value(1)),
+                When(id__in=known_only_indir, then=Value(1)),
                 default=Value(0),
                 output_field=IntegerField()
             )
@@ -107,40 +106,6 @@ def toponomikon_location_view(request, loc_name):
     if request.method == 'POST':
         handle_inform_form(request)
         
-    # # INFORM LOCATION
-    # # dict(request.POST).items() == < QueryDict: {
-    # #     'csrfmiddlewaretoken': ['KcoYDwb7r86Ll2SdQUNrDCKs...'],
-    # #     '2': ['on'],
-    # #     'location': ['77']
-    # # } >
-    # if request.method == 'POST' and 'location' in request.POST:
-    #     data = dict(request.POST)
-    #     informed_ids = [k for k, v_list in data.items() if 'on' in v_list]
-    #     location_id = data['location'][0]
-    #     location = Location.objects.get(id=location_id)
-    #     location.known_indirectly.add(*informed_ids)
-    #
-    #     send_emails(request, informed_ids, location=this_location)
-    #     if informed_ids:
-    #         messages.info(request, f'Poinformowano wybrane postaci!')
-    #
-    # # INFORM KNOWLEDGE PACKETS
-    # # dict(request.POST).items() == < QueryDict: {
-    # #   'csrfmiddlewaretoken': ['42GqawP0aa5WOfpuTkKixYsROBaKSQng...'],
-    # #   '2': ['on'],
-    # #   'kn_packet': ['38']
-    # # } >
-    # elif request.method == 'POST' and 'kn_packet' in request.POST:
-    #     data = dict(request.POST)
-    #     informed_ids = [k for k, v_list in data.items() if 'on' in v_list]
-    #     kn_packet_id = data['kn_packet'][0]
-    #     kn_packet = KnowledgePacket.objects.get(id=kn_packet_id)
-    #     kn_packet.acquired_by.add(*informed_ids)
-    #
-    #     send_emails(request, informed_ids, kn_packet=kn_packet)
-    #     if informed_ids:
-    #         messages.info(request, f'Poinformowano wybrane postaci!')
-    
     context = {
         'page_title': page_title,
         'this_location': this_location,
