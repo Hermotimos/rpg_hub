068d8e736e59daa802862b6016a7a5e8fb591054
Hermotimos <lukas.kozicki@gmail.com>
2020-09-26 12:52:09 +0200

    migrations

--- rules/migrations/0006_auto_20200926_1125.py
+++ rules/migrations/0006_auto_20200926_1125.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0006_auto_20200926_1107'),
+        ('users', '0001_initial'),
         ('rules', '0005_auto_20200926_1122'),
     ]
 
--- rules/migrations/0008_auto_20200926_1129.py
+++ rules/migrations/0008_auto_20200926_1129.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0006_auto_20200926_1107'),
+        ('users', '0001_initial'),
         ('rules', '0007_auto_20200926_1128'),
     ]
 
