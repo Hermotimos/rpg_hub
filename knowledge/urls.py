f21906536d18fea166258187a180dfdcac3532cb
Hermotimos <lukas.kozicki@gmail.com>
2020-10-31 14:29:29 +0100

    minor fix

--- knowledge/urls.py
+++ knowledge/urls.py
@@ -5,8 +5,7 @@ from knowledge import views
 
 app_name = 'knowledge'
 urlpatterns = [
-    path('kn-packets-in-skills/<str:model_name>/', views.knowledge_packets_in_skills_view,
+    path('kn-packets-in-skills/<str:model_name>/',
+         views.knowledge_packets_in_skills_view,
          name='knowledge-packets-in-skills'),
-    path('kn-packet/<int:kn_packet_id>/', views.knowledge_packet_view,
-         name='knowledge-packet'),
 ]
