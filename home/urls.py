d5506629139c8444c3f1f3e508191d344d488ed6
Hermotimos <lukas.kozicki@gmail.com>
2020-11-04 20:56:25 +0100

    home view switch from CBS to FBV

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,5 @@ from home import views
 app_name = 'home'
 urlpatterns = [
     path('', views.home_view, name='home'),
-    # path('dupa/', views.dupa_view, name='dupa'),
-    path('dupa/', views.DupaView.as_view(), name='dupa')
+    path('dupa/', views.dupa_view, name='dupa'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,8 +2,6 @@ from random import randrange
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.utils.decorators import method_decorator
-from django.views import View
 
 from users.models import Profile
 
@@ -11,22 +9,19 @@ from users.models import Profile
 @login_required
 def home_view(request):
     profile = request.user.profile
-    rand_profiles = []
-
     if profile.status == 'gm':
         known_profiles = Profile.objects.exclude(id=profile.id)
     # else:
     #     known_profiles = [profile]
-
-        rand_profiles = []
-        rand_nums = []
-        counter = 0
-        while counter < 4:
-            rand = randrange(len(known_profiles))
-            if rand not in rand_nums:
-                rand_profiles.append(known_profiles[rand])
-                rand_nums.append(rand)
-                counter += 1
+    rand_profiles = []
+    rand_nums = []
+    counter = 0
+    while counter < 4:
+        rand = randrange(len(known_profiles))
+        if rand not in rand_nums:
+            rand_profiles.append(known_profiles[rand])
+            rand_nums.append(rand)
+            counter += 1
 
     context = {
         'page_title': 'Hyllemath',
@@ -35,14 +30,8 @@ def home_view(request):
     return render(request, 'home/home.html', context)
 
 
-#
-# @login_required
-# def dupa_view(request):
-#     return render(request, 'home/dupa.html', {'page_title': 'Dupa!'})
+@login_required
+def dupa_view(request):
+    return render(request, 'home/dupa.html', {'page_title': 'Dupa!'})
 
 
-class DupaView(View):
-
-    @method_decorator(login_required)
-    def get(self, request):
-        return render(request, 'home/dupa.html', {'page_title': 'Dupa!'})
