c5d85f42b9741db1c4fc19608d6c0fd2bb46c726
Hermotimos <lukas.kozicki@gmail.com>
2021-01-18 23:45:38 +0100

    minor

--- rpg_project/utils.py
+++ rpg_project/utils.py
@@ -169,7 +169,7 @@ def send_emails(request, profile_ids=None, **kwargs):
         else:
             demand = kwargs['demand']
             subject = f"[RPG] Dezyderat {demand.id} [nowy]"
-            message = f"Dezyderat od {demand.author}:\n" \
+            message = f"{demand.author} wysłał Ci Dezyderat:\n" \
                       f"{request.get_host()}/contact/demands/detail:{demand.id}/\n\n"
             messages.info(request, 'Dezyderat został wysłany!')
         
