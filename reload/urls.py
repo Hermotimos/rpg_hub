a75a6e404cc8317efc2114a6e5f7923fcda0c95a
Hermotimos <lukas.kozicki@gmail.com>
2021-02-20 13:53:38 +0100

    fix

--- reload/urls.py
+++ reload/urls.py
@@ -5,7 +5,7 @@ from reload import views
 app_name = 'reload'
 urlpatterns = [
     path('', views.reload_main_view, name='reload-main'),
-    path('reload-chronicles/', views.reload_history, name='reload-chronicles'),
+    path('reload-chronicles/', views.reload_chronicles, name='reload-chronicles'),
     path('reload-imaginarion/', views.reload_imaginarion, name='reload-imaginarion'),
     path('reload-rules/', views.reload_rules, name='reload-rules'),
     path('reload-toponomikon/', views.reload_toponomikon, name='reload-toponomikon'),
