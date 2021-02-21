6bbb4a339f151a5dd5ac7d91af53460dcf2f45c7
Hermotimos <lukas.kozicki@gmail.com>
2021-02-21 15:54:52 +0100

    fix cognomen not required

--- prosoponomikon/models.py
+++ prosoponomikon/models.py
@@ -48,7 +48,7 @@ from users.models import Profile
 class Character(Model):
     profile = OneToOne(to=Profile, on_delete=CASCADE)
     name = CharField(max_length=100)
-    cognomen = CharField(max_length=250)
+    cognomen = CharField(max_length=250, blank=True, null=True)
     frequented_locations = M2M(
         to=Location,
         related_name='frequented_by_characters',
