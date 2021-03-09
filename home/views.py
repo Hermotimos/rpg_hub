8e375a5dfcedbac901bb2f3996f7f1f05aee4160
Hermotimos <lukas.kozicki@gmail.com>
2021-03-09 19:50:03 +0100

    snippet for character pictures in home page and in Toponomikon

--- home/views.py
+++ home/views.py
@@ -2,39 +2,37 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render
 
+from prosoponomikon.models import Character
 from rpg_project.utils import sample_from_qs
 from toponomikon.models import Location
-from users.models import Profile
 
 
 @login_required
 def home_view(request):
     profile = request.user.profile
-
-    known_profiles = Profile.objects.exclude(id=profile.id)
-    known_profiles = known_profiles.select_related('character')
-    known_profiles = known_profiles.prefetch_related(
-        'character__known_directly', 'character__known_indirectly',
-        'character__name')
+    
+    known_characters = Character.objects.exclude(profile=profile)
+    known_characters = known_characters.select_related('profile')
+    known_characters = known_characters.prefetch_related(
+        'known_directly', 'known_indirectly', 'name')
     
     known_locations = Location.objects.select_related('main_image__image')
     known_locations = known_locations.prefetch_related(
         'known_directly', 'known_indirectly')
 
     if profile.status != 'gm':
-        known_profiles = known_profiles.filter(
-            Q(character__known_directly=profile)
-            | Q(character__known_indirectly=profile))
+        known_characters = known_characters.filter(
+            Q(known_directly=profile) | Q(known_indirectly=profile))
         known_locations = known_locations.filter(
             Q(known_directly=profile) | Q(known_indirectly=profile))
         
     # set() ensures that if len(known) < k, than duplicates will be removed
-    rand_profiles = sample_from_qs(qs=known_profiles, max_size=4)
+    rand_characters = sample_from_qs(qs=known_characters, max_size=4)
     rand_locations = sample_from_qs(qs=known_locations, max_size=4)
 
     context = {
         'page_title': 'Hyllemath',
-        'rand_profiles': rand_profiles,
+        'rand_characters': rand_characters,
         'rand_locations': rand_locations,
     }
     return render(request, 'home/home.html', context)
