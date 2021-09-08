de54ef23b7b58cfee13653b106f48105ccad69d1
Hermotimos <lukas.kozicki@gmail.com>
2021-09-08 16:37:48 +0200

    users admin simplified

--- users/admin.py
+++ users/admin.py
@@ -16,6 +16,7 @@ class ProfileAdminForm(forms.ModelForm):
         WYPEŁNIJ JE W DRUGIEJ TURZE !!!
     </b>
     """
+    
     skills = forms.ModelMultipleChoiceField(
         queryset=Skill.objects.all(),
         required=False,
@@ -65,6 +66,17 @@ class ProfileAdminForm(forms.ModelForm):
         label=format_html(warning),
     )
 
+    fields_and_models = {
+        'skills': Skill,
+        'synergies': Synergy,
+        'klasses': Klass,
+        'elite_professions': EliteProfession,
+        'elite_klasses': EliteKlass,
+        'plates': Plate,
+        'shields': Shield,
+        'weapons': Weapon,
+    }
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         id_ = self.instance.id
@@ -72,52 +84,19 @@ class ProfileAdminForm(forms.ModelForm):
             # If this is "New" form, avoid filling "virtual" field with data
             return
         try:
-            self.__dict__['initial'].update(
-                {'skills': Skill.objects.filter(allowed_profiles=id_)})
-            self.__dict__['initial'].update(
-                {'synergies': Synergy.objects.filter(allowed_profiles=id_)})
-            self.__dict__['initial'].update(
-                {'klasses': Klass.objects.filter(allowed_profiles=id_)})
-            self.__dict__['initial'].update(
-                {'elite_professions': EliteProfession.objects.filter(allowed_profiles=id_)})
-            self.__dict__['initial'].update(
-                {'elite_klasses': EliteKlass.objects.filter(allowed_profiles=id_)})
-            self.__dict__['initial'].update(
-                {'plates': Plate.objects.filter(allowed_profiles=id_)})
-            self.__dict__['initial'].update(
-                {'shields': Shield.objects.filter(allowed_profiles=id_)})
-            self.__dict__['initial'].update(
-                {'weapons': Weapon.objects.filter(allowed_profiles=id_)})
+            for field, Model in self.fields_and_models.items():
+                self.__dict__['initial'].update(
+                    {field: Model.objects.filter(allowed_profiles=id_)})
         except AttributeError:
             pass
 
     def save(self, *args, **kwargs):
         instance = super().save(*args, **kwargs)
         try:
-            update_rel_objs(
-                instance, Skill,
-                self.cleaned_data['skills'], "allowed_profiles")
-            update_rel_objs(
-                instance, Synergy,
-                self.cleaned_data['synergies'], "allowed_profiles")
-            update_rel_objs(
-                instance, Klass,
-                self.cleaned_data['klasses'], "allowed_profiles")
-            update_rel_objs(
-                instance, EliteProfession,
-                self.cleaned_data['elite_professions'], "allowed_profiles")
-            update_rel_objs(
-                instance, EliteKlass,
-                self.cleaned_data['elite_klasses'], "allowed_profiles")
-            update_rel_objs(
-                instance, Plate,
-                self.cleaned_data['plates'], "allowed_profiles")
-            update_rel_objs(
-                instance, Shield,
-                self.cleaned_data['shields'], "allowed_profiles")
-            update_rel_objs(
-                instance, Weapon,
-                self.cleaned_data['weapons'], "allowed_profiles")
+            for field, Model in self.fields_and_models.items():
+                update_rel_objs(
+                    instance, Model, self.cleaned_data[field],
+                    "allowed_profiles")
         except ValueError:
             text = self.cleaned_data['text']
             raise ValueError(
@@ -131,9 +110,6 @@ class ProfileAdminForm(forms.ModelForm):
 
 class ProfileAdmin(admin.ModelAdmin):
     form = ProfileAdminForm
-    # formfield_overrides = {
-    #     CharField: {'widget': TextInput(attrs={'size': 50})},
-    # }
     list_display = ['get_img', 'user', 'status', 'is_alive', 'is_active', 'image']
     list_editable = ['status', 'is_alive', 'is_active', 'image']
     list_filter = ['status', 'is_alive', 'is_active']
