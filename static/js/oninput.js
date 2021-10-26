3e90e0f221164413d8aff91a5a0d3b91d1e89fda
Hermotimos <lukas.kozicki@gmail.com>
2021-10-26 23:16:23 +0200

    del unsed code

--- static/js/oninput.js
+++
@@ -1,4 +0,0 @@
-function showIsTyping() {
-    document.getElementById('is-typing').innerHTML =
-    '<td class="align-center pt-2"><img class="rounded-circle article-img" src="/media/profile_pics/profile_default.jpg"></td><td class="align-bottom">...</td>';
-}
