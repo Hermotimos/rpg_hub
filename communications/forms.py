b267f3fd0b80684e8b680c0303275488ac96e73b
Hermotimos <lukas.kozicki@gmail.com>
2021-11-18 20:14:34 +0100

    del unused code

--- communications/forms.py
+++ communications/forms.py
@@ -8,38 +8,6 @@ from communications.models import Topic, ThreadTag, Statement, Option, \
     Announcement, Debate, Thread
 from users.models import Profile
 
-from django.forms import Select
-
-
-class SelectWOA(Select):
-    """
-    Select With Option Attributes:
-        subclass of Django's Select widget that allows attributes in options,
-        like disabled="disabled", title="help text", class="some classes",
-              style="background: color;"...
-
-    Pass a dict instead of a string for its label:
-        choices = [ ('value_1', 'label_1'),
-                    ...
-                    ('value_k', {'label': 'label_k', 'foo': 'bar', ...}),
-                    ... ]
-    The option k will be rendered as:
-        <option value="value_k" foo="bar" ...>label_k</option>
-    """
-
-    def create_option(self, name, value, label, selected, index,
-                      subindex=None, attrs=None):
-        if isinstance(label, dict):
-            opt_attrs = label.copy()
-            label = opt_attrs.pop('label')
-        else:
-            opt_attrs = {}
-        option_dict = super(SelectWOA, self).create_option(name, value,
-            label, selected, index, subindex=subindex, attrs=attrs)
-        for key,val in opt_attrs.items():
-            option_dict['attrs'][key] = val
-        return option_dict
-    
 
 class TopicCreateForm(forms.ModelForm):
     
