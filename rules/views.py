8bb6f32d0e9d6991d551e84df208516f13dbfff9
Hermotimos <lukas.kozicki@gmail.com>
2022-01-29 18:45:31 +0100

    temp cleanup code

--- rules/views.py
+++ rules/views.py
@@ -136,6 +136,18 @@ def rules_skills_list_view(request):
     skill_types = SkillType.objects.filter(kinds__name='Powszechne')
     skill_types = skill_types.prefetch_related(Prefetch('skills', queryset=skills), 'skill_groups')
     skill_types = skill_types.filter(skills__in=skills).distinct()
+    
+    from rules.models import Modifier, Condition
+    for m in Modifier.objects.all():
+        if not m.perks.exists() and not m.perks_new.exists():
+            print(m)
+            m.delete()
+    
+    for m in Condition.objects.all():
+        if not m.conditional_modifiers.exists():
+            print(m)
+            m.delete()
+    
 
     context = {
         'current_profile': current_profile,
