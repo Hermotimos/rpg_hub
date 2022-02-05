8e3b79906aca877c5e5175a9bda6b098194a22fd
Hermotimos <lukas.kozicki@gmail.com>
2022-02-05 10:53:12 +0100

    Progi Przeciążenia

--- rules/views.py
+++ rules/views.py
@@ -1,3 +1,7 @@
+from collections import namedtuple
+from math import floor
+from typing import Tuple
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Prefetch
@@ -166,17 +170,55 @@ def rules_tests_view(request):
     current_profile = Profile.objects.get(id=request.session['profile_id'])
     context = {
         'current_profile': current_profile,
-        'page_title': 'Testy Cech'
+        'page_title': 'Testy Cech',
     }
     return render(request, 'rules/tests.html', context)
 
 
+LOAD_LIMITS = [
+    [0, 3],
+    [2, 6],
+    [3, 10],
+    [6, 15],
+    [9, 22],
+    [12, 25],
+    [15, 28],
+    [18, 35],
+    [20, 45],
+    [20, 57],
+    [20, 62],
+    [23, 70],
+    [24, 75],
+    [27, 85],
+    [30, 90],
+    [35, 98],
+    [40, 110],
+    [55, 128],
+    [68, 140],
+    [80, 153]
+]
+
+
+def get_overload_ranges(vals: Tuple[int, int]) -> namedtuple:
+    load_regular, load_max = vals
+    third = (load_max - load_regular) // 3
+    overload_1 = f"{load_regular+1} - {load_regular+third}"
+    overload_2 = f"{load_regular+1+third} - {load_regular+third*2}"
+    overload_3 = f"{min(load_regular+1+third*2, load_max-1)} - {load_max-1}"
+    LoadInfo = namedtuple(
+        'LoadInfo',
+        ['load_regular', 'load_max', 'overload_1', 'overload_2', 'overload_3'])
+    return LoadInfo(load_regular, load_max, overload_1, overload_2, overload_3)
+    
+
 @login_required
 def rules_movement_view(request):
     current_profile = Profile.objects.get(id=request.session['profile_id'])
+    load_infos = [get_overload_ranges(v) for v in LOAD_LIMITS]
     context = {
         'current_profile': current_profile,
-        'page_title': 'Przeciążenie, Wydolność i Ruch'
+        'page_title': 'Przeciążenie, Wydolność i Ruch',
+        'load_infos': load_infos,
     }
     return render(request, 'rules/movement.html', context)
 
