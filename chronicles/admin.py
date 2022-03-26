00aeaad8e3f898b4993867561d6a716f85977cb2
Hermotimos <lukas.kozicki@gmail.com>
2022-03-26 10:20:19 +0100

    chronicles admin mods 2

--- chronicles/admin.py
+++ chronicles/admin.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib import admin
-from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.db import models
 
 from chronicles.models import (
@@ -11,8 +10,8 @@ from chronicles.models import (
     GameSession,
 )
 from communications.models import Thread
-from rpg_project.utils import formfield_for_dbfield_cached, formfield_with_cache
-from toponomikon.models import SecondaryLocation
+from rpg_project.utils import formfield_with_cache
+from toponomikon.models import Location
 from users.models import Profile
 
 
@@ -22,6 +21,36 @@ from users.models import Profile
 @admin.register(Date)
 class DateAdmin(admin.ModelAdmin):
     pass
+
+
+# -----------------------------------------------------------------------------
+
+
+@admin.register(HistoryEvent)
+class HistoryEventAdmin(admin.ModelAdmin):
+    fields = [
+        'date_start', 'date_end', 'in_timeunit', 'description_short',
+        'description_long', 'plot_threads', 'locations',
+        'known_short_desc', 'known_long_desc'
+    ]
+    filter_horizontal = [
+        'known_short_desc', 'known_long_desc', 'locations', 'picture_sets',
+        'plot_threads'
+    ]
+    list_display = [
+        'id', 'date_in_period', 'date_in_era', 'date_in_chronology',
+        'description_short', 'description_long', 'audio'
+    ]
+    list_editable = ['description_short', 'description_long', 'audio']
+    search_fields = ['description_short', 'description_long']
+    
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name in ["known_short_desc", "known_long_desc"]:
+            kwargs["queryset"] = Profile.non_gm.all()
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+
+# -----------------------------------------------------------------------------
 
 
 @admin.register(GameEvent)
@@ -56,11 +85,14 @@ class GameEventAdmin(admin.ModelAdmin):
             kwargs["queryset"] = Thread.objects.filter(kind='Debate')
         return super().formfield_for_manytomany(db_field, request, **kwargs)
         
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        fields = [
-            'audio',    # Tested that here only audio optimizes queries
-        ]
-        return formfield_for_dbfield_cached(self, db_field, fields, **kwargs)
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
+        for field in [
+            'audio',  # Tested that here only audio optimizes queries
+        ]:
+            if db_field.name == field:
+                formfield = formfield_with_cache(field, formfield, request)
+        return formfield
 
 
 # -----------------------------------------------------------------------------
@@ -120,40 +152,7 @@ class GameEventInline(admin.TabularInline):
                 formfield = formfield_with_cache(field, formfield, request)
         return formfield
     
-# -----------------------------------------------------------------------------
 
-
-class HistoryEventAdminForm(forms.ModelForm):
-    class Meta:
-        model = GameEvent
-        fields = [
-            'date_start', 'date_end', 'in_timeunit', 'description_short',
-            'description_long', 'plot_threads', 'locations',
-            'known_short_desc', 'known_long_desc'
-        ]
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['plot_threads'].label = 'Active PlotThreads'
-        self.fields['known_short_desc'].queryset = Profile.non_gm.all()
-        self.fields['known_long_desc'].queryset = Profile.non_gm.all()
-        
-        
-@admin.register(HistoryEvent)
-class HistoryEventAdmin(admin.ModelAdmin):
-    filter_horizontal = [
-        'known_short_desc', 'known_long_desc', 'locations', 'picture_sets',
-        'plot_threads'
-    ]
-    form = HistoryEventAdminForm
-    list_display = [
-        'id', 'date_in_period', 'date_in_era', 'date_in_chronology',
-        'description_short', 'description_long', 'audio'
-    ]
-    list_editable = ['description_short', 'description_long', 'audio']
-    search_fields = ['description_short', 'description_long']
-    
-    
 # -----------------------------------------------------------------------------
 
 
@@ -169,13 +168,16 @@ class GameSessionAdmin(admin.ModelAdmin):
     ordering = ['-game_no']
     search_fields = ['title']
 
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        fields = [
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
+        for field in [
             'chapter',
-        ]
-        return formfield_for_dbfield_cached(self, db_field, fields, **kwargs)
+        ]:
+            if db_field.name == field:
+                formfield = formfield_with_cache(field, formfield, request)
+        return formfield
 
-    
+
 @admin.register(Chapter)
 class ChapterAdmin(admin.ModelAdmin):
     list_display = ['chapter_no', 'title', 'image']
@@ -195,44 +197,29 @@ class PlotThreadAdmin(admin.ModelAdmin):
 # -----------------------------------------------------------------------------
 
 
-@admin.register(TimeUnit)
-class TimeUnitAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description_short', 'description_long']
-    search_fields = ['description_short', 'description_long']
+# @admin.register(TimeUnit)
+# class TimeUnitAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'description_short', 'description_long']
+#     search_fields = ['description_short', 'description_long']
     
-
-class ChronologyAdminForm(forms.ModelForm):
-    
-    class Meta:
-        model = TimeUnit
-        fields = ['name', 'name_genetive', 'date_start', 'date_end',
-                  'description_short', 'description_long',
-                  'known_short_desc', 'known_long_desc']
-
 
 @admin.register(Chronology)
 class ChronologyAdmin(admin.ModelAdmin):
+    fields = [
+        'name', 'name_genetive', 'date_start', 'date_end', 'description_short',
+        'description_long', 'known_short_desc', 'known_long_desc',
+    ]
     filter_horizontal = ['known_short_desc', 'known_long_desc']
-    form = ChronologyAdminForm
 
 
-class TimeSpanForm(forms.ModelForm):
-    
-    class Meta:
-        model = TimeUnit
-        fields = ['name', 'name_genetive', 'date_start', 'date_end',
-                  'in_timeunit', 'description_short', 'description_long',
-                  'known_short_desc', 'known_long_desc']
-
-
-class EraAdminForm(TimeSpanForm):
-    in_timeunit = forms.ModelChoiceField(queryset=Chronology.objects.all())
-
-
-@admin.register(Era)
-class EraAdmin(admin.ModelAdmin):
+@admin.register(Era, Period)
+class EraAndPeriodAdmin(admin.ModelAdmin):
+    fields = [
+        'name', 'name_genetive', 'date_start', 'date_end', 'description_short',
+        'description_long', 'known_short_desc', 'known_long_desc',
+        'in_timeunit',
+    ]
     filter_horizontal = ['known_short_desc', 'known_long_desc']
-    form = EraAdminForm
     formfield_overrides = {
         models.TextField: {'widget': forms.Textarea(attrs={'rows': 15, 'cols': 40})},
         models.CharField: {'widget': forms.TextInput(attrs={'size': 12})},
@@ -250,50 +237,20 @@ class EraAdmin(admin.ModelAdmin):
         'name', 'name_genetive', 'description_short', 'description_long'
     ]
 
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        fields = [
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
+        for field in [
             'date_start',
             'date_end',
             'in_timeunit',
-        ]
-        return formfield_for_dbfield_cached(self, db_field, fields, **kwargs)
-    
-    
-class PeriodAdminForm(TimeSpanForm):
-    in_timeunit = forms.ModelChoiceField(queryset=Era.objects.all())
-    locations = forms.ModelMultipleChoiceField(
-        queryset=SecondaryLocation.objects.filter(
-            location_type__name='Kraina'),
-        required=False,
-        widget=FilteredSelectMultiple('Secondary locations', False),
-    )
+        ]:
+            if db_field.name == field:
+                formfield = formfield_with_cache(field, formfield, request)
+        return formfield
 
-
-@admin.register(Period)
-class PeriodAdmin(EraAdmin):
-    filter_horizontal = ['known_short_desc', 'known_long_desc']
-    form = PeriodAdminForm
-    formfield_overrides = {
-        models.TextField: {'widget': forms.Textarea(attrs={'rows': 15, 'cols': 40})},
-        models.CharField: {'widget': forms.TextInput(attrs={'size': 12})},
-        models.ForeignKey: {'widget': forms.Select(attrs={'style': 'width:180px'})},
-    }
-    list_display = [
-        'id', 'name', 'name_genetive', 'date_start', 'date_end', 'in_timeunit',
-        'description_short', 'description_long'
-    ]
-    list_editable = [
-        'name', 'name_genetive', 'date_start', 'date_end', 'in_timeunit',
-        'description_short', 'description_long'
-    ]
-    search_fields = [
-        'name', 'name_genetive', 'description_short', 'description_long'
-    ]
-
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        fields = [
-            'date_start',
-            'date_end',
-            'in_timeunit',
-        ]
-        return formfield_for_dbfield_cached(self, db_field, fields, **kwargs)
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "in_timeunit":
+            kwargs["queryset"] = Chronology.objects.all()
+        if db_field.name == "locations":
+            kwargs["queryset"] = Location.objects.filter(location_type__name='Kraina')
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
