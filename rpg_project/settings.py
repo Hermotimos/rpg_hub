e1fe1e3e93383aaf99d812b2302d05abf890c8a1
Hermotimos <lukas.kozicki@gmail.com>
2022-04-30 21:58:09 +0200

    minor

--- rpg_project/settings.py
+++ rpg_project/settings.py
@@ -193,7 +193,3 @@ INTERNAL_IPS = ['127.0.0.1', ]
 # https://www.taricorp.net/2020/windows-mime-pitfalls/
 # https://stackoverflow.com/questions/16303098/django-development-server-and-mime-types/64055514#64055514
 # After editing registry - restart local server for changes to take effect
-
-
-# CKEDITOR_BASEPATH = os.path.join(os.path.dirname(STATIC_ROOT), 'ckeditor/ckeditor/')
-# print(CKEDITOR_BASEPATH)
