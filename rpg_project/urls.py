8fa344a9eb95a4678b99abd958a5b6b6634b1910
Hermotimos <lukas.kozicki@gmail.com>
2022-04-30 21:41:56 +0200

    minor

--- rpg_project/urls.py
+++ rpg_project/urls.py
@@ -37,7 +37,6 @@ urlpatterns = [
     # path('tinymce/', include('tinymce.urls')),
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
