d8f786b432e6040800f42bfc4a7f39da88d9514c
Hermotimos <lukas.kozicki@gmail.com>
2022-05-03 13:15:54 +0200

    settings cleanup

--- rpg_project/settings.py
+++ rpg_project/settings.py
@@ -1,25 +1,13 @@
 import io
-import json
 import os
 from urllib.parse import urlparse
 
 import environ
-from django.core.exceptions import ImproperlyConfigured
 from google.cloud import secretmanager
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# def get_secret(setting):
-#     """Get secret setting or fail with ImproperlyConfigured"""
-#     with open(os.path.join(f'{BASE_DIR}/rpg_project/', 'secret.json')) as f:
-#         secrets = json.load(f)
-#     try:
-#         return secrets[setting]
-#     except KeyError:
-#         raise ImproperlyConfigured(f"Set the {setting} setting")
 
 
 # -----------------------------------------------------------------------------
@@ -46,16 +34,13 @@ elif os.getenv("TRAMPOLINE_CI", None):
 
 # Pull secrets from Secret Manager
 elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    # Pull secrets from Secret Manager
     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-
     client = secretmanager.SecretManagerServiceClient()
     settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
-    env.read_env(io.StringIO(payload))
     
+    env.read_env(io.StringIO(payload))
 else:
     raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 
