ec45a4c565a6a2d242b9efb30c9d1902f8539334
Hermotimos <lukas.kozicki@gmail.com>
2022-05-03 17:55:41 +0200

    ALLOWED_HOSTS GCP

--- rpg_project/settings.py
+++ rpg_project/settings.py
@@ -69,7 +69,10 @@ if APPENGINE_URL:
     if not urlparse(APPENGINE_URL).scheme:
         APPENGINE_URL = f"https://{APPENGINE_URL}"
 
-    ALLOWED_HOSTS = [urlparse(APPENGINE_URL).netloc]
+    # ALLOWED_HOSTS = [urlparse(APPENGINE_URL).netloc]
+    # For enabling older app versions to host site
+    ALLOWED_HOSTS = ['*']
+    
     CSRF_TRUSTED_ORIGINS = [APPENGINE_URL]
     SECURE_SSL_REDIRECT = True
 else:
