7c693bdcb11070ce085139e2d8e468ab8cef1b58
Hermotimos <lukas.kozicki@gmail.com>
2022-08-16 19:56:13 +0200

    contact: fix Demand addressees in form

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,7 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django import forms
+from django.db.models import Q
 
 from contact.models import Demand, DemandAnswer, Plan
 from users.models import Profile
@@ -23,17 +24,15 @@ class DemandsCreateForm(forms.ModelForm):
         self.fields['image'].label = "Załącz obraz"
         self.fields['text'].label = "Tekst"
 
-        addressees = Profile.contactables.exclude(id=profile.id)
-        gms = addressees.filter(status='gm')
-        print(profile.character.acquaintances.all())
-        if profile.status == 'player':
-            addressees = addressees.filter(
-                character__in=profile.character.acquaintances.all())
-            addressees = (addressees | gms)
-        elif profile.status == 'npc':
-            addressees = addressees.filter(status='gm')
+        if profile.status == 'gm':
+            addressees = Profile.players.all()
+        elif profile.status == 'player':
+            addressees = Profile.contactables.filter(
+                Q(character__in=profile.character.acquaintances.all()) | Q(status='gm'))
+        else:
+            addressees = Profile.objects.none()
             
-        self.fields['addressee'].queryset = addressees
+        self.fields['addressee'].queryset = addressees.exclude(id=profile.id)
         
         self.helper = FormHelper()
         self.helper.add_input(
