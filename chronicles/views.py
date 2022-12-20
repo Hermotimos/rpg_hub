1d9ac19d877e74e21311ddeccb2fdb48bcbeac49
Hermotimos <lukas.kozicki@gmail.com>
2022-12-20 20:29:45 +0100

    fix label in inform for GameEvent (TimeUnit)

--- chronicles/views.py
+++ chronicles/views.py
@@ -206,9 +206,8 @@ def game_event_inform_view(request, game_event_id):
 
     context = {
         'page_title': 'Poinformuj o wydarzeniu',
-        'event': game_event,
+        'game_event': game_event,
         'informables': game_event.informables(current_profile),
-        'event_type': 'game_event'
     }
     if current_profile in allowed or current_profile.status == 'gm':
         return render(request, 'chronicles/_event_inform.html', context)
