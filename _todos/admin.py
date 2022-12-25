1b002e2bd3ee85fac15a94e639d2f9c7473c38fc
Hermotimos <lukas.kozicki@gmail.com>
2022-12-25 13:34:05 +0100

    _todos app - more

--- _todos/admin.py
+++ _todos/admin.py
@@ -1,3 +1,5 @@
+from statistics import mean
+from django.utils.safestring import mark_safe
 from django import forms
 from django.contrib import admin
 from django.db import models
@@ -8,8 +10,8 @@ from _todos.models import TODOList
 @admin.register(TODOList)
 class TODOListAdmin(admin.ModelAdmin):
     formfield_overrides = {
-        models.DecimalField: {'widget': forms.NumberInput(attrs={'size': 6})},
-        models.TextField: {'widget': forms.Textarea(attrs={'rows': 1, 'cols': 15})},
+        models.DecimalField: {'widget': forms.NumberInput(attrs={'size': 5})},
+        models.TextField: {'widget': forms.Textarea(attrs={'rows': 2, 'cols': 20})},
     }
     list_display = [
         'daydate', 'SUNWALK', 'MED', 'TETRIS', 'RELAX', 'sleep', 'IForKETO',
@@ -17,7 +19,8 @@ class TODOListAdmin(admin.ModelAdmin):
         'fishoilord3', 'water', 'coffeex2', 'noA', 'warmup', 'stretching',
         'workout', 'CODE', 'ENG', 'DE', 'FR', 'UKR',
         'awareness', 'happiness', 'openness', 'focus',
-        'anger', 'fear', 'emptiness', 'chaos',
+        'anger', 'fear', 'emptiness', 'chaos', 'res',
+        'comments',
     ]
     list_editable = [
         'SUNWALK', 'MED', 'TETRIS', 'RELAX', 'sleep', 'IForKETO',
@@ -26,9 +29,32 @@ class TODOListAdmin(admin.ModelAdmin):
         'workout', 'CODE', 'ENG', 'DE', 'FR', 'UKR',
         'awareness', 'happiness', 'openness', 'focus',
         'anger', 'fear', 'emptiness', 'chaos',
+        'comments',
     ]
 
     class Media:
         css = {
-            'all': ('/static/css/admin.css',)
+            'all': ('/static/css/todos_admin.css',)
         }
+
+    def res(self, obj):
+        try:
+            sum_plus = mean([obj.awareness, obj.happiness, obj.openness, obj.focus])
+            sum_minus = mean([obj.anger, obj.fear, obj.emptiness, obj.chaos])
+            res = sum_plus - sum_minus
+
+            if res < 1.1:
+                color = "red"
+            elif res < 2.1:
+                color = "gold"
+            elif res < 3.1:
+                color = "deepskyblue"
+            elif res < 4.1:
+                color = "indigo"
+            else:
+                color = "black"
+                
+            return mark_safe(f'<span style="color: {color}"><b>{res}</b></span>')
+        
+        except TypeError:
+            return "-"
