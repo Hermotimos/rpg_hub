2cfeff8ec40b81d4b22f3bd9b76d0265af6f3907
hermotimos <lukas.kozicki@gmail.com>
2023-09-18 18:18:52 +0200

    minor reordering in urls - use include to group similar URLs

--- chronicles/urls.py
+++ chronicles/urls.py
@@ -1,19 +1,26 @@
-from django.urls import path
+from django.urls import include, path
+
 from chronicles import views
 
 
 app_name = 'chronicles'
 urlpatterns = [
-    # chronicle
-    path('chronicle/main/', views.chronicle_main_view, name='chronicle-main'),
-    path('chronicle/all/', views.chronicle_all_view, name='chronicle-all'),
-    path('chronicle/game:<int:game_id>/', views.chronicle_game_view,
-         name='chronicle-game'),
-    path('chronicle/chapter:<int:chapter_id>/', views.chronicle_chapter_view,
-         name='chronicle-chapter'),
+
+    path(
+        'chronicle/',
+        include(
+            [
+                path('main/', views.chronicle_main_view, name='chronicle-main'),
+                path('all/', views.chronicle_all_view, name='chronicle-all'),
+                path('game:<int:game_id>/', views.chronicle_game_view, name='chronicle-game'),
+                path('chapter:<int:chapter_id>/', views.chronicle_chapter_view, name='chronicle-chapter'),
+            ]
+        )
+    ),
+
     path('inform/game-event:<int:game_event_id>/',
          views.game_event_inform_view, name='game-event-inform'),
-    
+
     # timeline
     path('timeline/', views.timeline_view, name='timeline'),
     path('chronologies/', views.chronologies_view, name='chronologies'),
--- contact/urls.py
+++ contact/urls.py
@@ -1,29 +1,40 @@
-from django.urls import path
+from django.urls import include, path
+
 from contact import views
 
 
 app_name = 'contact'
 urlpatterns = [
-    # demands
-    path('demands/main/', views.demands_main_view, name='demands-main'),
-    path('demands/create/', views.demands_create_view, name='demands-create'),
-    path('demands/delete:<int:demand_id>/', views.demands_delete_view,
-         name='demands-delete'),
-    path('demands/detail:<int:demand_id>/', views.demands_detail_view,
-         name='demands-detail'),
-    path('demands/done-undone:<int:demand_id>/done',
-         views.demand_done_undone_view, {'is_done': True},
-         name='demand-done'),
-    path('demands/done-undone:<int:demand_id>/undone',
-         views.demand_done_undone_view, {'is_done': False},
-         name='demand-undone'),
-    
-    # plans
-    path('plans/main/', views.plans_main_view, name='plans-main'),
-    path('plans/for-gm/', views.plans_for_gm_view, name='plans-for-gm'),
-    path('plans/create/', views.plans_create_view, name='plans-create'),
-    path('plans/delete:<int:plan_id>/', views.plans_delete_view,
-         name='plans-delete'),
-    path('plans/modify:<int:plan_id>/', views.plans_modify_view,
-         name='plans-modify'),
+
+    path(
+        'demands/',
+        include(
+            [
+                path('main/', views.demands_main_view, name='demands-main'),
+                path('create/', views.demands_create_view, name='demands-create'),
+                path('delete:<int:demand_id>/', views.demands_delete_view, name='demands-delete'),
+                path('detail:<int:demand_id>/', views.demands_detail_view, name='demands-detail'),
+                path('done-undone:<int:demand_id>/done',
+                    views.demand_done_undone_view, {'is_done': True},
+                    name='demand-done'),
+                path('done-undone:<int:demand_id>/undone',
+                    views.demand_done_undone_view, {'is_done': False},
+                    name='demand-undone'),
+            ]
+        )
+
+     ),
+
+    path(
+        'plans/',
+        include(
+            [
+                path('main/', views.plans_main_view, name='plans-main'),
+                path('for-gm/', views.plans_for_gm_view, name='plans-for-gm'),
+                path('create/', views.plans_create_view, name='plans-create'),
+                path('delete:<int:plan_id>/', views.plans_delete_view, name='plans-delete'),
+                path('modify:<int:plan_id>/', views.plans_modify_view, name='plans-modify'),
+            ]
+        )
+    ),
 ]
