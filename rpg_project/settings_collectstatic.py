5bbb77ce088b3657b1840d935db3ff8d51d9c356
hermotimos <lukas.kozicki@gmail.com>
2023-09-22 21:31:12 +0200

    move file for staticfiles collectstatic

---
+++ rpg_project/settings_collectstatic.py
@@ -0,0 +1,34 @@
+
+from rpg_project.settings import *
+
+"""
+This file is for "collecting" static files from the local machine to Storage bucket
+In manage.py temporarily replace:
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rpg_project.settings')
+
+    with
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rpg_project.settings_collectstatic')
+
+    Then run python manage.py collectstatic.
+    Revert changes in manage.py
+"""
+
+GS_BUCKET_NAME = env("GS_BUCKET_NAME")
+STATIC_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/"
+GS_LOCATION = "static"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+DEFAULT_FILE_STORAGE = 'rpg_project.storages.GoogleCloudMediaFileStorage'
+STATICFILES_STORAGE = 'rpg_project.storages.GoogleCloudStaticFileStorage'
+
+
+GOOGLE_APPLICATION_CREDENTIALS = env("GOOGLE_APPLICATION_CREDENTIALS")
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    GOOGLE_APPLICATION_CREDENTIALS)
+
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')        # path to uploaded pics
+# MEDIA_URL = '/media/'                               # url to media
+
+
