0353e72fa7ecd6626297c69f539d10f15b469379
hermotimos <lukas.kozicki@gmail.com>
2023-09-22 18:09:43 +0200

    revert Memcached

--- rpg_project/settings.py
+++ rpg_project/settings.py
@@ -193,17 +193,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # -----------------------------------------------------------------------------
 # Cache
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-#     }
-# }
 CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
-        "LOCATION": "127.0.0.1:11211",
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
 
 # -----------------------------------------------------------------------------
 # Password validation
