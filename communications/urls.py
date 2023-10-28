1d85be521d1140026b8f19271d98f588c47da450
Łukasz Kozicki <lukaskozicki@gmail.com>
2023-10-28 07:19:52 +0000

    REST with DRF simple example

--- communications/urls.py
+++ communications/urls.py
@@ -6,19 +6,19 @@ app_name = 'communications'
 urlpatterns = [
     path('threads:<str:thread_kind>/<str:tag_title>/', views.threads_view, name='threads'),
     path('thread/<int:thread_id>/<str:tag_title>/', views.thread_view, name='thread'),
-    
+
     path('statement-create', views.statement_create_view, name='statement-create'),
     path('statements/<int:thread_id>/', views.statements, name='statements'),
-    
+
     path('thread:<int:thread_id>/unfollow', views.unfollow_thread_view, name='unfollow'),
     path('thread:<int:thread_id>/follow', views.follow_thread_view, name='follow'),
-    
+
     path('create-thread:<str:thread_kind>/', views.create_thread_view, name='create-thread'),
-    
+
     # news
-    
+
     # path('news-detail:<int:news_id>/follow', views.follow_news_view, name='follow'),
-    
+
     # survey
     # path('survey-detail:<int:survey_id>/survey-option:<int:option_id>/yes', views.vote_yes_view, name='survey-yes'),
     # path('survey-detail:<int:survey_id>/survey-option:<int:option_id>/no', views.vote_no_view, name='survey-no'),
@@ -28,6 +28,6 @@ urlpatterns = [
     # path('survey-detail:<int:survey_id>/survey-option:<int:option_id>/delete', views.survey_option_delete_view,
     #      name='survey-option-delete'),
 
-    
-    
+
+
 ]
